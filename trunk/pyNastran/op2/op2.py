05ee69b9f7dd00dc583d4dd96041d3b700479275
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2011-11-06 06:10:53 +0000

    added support displacements and spc forces (note only for the test op2) added some objects to place the results in (need to add subcase to them)

--- trunk/pyNastran/op2/op2.py
+++ trunk/pyNastran/op2/op2.py
@@ -4,38 +4,9 @@ import os
 import sys
 import struct
 from struct import unpack
+from op2_Objects import *
 
-class Op2(FortranFile,Op2Codes):
-    def __init__(self,infileName): 
-        self.infilename = infileName
-    
-    def read(self):
-        self.op2 = open(self.infilename,'rb')
- 
-        self.n = self.op2.tell()
-        print "self.n = ",self.n
-        self.readMarkers([3])
-        ints = self.readIntBlock()
-        print "*ints = ",ints
-        self.readMarkers([7])
-
-        word = self.readStringBlock() # Nastran Fort Tape ID Code - 
-        print "word = |%r|" %(word)
-
-        self.readMarkers([2])
-
-        self.skip(4*4)
-        self.readTable_Geom1()
-        self.readTable_Geom2()
-        self.readTable_Geom3()
-        self.readTable_Geom4()
-        self.readTable_OQG1()
-        self.readTable_OES1X1()
-        print "end of oes1x1"
-
-        self.printSection(4*51+12)
-        
-        
+class GeometryTables(object):
     def readTable_Geom1(self):
         self.readMarkers([-1,0,2])
 
@@ -58,7 +29,7 @@ class Op2(FortranFile,Op2Codes):
         #self.printTableCode(marker)
         
         ints = self.readIntBlock()
-        print "*ints = ",ints, len(ints)
+        #print "*ints = ",ints, len(ints)
 
         while ints:
             coord1 = ints[:6]
@@ -172,6 +143,38 @@ class Op2(FortranFile,Op2Codes):
         self.readMarkers([-5,1,0,0,2])
         print "------------"
 
+
+class Op2(FortranFile,Op2Codes,GeometryTables):
+    def __init__(self,infileName): 
+        self.infilename = infileName
+    
+    def read(self):
+        self.op2 = open(self.infilename,'rb')
+ 
+        self.n = self.op2.tell()
+        print "self.n = ",self.n
+        self.readMarkers([3])
+        ints = self.readIntBlock()
+        print "*ints = ",ints
+        self.readMarkers([7])
+
+        word = self.readStringBlock() # Nastran Fort Tape ID Code - 
+        print "word = |%r|" %(word)
+
+        self.readMarkers([2])
+
+        self.skip(4*4)
+        self.readTable_Geom1()
+        self.readTable_Geom2()
+        self.readTable_Geom3()
+        self.readTable_Geom4()
+        self.readTable_OQG1()
+        self.readTable_OES1X1()
+        print "end of oes1x1"
+
+        #self.printSection(4*51+12)
+        
+        
     def readTable_OQG1(self):
         ## OQG1
         word = self.readStringBlock()
@@ -182,35 +185,84 @@ class Op2(FortranFile,Op2Codes):
         self.readMarkers([-2,1,0,7])
         ints = self.readIntBlock()
         print "*ints = ",ints
-        self.readMarkers([-3,1,0,146])
+        self.readMarkers([-3,1,0])
+        bufferWords = self.getMarker()
+        print "bufferWords = ",bufferWords,bufferWords*4
+
         
-        self.skip(4*51)
+        #data = self.getData(4)
+        #bufferSize, = unpack('i',data)
+        #data = self.getData(4*51)
+        #self.printBlock(data)
+        data = self.getData(4)
+        bufferSize, = unpack('i',data)
+        print "bufferSize = ",bufferSize
+        data = self.getData(4*50)
+        aCode = self.getBlockIntEntry(data,1)
+        print "aCode = ",aCode
+        (analysisCode,deviceCode,tableCode,three,subcase) = self.parseAnalysisCode(data)
+        
+
+        #self.skip(4*47)
         word = self.readString(384)
         print "word = |%s|" %(word)
         self.readHollerith()
         
-        self.readMarkers([-4,1,0,32])
-        self.scan(4*34)
+        self.readMarkers([-4,1,0])
+        wordCount = self.getMarker()
+        data = self.readBlock()
+        #self.printBlock(data)
+
+        iSubcase = 1 ## @todo temporary
+        spcForcesObj = spcForcesObject(iSubcase)
+        self.readScalars(deviceCode,data,spcForcesObj)
 
         self.readMarkers([-5,1,0,0,2])
-        #word = self.readStringBlock()
-        #print "word = |%r|" %(word)
-        self.scan(16)
+        print str(spcForcesObj)
+
+        word = self.readStringBlock()  # OUGV1
+        print "word = |%r|" %(word)
         self.readMarkers([-1,7])
         ints = self.readIntBlock()
         print "*ints = ",ints
         self.readMarkers([-2,1,0,7])
         ints = self.readIntBlock()
         print "*ints = ",ints
-        self.readMarkers([-3,1,0,146])
-        self.skip(4*51)
+        self.readMarkers([-3,1,0])
+        bufferWords = self.getMarker()
+        print "bufferWords = ",bufferWords,bufferWords*4,'\n'
+
+        data = self.getData(4*51)
+        #self.printBlock(data)
+
+        #self.skip(4*51)
         word = self.readString(384)
         print "word = |%s|" %(word)
         self.readHollerith()
+        
 
-        self.readMarkers([-4,1,0,32])
-        self.skip(4*34)
+        self.readMarkers([-4,1,0])
+        bufferWords = self.getMarker()
+        data = self.readBlock()
+        
+        iSubcase = 1 ## @todo temporary
+        dispObj = displacementObject(iSubcase)
+        self.readScalars(deviceCode,data,dispObj)
+        print str(dispObj)
+
         self.readMarkers([-5,1,0,0,2])
+        #sys.exit('end of displacements')
+
+    def readScalars(self,deviceCode,data,scalarObject):
+        while data:
+            (gridDevice,gridType,dx,dy,dz,rx,ry,rz) = unpack('iiffffff',data[0:32])
+            #print "gridDevice = ",gridDevice
+            #print "deviceCode = ",deviceCode
+            grid = (gridDevice-deviceCode)/10
+            print "grid=%g dx=%g dy=%g dz=%g" %(grid,dx,dy,dz)
+            scalarObject.add(grid,dx,dy,dz,rx,ry,rz)
+            data = data[32:]
+        ###
 
     def readTable_OES1X1(self):
         word = self.readStringBlock() # OES1X1
@@ -233,25 +285,21 @@ class Op2(FortranFile,Op2Codes):
         bufferWords = self.getMarker()
         print "bufferWords = ",bufferWords,bufferWords*4
         print "tell3 = ",self.op2.tell()
-        #self.skip(4*51)
         
         data = self.getData(4)
         bufferSize, = unpack('i',data)
-
-
         data = self.getData(4*51)
 
-        self.printBlock(data)
+        #self.printBlock(data)
         nWide = self.getBlockIntEntry(data,10)
         print "nWide = ",nWide
         thermal = self.getBlockIntEntry(data,21)
 
         #print "len(block) = ",len(data)  # 4
-        (aCode,tCode,elementType,iSubcase) = unpack('iiii',data[:16])
-        print "aCode=%s tCode=%s elementType=%s iSubcase=%s" %(aCode,tCode,elementType,iSubcase)
+        (analysisCode,deviceCode,tCode,elementType,iSubcase) = self.parseAnalysisCode(data)
         data = data[16:]
         
-        (word5,word6,word7) = unpack('iii',data[:12]) # depends on aCode,tCode
+        (word5,word6,word7) = unpack('iii',data[:12]) # depends on analysisCode,tCode
         print "word5=%s word6=%s word7=%s" %(word5,word6,word7)
         data = data[12:]
          # 8      8        10         11
@@ -267,41 +315,41 @@ class Op2(FortranFile,Op2Codes):
         
         print "tell4 = ",self.op2.tell()
         print "n4 = ",self.n
-        #if self.n >= 5604:  raise Exception(self.n)
 
         print "word* = |%s|" %(word)
         self.readMarkers([-4,1,0])
         #self.printSection(100)
 
         data = self.getData(16)
-        self.printBlock(data)
-        bufferWords,  = unpack('i',data[4:8])
+        #self.printBlock(data)
+        bufferWords, = unpack('i',data[4:8])
 
         print "*********************"
         #bufferWords = self.getMarker() # 87 - buffer
         print "bufferWords = ",bufferWords,bufferWords*4
         print "*elementType = ",elementType
         
-
         print "op2.tell=%s n=%s" %(self.op2.tell(),self.n)
+        assert self.op2.tell()==5656
+
         data = self.getData(bufferWords*4)
-        self.printBlock(data)
+        #self.printBlock(data)
         if elementType==144:
             self.CQUAD4(data)  # 144
         else:
             raise RuntimeError('elementType=%s is not supported' %(elmentType))
 
         self.readMarkers([-5,1,0,])
-        print "tell5 = ",self.op2.tell()
+        #print "tell5 = ",self.op2.tell()
         self.readMarkers([0,0,])
-        print "end tell = ",self.op2.tell()
+        #print "end tell = ",self.op2.tell()
 
     def CQUAD4(self,data):
         """
         GRID-ID  DISTANCE,NORMAL-X,NORMAL-Y,SHEAR-XY,ANGLE,MAJOR MINOR,VONMISES
         """
         nNodes = 5 # centroid + 4 corner points
-        self.printSection(20)
+        #self.printSection(20)
         #term = data[0:4] CEN/
         #data = data[4:]
         print "*****"
@@ -317,10 +365,18 @@ class Op2(FortranFile,Op2Codes):
                 out = unpack('iffffffffffffffff',eData[0:68])
                 (grid,fd1,sx1,sy1,txy1,angle1,major1,minor1,vm1,
                       fd2,sx2,sy2,txy2,angle2,major2,minor2,vm2,) = out
-                print "eid=%i grid=%i fd1=%i sx1=%i sy1=%i txy1=%i angle1=%i major1=%i minor1=%i vm1=%i" %(eid,grid,fd1,sx1,sy1,txy1,angle1,major1,minor1,vm1)
-                print "               fd2=%i sx2=%i sy2=%i txy2=%i angle2=%i major2=%i minor2=%i vm2=%i\n"          %(fd2,sx2,sy2,txy2,angle2,major2,minor2,vm2)
-                print "len(data) = ",len(data)
+                #print "eid=%i grid=%i fd1=%i sx1=%i sy1=%i txy1=%i angle1=%i major1=%i minor1=%i vm1=%i" %(eid,grid,fd1,sx1,sy1,txy1,angle1,major1,minor1,vm1)
+                #print "               fd2=%i sx2=%i sy2=%i txy2=%i angle2=%i major2=%i minor2=%i vm2=%i\n"          %(fd2,sx2,sy2,txy2,angle2,major2,minor2,vm2)
+                #print "len(data) = ",len(data)
             ###
             #sys.exit('asdf')
         self.skip(4)
         ###
+
+    def parseAnalysisCode(self,data):
+        #self.printBlock(data)
+        (aCode,tCode,elementType,iSubcase) = unpack('iiii',data[:16])
+        deviceCode   = aCode%10
+        approachCode = (aCode-deviceCode)/10
+        print "aCode=%s analysisCode=%s deviceCode=%s tCode=%s elementType=%s iSubcase=%s" %(aCode,approachCode,deviceCode,tCode,elementType,iSubcase)
+        return (approachCode,deviceCode,tCode,elementType,iSubcase)
