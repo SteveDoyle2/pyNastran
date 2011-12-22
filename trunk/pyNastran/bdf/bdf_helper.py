557e1932877dd00e533b6c60947a186d4042a2e9
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2011-12-22 04:18:10 +0000

    split out springs updated per v0.2.0

--- trunk/pyNastran/bdf/bdf_helper.py
+++ trunk/pyNastran/bdf/bdf_helper.py
@@ -1,9 +1,10 @@
 import sys
 import copy
-from pyNastran.bdf.fieldWriter import printCard
-from pyNastran.bdf.bdf_writeMesh import writeMesh
+from pyNastran.bdf.fieldWriter     import printCard
+from pyNastran.bdf.bdf_writeMesh   import writeMesh
 from pyNastran.bdf.bdf_cardMethods import cardMethods
-from pyNastran.bdf.crossReference import XrefMesh
+from pyNastran.bdf.crossReference  import XrefMesh
+from pyNastran.bdf.methods         import bdf_Methods
 
 class getMethods(object):
     def __init__(self):
@@ -192,63 +193,7 @@ class getMethods(object):
 
     def NLParm(self,nid):
         return self.nlparms[nid]
-
-    #--------------------
-    # METHODS
-
-    def MassProperties(self):
-                 #Ixx Iyy Izz, Ixy, Ixz Iyz
-        I = array(0., 0., 0.,  0.,  0., 0.,)
-        for element in self.elements:
-            p = e.Centroid()  # not really coded across the board
-            m = e.Mass()
-            (x,y,z) = p
-            I[0] = m*x*x  # Ixx
-            I[1] = m*y*y  # Iyy
-            I[2] = m*z*z  # Izz
-            I[3] = m*x*y  # Ixy
-            I[4] = m*x*z  # Ixz
-            I[5] = m*y*z  # Iyz
-        ###
-        return I
-            
-    def sumForces(self):
-        for key,loadCase in self.loads.items():
-            F = array([0.,0.,0.])
-            #print "loadCase = ",loadCase
-            for load in loadCase:
-                #print "load = ",load
-                if isinstance(load,Force):
-                    f = load.mag*load.xyz
-                    print "f = ",f
-                    F += f
-                ###
-            self.log.info("case=%s F=%s\n\n" %(key,F))
-        ###
-
-    def sumMoments(self):
-        p = array([0.,0.5,0.])
-        for key,loadCase in self.loads.items():
-            M = array([0.,0.,0.])
-            F = array([0.,0.,0.])
-            #print "loadCase = ",loadCase
-            for load in loadCase:
-                #print "load = ",load
-                if isinstance(load,Force):
-                    f = load.mag*load.xyz
-                    node = self.Node(load.node)
-                    #print "node = ",node
-                    r = node.Position() - p
-                    m = cross(r,f)
-                    #print "m    = ",m
-                    M += m
-                    F += f
-                elif isinstance(load,Moment):
-                    m = load.mag*load.xyz
-                    M += m
-                ###
-            print "case=%s F=%s M=%s\n\n" %(key,F,M)
-        ###
+    ###
 
 class addMethods(object):
     def __init__(self):
