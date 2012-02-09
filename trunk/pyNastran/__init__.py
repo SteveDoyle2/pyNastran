7268ff46c61b193583b23dea1ddd8550ab55a991
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-02-09 05:58:48 +0000

    more trivial updates

--- trunk/pyNastran/__init__.py
+++ trunk/pyNastran/__init__.py
@@ -21,7 +21,7 @@ else:
 __author__  = "Steven Doyle"
 __email__   = 'mesheb82@gmail.com'
 __desc__    = 'Nastran BDF & OP2 File reader/editor/writer/viewer'
-__copyright__   = 'Copyright 2011-2012, The pyNastran Project'
+__copyright__   = 'Copyright 2011-2012, pyNastran; %s' %(__author__)
 __license__     = 'LGPLv3'
 __releaseDate__ = '2011/2/??'
 __version__     = '0.3.0.r%s' %(revision)
