cd391d5224f6f48e6701ca8946845768c7b2e4f9
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-03-16 03:40:28 +0000

    split f06.py

---
+++ trunk/pyNastran/f06/tables/oes.py
@@ -0,0 +1,335 @@
+from pyNastran.op2.tables.oes.oes_solids        import solidStressObject
+
+class StressObject(object):
+    def __init__(self,iSubcase,data):
+        self.iSubcase = iSubcase
+        self.grids = []
+        #self.gridTypes = []
+        #self.translations = []
+        #self.rotations = []
+        
+        self.data = []
+        self.addData(data)
+        
+    def addData(self,data):
+        for line in data:
+            self.data.append(line)
+        return
+            #(gridID,gridType,t1,t2,t3,t4,t5,t6) = line
+            #self.grids.append(gridID)
+            #self.gridTypes.append(gridType)
+            #self.translations.append([t1,t2,t3])
+            #self.rotations.append([t4,t5,t6])
+        ###
+        #print "grids = ",self.grids
+    
+    def __repr__(self):
+        msg  = 'Composite Shell Element Stress\n'
+        msg += "iSubcase = %s\n" %(self.iSubcase)
+        for line in self.data:
+            msg += '%s\n' %(line)
+        return msg
+            
+        msg += '%-8s %8s %10s %10s %10s %10s %10s %10s\n' %('gridID','gridType','t1','t2','t3','t4','t5','t6')
+        for (gridID,gridType,translation,rotation) in zip(self.grids,self.gridTypes,self.translations,self.rotations):
+            (t1,t2,t3) = translation
+            (t4,t5,t6) = rotation
+            msg += "%-8i %8s %10.4g %10.4g %10.4g %10.4g %10.4g %10.4g\n" %(gridID,gridType,t1,t2,t3,t4,t5,t6)
+        ###
+        return msg
+
+class IsoStressObject(object):
+    def __init__(self,iSubcase,data,isFiberDistance,isVonMises):
+        self.iSubcase = iSubcase
+        self.grids = []
+        self.isFiberDistance = isFiberDistance
+        self.isVonMises = isVonMises
+
+        self.data = []
+        self.addData(data)
+        
+    def addData(self,data):        
+        for line in data:
+            self.data.append(line)
+        return
+    
+    def __repr__(self):
+        msg  = 'Isotropic Shell Element Stress\n'
+        msg += "iSubcase = %s\n" %(self.iSubcase)
+        for line in self.data:
+            msg += '%s\n' %(line)
+        return msg
+
+class BarStressObject(object):
+    def __init__(self,iSubcase,data):
+        self.iSubcase = iSubcase
+        self.grids = []
+
+        self.data = []
+        self.addData(data)
+        
+    def addData(self,data):        
+        for line in data:
+            self.data.append(line)
+        return
+
+    def __repr__(self):
+        msg  = 'Bar Element Stress\n'
+        msg += "iSubcase = %s\n" %(self.iSubcase)
+        for line in self.data:
+            msg += '%s\n' %(line)
+        return msg
+
+
+class OES(object):
+    def __init__(self):
+        self.stress = {}
+        self.isoStress = {}
+        self.barStress = {}
+        self.solidStress = {}
+
+    def barStress(self):
+        """
+                                       S T R E S S E S   I N   B A R   E L E M E N T S          ( C B A R )
+        ELEMENT        SA1            SA2            SA3            SA4           AXIAL          SA-MAX         SA-MIN     M.S.-T
+          ID.          SB1            SB2            SB3            SB4           STRESS         SB-MAX         SB-MIN     M.S.-C
+             12    0.0            0.0            0.0            0.0            1.020730E+04   1.020730E+04   1.020730E+04 
+                   0.0            0.0            0.0            0.0                           1.020730E+04   1.020730E+04 
+        """
+        (subcaseName,iSubcase,transient,analysisCode) = self.readSubcaseNameID()
+        headers = self.skip(2)
+        print "headers = %s" %(headers)
+        
+        #isFiberDistance = False
+        #isVonMises = False  # Von Mises/Max Shear
+        #if 'DISTANCE' in headers:
+        #    isFiberDistance = True
+        #if 'VON MISES' in headers:
+        #    isVonMises = True
+
+        data = self.readBarStress()
+        if iSubcase in self.barStress:
+            self.barStress[iSubcase].addData(data)
+        else:
+            self.barStress[iSubcase] = BarStressObject(iSubcase,data)
+        self.iSubcases.append(iSubcase)
+    
+    def readBarStress(self):
+        """
+        ELEMENT        SA1            SA2            SA3            SA4           AXIAL          SA-MAX         SA-MIN     M.S.-T
+          ID.          SB1            SB2            SB3            SB4           STRESS         SB-MAX         SB-MIN     M.S.-C
+             12    0.0            0.0            0.0            0.0            1.020730E+04   1.020730E+04   1.020730E+04 
+                   0.0            0.0            0.0            0.0                           1.020730E+04   1.020730E+04 
+        """
+        data = []
+        while 1:
+            line = self.infile.readline()[1:].strip().split()
+            if 'PAGE' in line:
+                break
+            print line
+            sline = self.parseLine(line,[int,float,float,float,float, float, float,float,float]) # line 1
+            sline = ['CBAR']+sline
+            #data.append(sline)
+            line = self.infile.readline()[1:].strip().split()
+            sline += self.parseLine(line,[    float,float,float,float,        float,float,float]) # line 2
+            data.append(sline)
+            self.i+=2
+            ###
+        ###
+        #print "--------"
+        #for line in data:
+        #    print line
+        #sys.exit()
+        return data
+
+    def quadCompositeStress(self):
+        """
+                       S T R E S S E S   I N   L A Y E R E D   C O M P O S I T E   E L E M E N T S   ( Q U A D 4 )
+        ELEMENT  PLY  STRESSES IN FIBER AND MATRIX DIRECTIONS    INTER-LAMINAR  STRESSES  PRINCIPAL STRESSES (ZERO SHEAR)      MAX
+          ID      ID    NORMAL-1     NORMAL-2     SHEAR-12     SHEAR XZ-MAT  SHEAR YZ-MAT  ANGLE    MAJOR        MINOR        SHEAR
+            181    1   3.18013E+04  5.33449E+05  1.01480E+03   -7.06668E+01  1.90232E+04   89.88  5.33451E+05  3.17993E+04  2.50826E+05
+            181    2   1.41820E+05  1.40805E+05  1.25412E+05   -1.06000E+02  2.85348E+04   44.88  2.66726E+05  1.58996E+04  1.25413E+05
+        """
+        (subcaseName,iSubcase,transient,analysisCode) = self.readSubcaseNameID()
+        headers = self.skip(2)
+        #print "headers = %s" %(headers)
+        data = self.readTable([int,int,float,float,float,float,float,float,float,float,float])
+        if iSubcase in self.stress:
+            self.stress[iSubcase].addData(data)
+        else:
+            self.stress[iSubcase] = StressObject(iSubcase,data)
+        self.iSubcases.append(iSubcase)
+        #print self.stress[iSubcase]
+        
+    def triStress(self):
+        """
+                                 S T R E S S E S   I N   T R I A N G U L A R   E L E M E N T S   ( T R I A 3 )
+        ELEMENT      FIBER               STRESSES IN ELEMENT COORD SYSTEM             PRINCIPAL STRESSES (ZERO SHEAR)                 
+          ID.       DISTANCE           NORMAL-X       NORMAL-Y      SHEAR-XY       ANGLE         MAJOR           MINOR        VON MISES
+              8   -1.250000E-01     -1.303003E+02   1.042750E+04  -1.456123E+02   -89.2100    1.042951E+04   -1.323082E+02   1.049629E+04
+                   1.250000E-01     -5.049646E+02   1.005266E+04  -2.132942E+02   -88.8431    1.005697E+04   -5.092719E+02   1.032103E+04
+        """
+        (subcaseName,iSubcase,transient,analysisCode) = self.readSubcaseNameID()
+        headers = self.skip(2)
+        #print "headers = %s" %(headers)
+        
+        isFiberDistance = False
+        isVonMises = False  # Von Mises/Max Shear
+        if 'DISTANCE' in headers:
+            isFiberDistance = True
+        if 'VON MISES' in headers:
+            isVonMises = True
+
+        data = self.readTriStress()
+        if iSubcase in self.isoStress:
+            self.isoStress[iSubcase].addData(data)
+        else:
+            self.isoStress[iSubcase] = IsoStressObject(iSubcase,data,isFiberDistance,isVonMises)
+        self.iSubcases.append(iSubcase)
+
+    def readTriStress(self):
+        """
+                ELEMENT      FIBER               STRESSES IN ELEMENT COORD SYSTEM             PRINCIPAL STRESSES (ZERO SHEAR)                 
+                  ID.       DISTANCE           NORMAL-X       NORMAL-Y      SHEAR-XY       ANGLE         MAJOR           MINOR        VON MISES
+                      8   -1.250000E-01     -1.303003E+02   1.042750E+04  -1.456123E+02   -89.2100    1.042951E+04   -1.323082E+02   1.049629E+04
+                           1.250000E-01     -5.049646E+02   1.005266E+04  -2.132942E+02   -88.8431    1.005697E+04   -5.092719E+02   1.032103E+04
+        """
+        data = []
+        while 1:
+            line = self.infile.readline()[1:].strip().split()
+            if 'PAGE' in line:
+                break
+            print line
+            sline = self.parseLine(line,[int,float, float,float,float, float,float,float, float]) # line 1
+            sline = ['CTRIA3']+sline
+            data.append(sline)
+            line = self.infile.readline()[1:].strip().split()
+            sline = self.parseLine(line,[    float, float,float,float, float,float,float, float]) # line 2
+            data.append(sline)
+            self.i+=2
+            ###
+        ###
+        return data
+
+    def quadStress(self):
+        """
+                             S T R E S S E S   I N   Q U A D R I L A T E R A L   E L E M E N T S   ( Q U A D 4 )        OPTION = BILIN
+
+        ELEMENT              FIBER            STRESSES IN ELEMENT COORD SYSTEM         PRINCIPAL STRESSES (ZERO SHEAR)
+          ID      GRID-ID   DISTANCE        NORMAL-X      NORMAL-Y      SHEAR-XY      ANGLE        MAJOR         MINOR       VON MISES
+              6    CEN/4  -1.250000E-01  -4.278394E+02  8.021165E+03 -1.550089E+02   -88.9493   8.024007E+03 -4.306823E+02  8.247786E+03
+                           1.250000E-01   5.406062E+02  1.201854E+04 -4.174177E+01   -89.7916   1.201869E+04  5.404544E+02  1.175778E+04
+
+                       4  -1.250000E-01  -8.871141E+02  7.576036E+03 -1.550089E+02   -88.9511   7.578874E+03 -8.899523E+02  8.060780E+03
+                           1.250000E-01  -8.924081E+01  1.187899E+04 -4.174177E+01   -89.8002   1.187913E+04 -8.938638E+01  1.192408E+04
+        """
+        (subcaseName,iSubcase,transient,analysisCode) = self.readSubcaseNameID()
+        headers = self.skip(3)
+        #print "headers = %s" %(headers)
+        
+        isFiberDistance = False
+        isVonMises = False  # Von Mises/Max Shear
+        if 'DISTANCE' in headers:
+            isFiberDistance = True
+        if 'VON MISES' in headers:
+            isVonMises = True
+
+        data = self.readQuadBilinear()
+        if iSubcase in self.isoStress:
+            self.isoStress[iSubcase].addData(data)
+        else:
+            self.isoStress[iSubcase] = IsoStressObject(iSubcase,data,isFiberDistance,isVonMises)
+        self.iSubcases.append(iSubcase)
+
+    def readQuadBilinear(self):
+        data = []
+        while 1:
+            if 1: # CEN/4
+                line = self.infile.readline()[1:].strip().split()
+                if 'PAGE' in line:
+                    return data
+                sline = self.parseLine(line,[int,str,float, float,float,float, float,float,float, float]) # line 1
+                sline = ['CQUAD4']+sline
+                data.append(sline)
+                line = self.infile.readline()[1:].strip().split()
+                sline = self.parseLine(line,[        float, float,float,float, float,float,float, float]) # line 2
+                data.append(sline)
+                line = self.infile.readline() # blank line
+                self.i+=3
+            ###
+            for i in range(4):
+                line = self.infile.readline()[1:].strip().split()
+                sline = self.parseLine(line,[int,float, float,float,float, float,float,float, float]) # line 1
+                data.append(sline)
+                line = self.infile.readline()[1:].strip().split()
+                sline = self.parseLine(line,[    float, float,float,float, float,float,float, float]) # line 2
+                data.append(sline)
+                line = self.infile.readline() # blank line
+                self.i+=3
+            ###
+        ###
+        return data
+
+    def solidStressHexa(self):
+        return self.readSolidStress('CHEXA',8)
+    def solidStressPenta(self):
+        return self.readSolidStress('CPENTA',6)
+    def solidStressTetra(self):
+        return self.readSolidStress('CTETRA',4)
+
+    def readSolidStress(self,eType,n):
+        """
+        analysisCode = 1 (Statics)
+        deviceCode   = 1 (Print)
+        tableCode    = 5 (Stress/Strain)
+        sortCode     = 0 (Sort2,Real,Sorted Results) => sortBits = [0,0,0]
+        formatCode   = 1 (Real)
+        sCode        = 0 (Stress)
+        numWide      = 8 (???)
+        """
+        (subcaseName,iSubcase,transient,analysisCode) = self.readSubcaseNameID()
+        headers = self.skip(2)
+        #print "headers = %s" %(headers)
+
+        isMaxShear = True
+        if 'VON MISES' in headers:
+            isMaxShear = False
+            
+        data = self.read3DStress(eType,n)
+        stressBits = self.makeStressBits(isMaxShear=False)
+        dataCode = {'log':self.log,'analysisCode':1,'deviceCode':1,'tableCode':5,
+                    'sortCode':0,'sortBits':[0,0,0],'numWide':8,'elementName':eType,'formatCode':1,
+                    'sCode':0,'stressBits':stressBits}
+
+        if iSubcase in self.solidStress:
+            self.solidStress[iSubcase].addF06Data(data,transient)
+        else:
+            #self.solidStress[iSubcase] = SolidStressObject(iSubcase,data)
+            self.solidStress[iSubcase] = solidStressObject(dataCode,iSubcase,transient)
+            self.solidStress[iSubcase].addF06Data(data,transient)
+        self.iSubcases.append(iSubcase)
+
+    def makeStressBits(self,isMaxShear,isFiberDistance=True):
+        stressBits = [0,0,0]
+        if isMaxShear==False:
+            stressBits[0] = 1
+        
+        if isFiberDistance:
+            stressBits[2] = 1
+        return stressBits
+        
+    def read3DStress(self,eType,n):
+        data = []
+        while 1:
+            line = self.infile.readline().rstrip('\n\r') #[1:]
+                    #              CENTER         X          #          XY             #        A         #
+            sline = [line[1:17],line[17:24],line[24:28],line[28:43],line[43:47],line[47:63],line[63:66],line[66:80],  line[80:83],line[83:88],line[88:93],line[93:98],line[99:113],line[113:130]]
+            sline = [s.strip() for s in sline]
+            if 'PAGE' in line:
+                break
+            elif '' is not sline[0]:
+                sline = [eType]+sline
+            data.append(sline)
+        ###
+        return data
+        
---
+++ trunk/pyNastran/f06/tables/oug.py
@@ -0,0 +1,96 @@
+from pyNastran.op2.tables.oug.oug_displacements import displacementObject
+from pyNastran.op2.tables.oug.oug_temperatures  import temperatureObject
+
+class OUG(object):
+    def __init__(self):
+        pass
+
+    def displacement(self):
+        """
+                                             D I S P L A C E M E N T   V E C T O R
+ 
+        POINT ID.   TYPE          T1             T2             T3             R1             R2             R3
+               1      G      9.663032E-05   0.0           -2.199001E-04   0.0           -9.121119E-05   0.0
+               2      G      0.0            0.0            0.0            0.0            0.0            0.0
+               3      G      0.0            0.0            0.0            0.0            0.0            0.0
+               
+        analysisCode = 1 (Statics)
+        deviceCode   = 1 (Print)
+        tableCode    = 1 (Displacement)
+        sortCode     = 0 (Sort2,Real,Sorted Results) => sortBits = [0,0,0]
+        numWide      = 8 (???)
+        """
+        (subcaseName,iSubcase,transient,analysisCode) = self.readSubcaseNameID()
+        headers = self.skip(2)
+        dataCode = {'log':self.log,'analysisCode':analysisCode,'deviceCode':1,'tableCode':1,
+                    'sortCode':0,'sortBits':[0,0,0],'numWide':8}
+        #print "headers = %s" %(headers)
+        data = self.readTable([int,str,float,float,float,float,float,float])
+        if iSubcase in self.disp:
+            self.disp[iSubcase].addF06Data(data,transient)
+        else:
+            #self.disp[iSubcase] = DisplacementObject(iSubcase,data)
+            disp = displacementObject(dataCode,iSubcase)
+            disp.addF06Data(data,transient)
+            self.disp[iSubcase] = disp
+        self.iSubcases.append(iSubcase)
+
+    def temperatureVector(self):
+        """
+        LOAD STEP =  1.00000E+00
+                                              T E M P E R A T U R E   V E C T O R
+ 
+        POINT ID.   TYPE      ID   VALUE     ID+1 VALUE     ID+2 VALUE     ID+3 VALUE     ID+4 VALUE     ID+5 VALUE
+               1      S      1.300000E+03   1.300000E+03   1.300000E+03   1.300000E+03   1.300000E+03   1.300000E+03
+               7      S      1.300000E+03   1.300000E+03   1.300000E+03   1.300000E+03
+        analysisCode = 1 (Statics)
+        deviceCode   = 1 (Print)
+        tableCode    = 1 (Displacement/Temperature)
+        sortCode     = 0 (Sort2,Real,Sorted Results) => sortBits = [0,0,0]
+        formatCode   = 1 (Real)
+        sCode        = 0 (Stress)
+        numWide      = 8 (???)
+        """
+        (subcaseName,iSubcase,transient,analysisCode) = self.readSubcaseNameID()
+        #print transient
+        
+        headers = self.skip(2)
+        #print "headers = %s" %(headers)
+        data = self.readTemperatureTable()
+        
+        dataCode = {'log':self.log,'analysisCode':1,'deviceCode':1,'tableCode':5,'sortCode':0,
+                    'sortBits':[0,0,0],'numWide':8,
+                    #'formatCode':1,
+                    #'elementName':eType,'sCode':0,'stressBits':stressBits
+                    }
+
+        if iSubcase in self.temperature:
+            self.temperature[iSubcase].addF06Data(data,transient)
+        else:
+            temp = temperatureObject(dataCode,iSubcase)
+            temp.addF06Data(data,transient)
+            self.temperature[iSubcase] = temp
+        self.iSubcases.append(iSubcase)
+
+    def readTemperatureTable(self):
+        data = []
+        Format = [int,str,float,float,float,float,float,float]
+        while 1:
+            line = self.infile.readline()[1:].rstrip('\r\n ')
+            if 'PAGE' in line:
+                return data
+            sline = [line[0:15],line[15:22].strip(),line[22:40],line[40:55],line[55:70],line[70:85],line[85:100]]
+            sline = self.parseLineTemperature(sline,Format)
+            data.append(sline)
+        return data
+
+    def parseLineTemperature(self,sline,Format):
+        out = []
+        for entry,iFormat in zip(sline,Format):
+            if entry is '':
+                return out
+            #print "sline=|%r|\n entry=|%r| format=%r" %(sline,entry,iFormat)
+            entry2 = iFormat(entry)
+            out.append(entry2)
+        return out
+    
