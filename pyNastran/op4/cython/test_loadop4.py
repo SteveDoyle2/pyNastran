6280af596d90970f1e2859e6a0933789deae6965
al.danial@gmail.com <al.danial@gmail.com@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-03-30 05:21:43 +0000

    Enable loading of dense matrices from binary files. Note:  endian is hardwired to native; need to fix at some point.

--- pyNastran/op4/cython/test_loadop4.py
+++ pyNastran/op4/cython/test_loadop4.py
@@ -3,11 +3,6 @@
 import numpy as np
 import op4
 
-type_map = { 1 : 'RS' ,   # real    single precision
-             2 : 'RD' ,   # real    double precision
-             3 : 'CS' ,   # complex single precision
-             4 : 'CD' ,}  # complex double precision
-
 """
 for in_file in [ '../test/mat_b_dn.op4' ,
                  '../test/mat_b_s2.op4' ,
@@ -16,36 +11,26 @@ for in_file in [ '../test/mat_b_dn.op4' ,
                  '../test/mat_t_s1.op4' ,
                  '../test/mat_t_s2.op4' , ]:
 """
-for in_file in [ '../test/mat_t_dn.op4' ]:
+for in_file in [ '../test/mat_b_dn.op4' ,
+                 '../test/mat_t_dn.op4' ]:
     try:
         fh = op4.File(in_file, 'r')
     except:
         print('Failed to get header of %s' % (in_file))
         raise SystemExit
 
-
     print('%s' % ('=' * 61))
     print('%s' % (in_file))
-    print('    %-8s %5s %5s %8s %8s %2s %2s %2s %9s' % (
-          'Name', 'nRow', 'nCol', 'nStr', 'nNnz', 'T', 'Fr', 'Dg', 'Offset'))
-    for i in range(fh['nMat']):
-        print('%2d. %-8s %5d %5d %8d %8d %2s %2d %2d %9d' % (
-               i+1          ,
-               fh['name'][i],
-               fh['nRow'][i],
-               fh['nCol'][i],
-               fh['nStr'][i],
-               fh['nNnz'][i],
-               type_map[fh['type'][i]],
-               fh['form'][i],
-               fh['digits'][i],
-               fh['offset'][i],))
+    op4.print_header(fh)
 
     for i in range(fh['nMat']):
-        a = op4.Load(fh, n_mat=1, n_skip=i)  # n_mat not yet working
-        print('%2d. %-8s has type %s, contents of' % (
-              i+1, fh['name'][i], type(a[0,0])))
-        print(a)
+        a = op4.Load(fh, nmat=1, skip=i)  # n_mat not yet working
+        if a is None: 
+            print('op4.Load returned None')
+        else:
+            print('%2d. %-8s has type %s, contents of' % (
+                  i+1, fh['name'][i], type(a[0,0])))
+            print(a)
 
 # print 'It carries a reference to our deallocator: %s ' % a.base
 # np.testing.assert_allclose(a, np.arange(10))
