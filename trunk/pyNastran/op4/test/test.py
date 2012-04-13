1c5e3cd0790af88a637bdb6b5b6005bd481fd5fd
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-04-13 08:26:57 +0000

    added test for op4

---
+++ trunk/pyNastran/op4/test/test.py
@@ -0,0 +1,49 @@
+import pyNastran.op4.cython.op4 as op4
+#print "f = ",op4.__file__
+
+
+def failed_test1():
+    fh = op4.File(r'C:\Users\steve\Desktop\pyNastran\pyNastran\op4\test\mat_b_dn.op4','r')
+    fh.print_header()
+    #print fh.nmat = 9
+    
+    # crash
+    a,b,c = fh.Load(nmat=3,skip=0)
+    print a
+    print b
+    print c
+
+def pass_test1():
+    fh = op4.File('mat_b_dn.op4','r')
+    fh.print_header()
+    #print fh.nmat = 9
+    
+    a,b,c = fh.Load(nmat=3,skip=0)
+    print a
+    print b
+    print c
+
+def failed_test2():
+    fh = op4.File(r'C:\Users\steve\Desktop\pyNastran\pyNastran\op4\test\mat_b_dn.op4','r')
+    #print fh.nmat = 9
+
+    # crash with "unnamed is sparse, skipping for now"
+    (a,b,c,d, f,g,h,i) = fh.Load(nmat=9,skip=0) 
+    print a
+    print b
+    print c
+
+def pass_test2():
+    fh = op4.File('mat_b_dn.op4','r')
+    #print fh.nmat = 9
+
+    # ValueError:  need more than 8 values to unpack
+    (a,b,c,d, f,g,h,i) = fh.Load(nmat=9,skip=0) 
+    print a
+    print b
+    print c
+
+print "-------------"
+failed_test1()
+print "*********"
+failed_test2()
