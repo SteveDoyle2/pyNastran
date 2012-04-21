510d28e62e48e88525934a201da9260b779b4d97
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-04-21 18:44:46 +0000

    missed a file

---
+++ trunk/pyNastran/f06/tables/oqg.py
@@ -0,0 +1,45 @@
+from pyNastran.op2.tables.oqg_constraintForces.oqg_spcForces import spcForcesObject #,complexSpcForcesObject
+from pyNastran.op2.tables.oqg_constraintForces.oqg_mpcForces import mpcForcesObject #,complexMpcForcesObject
+
+
+class OQG(object):
+    def __init__(self):
+        self.spcForces = {}
+        self.mpcForces = {}
+
+    def getSpcForces(self):
+        (subcaseName,iSubcase,transient,analysisCode,isSort1) = self.readSubcaseNameID()
+        headers = self.skip(2)
+        #print "headers = %s" %(headers)
+
+        data = self.readTable([int,str,float,float,float,float,float,float])
+
+        dataCode = {'log':self.log,'analysisCode':analysisCode,'deviceCode':1,'tableCode':3,
+                    'sortCode':0,'sortBits':[0,0,0],'numWide':8,'tableName':'OQG',}
+
+        if iSubcase in self.spcForces:
+            self.spcForces[iSubcase].addF06Data(data,transient)
+        else:
+            spc = spcForcesObject(dataCode,iSubcase)
+            spc.addF06Data(data,transient)
+            self.spcForces[iSubcase] = spc
+        self.iSubcases.append(iSubcase)
+
+    def getMpcForces(self):
+        (subcaseName,iSubcase,transient,analysisCode,isSort1) = self.readSubcaseNameID()
+        headers = self.skip(2)
+        #print "headers = %s" %(headers)
+
+        data = self.readTable([int,str,float,float,float,float,float,float])
+
+        dataCode = {'log':self.log,'analysisCode':analysisCode,'deviceCode':1,'tableCode':39,
+                    'sortCode':0,'sortBits':[0,0,0],'numWide':8,'tableName':'OQG',}
+
+        if iSubcase in self.mpcForces:
+            self.mpcForces[iSubcase].addF06Data(data,transient)
+        else:
+            mpc = mpcForcesObject(dataCode,iSubcase)
+            mpc.addF06Data(data,transient)
+            self.mpcForces[iSubcase] = mpc
+        self.iSubcases.append(iSubcase)
+
