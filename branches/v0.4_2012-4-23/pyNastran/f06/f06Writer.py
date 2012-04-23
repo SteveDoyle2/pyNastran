6b26dbfcc364b9dbb0d9fdbc3a0001872df4b0ae
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-04-23 06:52:03 +0000

    commenting print statement

--- branches/v0.4_2012-4-23/pyNastran/f06/f06Writer.py
+++ branches/v0.4_2012-4-23/pyNastran/f06/f06Writer.py
@@ -214,7 +214,7 @@ class F06Writer(object):
                     if resType.has_key(iSubcase):
                         result = resType[iSubcase]
                         try:
-                            print result.__class__.__name__
+                            #print result.__class__.__name__
                             (msg,pageNum) = result.writeF06(header,pageStamp,pageNum=pageNum)
                         except:
                             #print "result name = %s" %(result.name())
