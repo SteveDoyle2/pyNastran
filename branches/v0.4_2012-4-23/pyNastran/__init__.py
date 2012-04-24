d61af55179bb5d846268664e3f60fbd1d2ae404f
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-04-24 01:58:44 +0000

    fixing typo, bumping again

--- branches/v0.4_2012-4-23/pyNastran/__init__.py
+++ branches/v0.4_2012-4-23/pyNastran/__init__.py
@@ -5,5 +5,5 @@ __copyright__   = 'Copyright 2011-2012, pyNastran; %s' %(__author__)
 __license__     = 'LGPLv3'
 __releaseDate__ = '2012/4/23'
 __releaseDate2__ = 'APRIL 23, 2012'
-__version__     = '0.4.0.r673' %(revision)
+__version__     = '0.4.0.r674'
 __website__     = 'http://code.google.com/p/pynastran/'
