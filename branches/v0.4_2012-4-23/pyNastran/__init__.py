f4cdacf626362f8bc2eab6b4591c6726681413c0
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-04-24 01:56:25 +0000

    updating version

--- branches/v0.4_2012-4-23/pyNastran/__init__.py
+++ branches/v0.4_2012-4-23/pyNastran/__init__.py
@@ -1,5 +1,3 @@
-revision = 'dev'
-
 __author__  = 'Steven Doyle'
 __email__   = 'mesheb82@gmail.com'
 __desc__    = 'Nastran BDF & OP2 File reader/editor/writer/viewer'
@@ -7,5 +5,5 @@ __copyright__   = 'Copyright 2011-2012, pyNastran; %s' %(__author__)
 __license__     = 'LGPLv3'
 __releaseDate__ = '2012/4/23'
 __releaseDate2__ = 'APRIL 23, 2012'
-__version__     = '0.4.0.r665' %(revision)
+__version__     = '0.4.0.r673' %(revision)
 __website__     = 'http://code.google.com/p/pynastran/'
