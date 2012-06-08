5a1ecc8d635c930bb5431beb879a6b296d5fc488
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-06-08 07:24:24 +0000

    fixed binary reader for cart3d

--- pyNastran/converters/cart3d/cart3dIO.py
+++ pyNastran/converters/cart3d/cart3dIO.py
@@ -5,7 +5,7 @@ from numpy import zeros
 import vtk
 from vtk import vtkTriangle
 
-from pyNastran.converters.cart3d.cart3d_reader import Cart3DReader
+from pyNastran.converters.cart3d.cart3d_reader import genericCart3DReader
 
 class Cart3dIO(object):
     def __init__(self):
@@ -57,7 +57,7 @@ class Cart3dIO(object):
         if skipReading:
             return
 
-        model = Cart3DReader()
+        model = genericCart3DReader(cart3dFileName)
         self.modelType = model.modelType
         (nodes,elements,regions,loads) = model.readCart3d(cart3dFileName)
         self.nNodes = model.nPoints
