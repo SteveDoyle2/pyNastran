1ff5ab1c4acfc7d731868d36e74df9570c7efa3d
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-07-14 23:12:16 +0000

    starting python to dmap parser

---
+++ trunk/pyNastran/bdf/dmap/spike.py
@@ -0,0 +1,14 @@
+a = 1
+b = 2
+c = 3.
+d = 4.
+intval    = a + b
+floatval  = c + d
+floatval2 = a + c
+
+dogv  = 'dog'
+frogv = 'frog'
+frogdogv = frogv + dogv
+
+#A = zeros((2,3),'f')
+#B = ones((2,3),'f')
