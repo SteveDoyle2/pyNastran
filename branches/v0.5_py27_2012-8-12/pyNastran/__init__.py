7412e4150b2be54eafd8d0786c1308be851c1ff5
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-08-13 05:41:47 +0000

    adding file headers

--- branches/v0.5_py27_2012-8-12/pyNastran/__init__.py
+++ branches/v0.5_py27_2012-8-12/pyNastran/__init__.py
@@ -18,12 +18,12 @@ if dev:
 else:
     revision = 'dev877'
 
-__author__  = 'Steven Doyle, Al Danials'
+__author__  = 'Steven Doyle, Al Danial'
 __email__   = 'mesheb82@gmail.com'
 __desc__    = 'Nastran BDF/F06/OP2/OP4 File reader/editor/writer/viewer'
 __copyright__   = 'Copyright 2011-2012, pyNastran; %s' %(__author__)
 __license__     = 'LGPLv3'
-__releaseDate__ = '2012/8/??'
-__releaseDate2__ = 'AUGUST ??, 2012'
+__releaseDate__ = '2012/8/13'
+__releaseDate2__ = 'AUGUST 13, 2012'
 __version__     = '0.5.r%s' %(revision)
 __website__     = 'http://code.google.com/p/pynastran/'
