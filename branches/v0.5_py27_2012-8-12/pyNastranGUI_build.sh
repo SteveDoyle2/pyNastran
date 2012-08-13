8bbb1c570d2ea0d6dbf6e6261c1ac0f0a7bd050b
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-08-13 05:46:12 +0000

    removed spec file

--- branches/v0.5_py27_2012-8-12/pyNastranGUI_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#python pyinstaller-pyinstaller-67b940c/pyinstaller.py ../pyNastran/pyNastran/gui/gui.py
-rm -rf build dist
-python pyinstaller-pyinstaller-67b940c/pyinstaller.py gui.spec
-#dist/pywin27/gui.exe
-#dist/gui/gui.exe
-
