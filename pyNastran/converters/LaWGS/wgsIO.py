6973ec05d3668404f922698a8d5407a9428028aa
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-08-16 02:31:16 +0000

    applying autopep8

--- pyNastran/converters/LaWGS/wgsIO.py
+++ pyNastran/converters/LaWGS/wgsIO.py
@@ -2,6 +2,7 @@ import vtk
 from vtk import vtkQuad
 from wgsReader import LaWGS
 
+
 class LaWGS_IO(object):
     def __init__(self):
         pass
@@ -9,12 +10,12 @@ class LaWGS_IO(object):
 #    lawgs = LaWGS('tmx1242.wgs')
 #    lawgs.run()
 
-    def loadLaWGSGeometry(self,lawgsFileName,dirname,isNodal,isCentroidal):
+    def loadLaWGSGeometry(self, lawgsFileName, dirname, isNodal, isCentroidal):
         self.isNodal = isNodal
         self.isCentroidal = isCentroidal
         #key = self.caseKeys[self.iCase]
         #case = self.resultCases[key]
-        
+
         skipReading = self.removeOldGeometry(lawgsFileName)
         if skipReading:
             return
@@ -22,13 +23,13 @@ class LaWGS_IO(object):
         model = LaWGS(lawgsFileName)
         self.modelType = model.modelType
         model.readLaWGS()
-        
-        nodes,elements = model.getPointsElements()
+
+        nodes, elements = model.getPointsElements()
         self.nNodes = len(nodes)
         self.nElements = len(elements)
 
         #print "nNodes = ",self.nNodes
-        print "nElements = ",self.nElements
+        print "nElements = ", self.nElements
 
         self.grid.Allocate(self.nElements, 1000)
         self.gridResult.SetNumberOfComponents(self.nElements)
@@ -36,15 +37,15 @@ class LaWGS_IO(object):
 
         points = vtk.vtkPoints()
         points.SetNumberOfPoints(self.nNodes)
-        self.gridResult.Allocate(self.nNodes,1000)
+        self.gridResult.Allocate(self.nNodes, 1000)
         #vectorReselt.SetNumberOfComponents(3)
         self.nidMap = {}
         #elem.SetNumberOfPoints(nNodes)
         if 0:
-            fraction = 1./self.nNodes # so you can color the nodes by ID
-            for nid,node in sorted(nodes.iteritems()):
-                points.InsertPoint(nid-1, *node)
-                self.gridResult.InsertNextValue(nid*fraction)
+            fraction = 1. / self.nNodes  # so you can color the nodes by ID
+            for nid, node in sorted(nodes.iteritems()):
+                points.InsertPoint(nid - 1, *node)
+                self.gridResult.InsertNextValue(nid * fraction)
                 #print str(element)
 
                 #elem = vtk.vtkVertex()
@@ -52,12 +53,12 @@ class LaWGS_IO(object):
                 #self.aQuadGrid.InsertNextCell(elem.GetCellType(), elem.GetPointIds())
                 #vectorResult.InsertTuple3(0, 0.0, 0.0, 1.0)
 
-        for nid,node in enumerate(nodes):
+        for nid, node in enumerate(nodes):
             points.InsertPoint(nid, *node)
-        print "nid = ",nid
+        print "nid = ", nid
 
-        for eid,element in enumerate(elements):
-            (p1,p2,p3,p4) = element
+        for eid, element in enumerate(elements):
+            (p1, p2, p3, p4) = element
             #print "element = ",element
             elem = vtkQuad()
             elem.GetPointIds().SetId(0, p1)
@@ -66,7 +67,7 @@ class LaWGS_IO(object):
             elem.GetPointIds().SetId(3, p4)
             self.grid.InsertNextCell(elem.GetCellType(), elem.GetPointIds())
         ###
-        print "eid = ",eid
+        print "eid = ", eid
         self.grid.SetPoints(points)
         #self.grid2.SetPoints(points2)
         #self.grid.GetPointData().SetScalars(self.gridResult)
@@ -86,26 +87,27 @@ class LaWGS_IO(object):
         self.scalarBar.VisibilityOn()
         self.scalarBar.Modified()
 
-        self.iSubcaseNameMap = {1:['Cart3d','']}
+        self.iSubcaseNameMap = {1: ['Cart3d', '']}
         cases = {}
         ID = 1
-        
+
         #print "nElements = ",nElements
-        cases = self.fillCart3dCase(cases,ID,regions,loads)
+        cases = self.fillCart3dCase(cases, ID, regions, loads)
 
         self.resultCases = cases
         self.caseKeys = sorted(cases.keys())
         #print "caseKeys = ",self.caseKeys
         #print "type(caseKeys) = ",type(self.caseKeys)
         self.iCase = -1
-        self.nCases = len(self.resultCases)-1 # number of keys in dictionary
-        self.cycleResults() # start at nCase=0
+        self.nCases = len(self.resultCases) - 1  # number of keys in dictionary
+        self.cycleResults()  # start at nCase=0
 
-if __name__=='__main__':
+if __name__ == '__main__':
     print ''
+
     def removeOldGeometry(self):
         pass
     test = LaWGS_IO()
     test.removeOldGeometry = removeOldGeometry
 
-    test.loadLaWGSGeometry('tmx1242.wgs','',True,True)
+    test.loadLaWGSGeometry('tmx1242.wgs', '', True, True)
