dd3b3bb221090c5d9983afe4af8d470040d91646
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-08-16 05:54:31 +0000

    applying autopep8

--- pyNastran/op2/op2Errors.py
+++ pyNastran/op2/op2Errors.py
@@ -1,24 +1,30 @@
 class InvalidCodeError(RuntimeError):
     pass
 
+
 class InvalidGridID_Error(ValueError):
     pass
+
 
 class TapeCodeError(InvalidCodeError):
     pass
 
+
 class EndOfFileError(RuntimeError):
     pass
+
 
 class TapeCodeError(RuntimeError):
     pass
 
+
 class ZeroBufferError(RuntimeError):
     pass
+
 
 class InvalidMarkersError(RuntimeError):
     pass
 
+
 class InvalidMarkerError(RuntimeError):
     pass
-
