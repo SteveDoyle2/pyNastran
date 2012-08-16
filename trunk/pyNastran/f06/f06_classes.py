e735cbda932829b30d4da7ff7d6302b2863c4677
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-08-16 02:44:05 +0000

    applying autopep8

--- trunk/pyNastran/f06/f06_classes.py
+++ trunk/pyNastran/f06/f06_classes.py
@@ -1,20 +1,20 @@
 class MaxDisplacement(object):
-    def __init__(self,data):
+    def __init__(self, data):
         self.translations = {}
         self.rotations = {}
         for line in data:
             sid = line[0]
             self.translations[sid] = line[1:4]
-            self.rotations[sid]    = line[4:]
+            self.rotations[sid] = line[4:]
         ###
 
-    def writeF06(self,pageStamp='',pageNum=1):
+    def writeF06(self, pageStamp='', pageNum=1):
         msg = ['0                                                  MAXIMUM  DISPLACEMENTS',
                '  SUBCASE/',
                '  DAREA ID        T1             T2             T3             R1             R2             R3']
-        for sid,trans in sorted(self.translations.iteritems()):
+        for sid, trans in sorted(self.translations.iteritems()):
             rot = self.rotations[sid]
-            msg.append('0 %8i  %13.8E %13.8E %13.8E %13.8E %13.8E %13.8E' %(tuple([sid]+trans+rot)))
-        msg.append(pageStamp+str(pageNum))
+            msg.append('0 %8i  %13.8E %13.8E %13.8E %13.8E %13.8E %13.8E' %
+                       (tuple([sid] + trans + rot)))
+        msg.append(pageStamp + str(pageNum))
         return '\n'.join(msg)
-        
--- trunk/pyNastran/f06/tables/oug.py
+++ trunk/pyNastran/f06/tables/oug.py
@@ -2,7 +2,7 @@
 from itertools import izip
 
 from pyNastran.op2.tables.oug.oug_displacements import DisplacementObject, ComplexDisplacementObject
-from pyNastran.op2.tables.oug.oug_temperatures  import TemperatureObject
+from pyNastran.op2.tables.oug.oug_temperatures import TemperatureObject
 
 
 class OUG(object):
@@ -13,40 +13,42 @@ class OUG(object):
     def getDisplacement(self):
         """
                                              D I S P L A C E M E N T   V E C T O R
- 
+
         POINT ID.   TYPE          T1             T2             T3             R1             R2             R3
                1      G      9.663032E-05   0.0           -2.199001E-04   0.0           -9.121119E-05   0.0
                2      G      0.0            0.0            0.0            0.0            0.0            0.0
                3      G      0.0            0.0            0.0            0.0            0.0            0.0
-               
+
         analysisCode = 1 (Statics)
         deviceCode   = 1 (Print)
         tableCode    = 1 (Displacement)
         sortCode     = 0 (Sort2,Real,Sorted Results) => sortBits = [0,0,0]
         numWide      = 8 (???)
         """
-        (subcaseName, iSubcase, transient, dt, analysisCode, isSort1) = self.readSubcaseNameID()
+        (subcaseName, iSubcase, transient, dt, analysisCode,
+            isSort1) = self.readSubcaseNameID()
         headers = self.skip(2)
-        dataCode = {'log':self.log, 'analysisCode':analysisCode, 
-                    'deviceCode':1, 'tableCode':1,
-                    'sortCode':0, 'sortBits':[0, 0, 0], 'numWide':8,
-                    'tableName':'OUG', 'nonlinearFactor': dt}
+        dataCode = {'log': self.log, 'analysisCode': analysisCode,
+                    'deviceCode': 1, 'tableCode': 1,
+                    'sortCode': 0, 'sortBits': [0, 0, 0], 'numWide': 8,
+                    'tableName': 'OUG', 'nonlinearFactor': dt}
         #print "headers = %s" %(headers)
-        data = self.readTable([int,str,float,float,float,float,float,float])
+        dataTypes = [int, str, float, float, float, float, float, float]
+        data = self.readTable(dataTypes)
 
         if iSubcase in self.displacements:
-            self.displacements[iSubcase].addF06Data(data,transient)
+            self.displacements[iSubcase].addF06Data(data, transient)
         else:
             isSort1 = True
-            disp = DisplacementObject(dataCode,isSort1,iSubcase)
-            disp.addF06Data(data,transient)
+            disp = DisplacementObject(dataCode, isSort1, iSubcase)
+            disp.addF06Data(data, transient)
             self.displacements[iSubcase] = disp
         self.iSubcases.append(iSubcase)
 
-    def getComplexDisplacement(self): # @todo: make a complexDisplacement object
+    def getComplexDisplacement(self):  # @todo: make a complexDisplacement object
         """
-          BACKWARD WHIRL                                                                                                                
-                                                                                                                 SUBCASE 2              
+          BACKWARD WHIRL
+                                                                                                                 SUBCASE 2
           POINT-ID =       101
                                            C O M P L E X   D I S P L A C E M E N T   V E C T O R
                                                              (MAGNITUDE/PHASE)
@@ -54,21 +56,22 @@ class OUG(object):
           FREQUENCY   TYPE          T1             T2             T3             R1             R2             R3
         2.000000E+01     G      3.242295E-16   1.630439E-01   1.630439E-01   1.691497E-17   1.362718E-01   1.362718E-01
                                 196.0668        90.0000       180.0000        63.4349       180.0000       270.0000
-        
+
         tableCode = 1 (Displacement)
         formatCode = 3 (Magnitude/Phase)
         sortBits = [0,1,1]  (Sort1,Real/Imaginary,RandomResponse)
         analysisCode = 5 (Frequency)
         sortCode = 2 (Random Response)
         """
-        (subcaseName, iSubcase, transient, dt, analysisCode, isSort1) = self.readSubcaseNameID()
+        (subcaseName, iSubcase, transient, dt, analysisCode,
+            isSort1) = self.readSubcaseNameID()
         headers = self.skip(3)
         data = []
 
-        dataCode = {'log':self.log, 'analysisCode':5, 'deviceCode':1,
-                    'tableCode':1, 'sortCode':2, 'sortBits':[0, 1, 1],
-                    'numWide':14, 'formatCode':3, 'tableName':'OUGV1',
-                    'nonlinearFactor': dt, 
+        dataCode = {'log': self.log, 'analysisCode': 5, 'deviceCode': 1,
+                    'tableCode': 1, 'sortCode': 2, 'sortBits': [0, 1, 1],
+                    'numWide': 14, 'formatCode': 3, 'tableName': 'OUGV1',
+                    'nonlinearFactor': dt,
                     #'mode':iMode,'eigr':transient[1], 'modeCycle':cycle,
                     #'dataNames':['mode', 'eigr', 'modeCycle'],
                     #'name':'mode',
@@ -82,10 +85,11 @@ class OUG(object):
             sline = line.strip().split()
             line2 = self.infile.readline()[1:].rstrip('\r\n ')
             sline += line2.strip().split()
-            out = [float(sline[0]),sline[1].strip(),float(sline[2]), float(sline[3]), float(sline[4]),
-                                                    float(sline[5]), float(sline[6]), float(sline[7]),
-                                                    float(sline[8]), float(sline[9]), float(sline[10]),
-                                                    float(sline[11]), float(sline[12]), float(sline[13]), ]
+            out = [float(sline[0]), sline[1].strip(),
+                   float(sline[2]), float(sline[3]), float(sline[4]),
+                   float(sline[5]), float(sline[6]), float(sline[7]),
+                   float(sline[8]), float(sline[9]), float(sline[10]),
+                   float(sline[11]), float(sline[12]), float(sline[13]), ]
             #print sline
             #print out
             data.append(out)
@@ -101,12 +105,12 @@ class OUG(object):
             disp.addF06Data(data, transient)
             self.displacements[iSubcase] = disp
         self.iSubcases.append(iSubcase)
-    
+
     def getTemperatureVector(self):
         """
         LOAD STEP =  1.00000E+00
                                               T E M P E R A T U R E   V E C T O R
- 
+
         POINT ID.   TYPE      ID   VALUE     ID+1 VALUE     ID+2 VALUE     ID+3 VALUE     ID+4 VALUE     ID+5 VALUE
                1      S      1.300000E+03   1.300000E+03   1.300000E+03   1.300000E+03   1.300000E+03   1.300000E+03
                7      S      1.300000E+03   1.300000E+03   1.300000E+03   1.300000E+03
@@ -118,16 +122,17 @@ class OUG(object):
         sCode        = 0 (Stress)
         numWide      = 8 (???)
         """
-        (subcaseName, iSubcase, transient, dt, analysisCode, isSort1) = self.readSubcaseNameID()
+        (subcaseName, iSubcase, transient, dt, analysisCode,
+            isSort1) = self.readSubcaseNameID()
         #print transient
-        
+
         headers = self.skip(2)
         #print "headers = %s" %(headers)
         data = self.readTemperatureTable()
-        
-        dataCode = {'log':self.log, 'analysisCode':1, 'deviceCode':1,
-                    'tableCode':1, 'sortCode':0, 'sortBits':[0,0,0],
-                    'numWide':8, 'tableName':'OUG', 'nonlinearFactor': dt,
+
+        dataCode = {'log': self.log, 'analysisCode': 1, 'deviceCode': 1,
+                    'tableCode': 1, 'sortCode': 0, 'sortBits': [0, 0, 0],
+                    'numWide': 8, 'tableName': 'OUG', 'nonlinearFactor': dt,
                     #'thermalCode':1,
                     #'formatCode':1,
                     #'elementName':eType,'sCode':0,'stressBits':stressBits
@@ -163,4 +168,3 @@ class OUG(object):
             entry2 = iFormat(entry)
             out.append(entry2)
         return out
-    
