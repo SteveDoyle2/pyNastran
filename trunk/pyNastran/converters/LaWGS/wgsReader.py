710d48edcf2fda211c434755fc9f35b5d82f28a1
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2012-08-16 02:31:16 +0000

    applying autopep8

--- trunk/pyNastran/converters/LaWGS/wgsReader.py
+++ trunk/pyNastran/converters/LaWGS/wgsReader.py
@@ -1,6 +1,7 @@
 import copy
-from math import sin,cos
-from numpy import array,radians,dot,zeros
+from math import sin, cos
+from numpy import array, radians, dot, zeros
+
 
 class LaWGS_Panel(object):
     """
@@ -8,29 +9,29 @@ class LaWGS_Panel(object):
     @param translate
     @param scale
     """
-    def __init__(self,key,header,group):
+    def __init__(self, key, header, group):
         #print "key=%s \nheader=|%s|" %(key,header)   # ,iSymG
-        (ID,nline,npnt,isyml,rx,ry,rz,tx,ty,tz,xScale,yScale,zScale,iSymG) = header.strip().split()
-        print "ID=%s name=%s imax=%s jmax=%s" %(ID,key,nline,npnt)
-        print "Rotate    = <%s,%s,%s>" %(rx,ry,rz)
-        print "Translate = <%s,%s,%s>" %(tx,ty,rz)
-        print "Scale     = <%s,%s,%s>" %(xScale,yScale,zScale)
+        (ID, nline, npnt, isyml, rx, ry, rz, tx, ty, tz, xScale,
+            yScale, zScale, iSymG) = header.strip().split()
+        print "ID=%s name=%s imax=%s jmax=%s" % (ID, key, nline, npnt)
+        print "Rotate    = <%s,%s,%s>" % (rx, ry, rz)
+        print "Translate = <%s,%s,%s>" % (tx, ty, rz)
+        print "Scale     = <%s,%s,%s>" % (xScale, yScale, zScale)
 
         self.name = key
-        self.rotate    = array([rx,ry,rz],'d')
-        self.translate = array([tx,ty,tz],'d')
-        self.scale     = array([xScale,yScale,zScale],'d')
-        npnt  = int(npnt)
+        self.rotate = array([rx, ry, rz], 'd')
+        self.translate = array([tx, ty, tz], 'd')
+        self.scale = array([xScale, yScale, zScale], 'd')
+        npnt = int(npnt)
         nline = int(nline)
 
         self.nCols = npnt
         self.nRows = nline
-        nGroupLines = npnt//2 # self.nCols
+        nGroupLines = npnt // 2  # self.nCols
         isSingleLine = False
-        if npnt%2==1: # self.nCols
+        if npnt % 2 == 1:  # self.nCols
             isSingleLine = True
             #nGroupLines+=1
-            
 
         #X = array((nRows,nCols))
         #Y = array((nRows,nCols))
@@ -43,20 +44,20 @@ class LaWGS_Panel(object):
             for row in xrange(nGroupLines):
                 line = group[irow]
                 #print " line = ",line
-                (x1,y1,z1,x2,y2,z2) = line.strip().split()
-                points.append( array([x1,y1,z1],'d') )
-                points.append( array([x2,y2,z2],'d') )
+                (x1, y1, z1, x2, y2, z2) = line.strip().split()
+                points.append(array([x1, y1, z1], 'd'))
+                points.append(array([x2, y2, z2], 'd'))
                 #print "points[%s]=%s" %(i,points[i])
                 #print "points[%s]=%s" %(i+1,points[i+1])
                 #print "---"
-                irow+=1
+                irow += 1
 
             if isSingleLine:
                 line = group[irow]
                 #print "*line = ",line
-                (x1,y1,z1) = line.strip().split()
-                points.append( array([x1,y1,z1],'d') )
-                irow+=1
+                (x1, y1, z1) = line.strip().split()
+                points.append(array([x1, y1, z1], 'd'))
+                irow += 1
             ###
         #for i,point in enumerate(points):
             #print "point[%s] = %s" %(i,point)
@@ -71,7 +72,7 @@ class LaWGS_Panel(object):
                 #jj = n%self.nCols
                 #ii = n/self.nCols
                 #print "n=%-2s i=%-2s j=%-2s ii=%-2s jj=%-2s" %(n,i,j,ii,jj)
-                n+=1
+                n += 1
             ###
             Points.append(points2)
             #print "len(points[%s]) = %s" %(j,len(points2))
@@ -79,9 +80,8 @@ class LaWGS_Panel(object):
         ###
         self.points = Points
         #print "len(self.points) = %s" %(len(self.points))
-    
-    
-    def buildRotationMatrix(self,r):
+
+    def buildRotationMatrix(self, r):
         """
         Form the rotation matrix used for geometrical transformations
         Taken from NASA TM 85767 defining LaWGS.
@@ -90,26 +90,26 @@ class LaWGS_Panel(object):
         #r = radians([self.phi,self.theta,self.psi])
         #print "self.rotate = ",self.rotate
         r = [radians(ri) for ri in self.rotate]
-        cPhi   = cos(r[0])
-        sPhi   = sin(r[0])
+        cPhi = cos(r[0])
+        sPhi = sin(r[0])
         cTheta = cos(r[1])
         sTheta = sin(r[1])
-        cPsi   = cos(r[2])
-        sPsi   = sin(r[2])
+        cPsi = cos(r[2])
+        sPsi = sin(r[2])
 
-        rot = zeros((3,3),'d')
+        rot = zeros((3, 3), 'd')
         #print rot
-        rot[0,0] =  cTheta*cPsi
-        rot[1,0] =  cTheta*sPsi
-        rot[2,0] = -sTheta
+        rot[0, 0] = cTheta * cPsi
+        rot[1, 0] = cTheta * sPsi
+        rot[2, 0] = -sTheta
 
-        rot[0,1] = -sPsi*cPhi + sTheta*cPsi*sPhi
-        rot[1,1] =  cPsi*cPhi + sTheta*sPsi*sPhi
-        rot[2,1] =  cTheta*sPhi
+        rot[0, 1] = -sPsi * cPhi + sTheta * cPsi * sPhi
+        rot[1, 1] = cPsi * cPhi + sTheta * sPsi * sPhi
+        rot[2, 1] = cTheta * sPhi
 
-        rot[0,2] =  sPsi*sPhi + sTheta*cPsi*cPhi
-        rot[1,2] = -cPsi*sPhi + sTheta*sPsi*cPhi
-        rot[2,2] =  cTheta*cPhi
+        rot[0, 2] = sPsi * sPhi + sTheta * cPsi * cPhi
+        rot[1, 2] = -cPsi * sPhi + sTheta * sPsi * cPhi
+        rot[2, 2] = cTheta * cPhi
 
         return rot
 
@@ -117,14 +117,14 @@ class LaWGS_Panel(object):
         rot = self.buildRotationMatrix(self.rotate)
         scale = self.scale
         translate = self.translate
-        
+
         points = self.points
         Points2 = copy.deepcopy(points)
-        print "size(points) = (%s,%s)\n" %(len(points),len(points[0]))
+        print "size(points) = (%s,%s)\n" % (len(points), len(points[0]))
         for i in xrange(self.nRows):
             #points2 = []
             for j in xrange(self.nCols):
-                Points2[i][j] = scale*(dot(rot,points[i][j]) + translate)
+                Points2[i][j] = scale * (dot(rot, points[i][j]) + translate)
             ###
         ###
         self.Points = Points2
@@ -134,37 +134,37 @@ class LaWGS_Panel(object):
         for i in xrange(self.nRows):
             #points2 = []
             for j in xrange(self.nCols):
-                Points.append( self.Points[i][j] )
+                Points.append(self.Points[i][j])
             ###
         ###
-        return Points,len(Points)
+        return Points, len(Points)
 
-    def getElements(self,pointI):
-        n = (self.nRows-1)*(self.nCols-1)
+    def getElements(self, pointI):
+        n = (self.nRows - 1) * (self.nCols - 1)
         #print "name=%s n=%s nout=%s" %(self.name,n)
         elements = []
 
-        for i in xrange(self.nRows-1):
-            for j in xrange(self.nCols-1):
-                elements.append(self.getElement(pointI,i,j))
-        assert n==len(elements)
-        return elements,n
+        for i in xrange(self.nRows - 1):
+            for j in xrange(self.nCols - 1):
+                elements.append(self.getElement(pointI, i, j))
+        assert n == len(elements)
+        return elements, n
 
-    def getElement(self,pointI,j,i):
-        p1 = (j  )*self.nCols+(i  )+pointI
-        p2 = (j  )*self.nCols+(i+1)+pointI
-        p3 = (j+1)*self.nCols+(i+1)+pointI
-        p4 = (j+1)*self.nCols+(i  )+pointI
-        return [p1,p2,p3,p4]
-    
-    def writeAsPlot3D(self,f):
+    def getElement(self, pointI, j, i):
+        p1 = (j) * self.nCols + (i) + pointI
+        p2 = (j) * self.nCols + (i + 1) + pointI
+        p3 = (j + 1) * self.nCols + (i + 1) + pointI
+        p4 = (j + 1) * self.nCols + (i) + pointI
+        return [p1, p2, p3, p4]
+
+    def writeAsPlot3D(self, f):
         X = []
         Y = []
         Z = []
         for i in xrange(self.nRows):
             #points2 = []
             for j in xrange(self.nCols):
-                (x,y,z) = self.Points[i][j]
+                (x, y, z) = self.Points[i][j]
                 X.append(x)
                 Y.append(y)
                 Z.append(z)
@@ -173,61 +173,63 @@ class LaWGS_Panel(object):
 
         msg = ''
         for x in X:
-            msg += '%s ' %(x)
-        f.write(msg+'\n')
+            msg += '%s ' % (x)
+        f.write(msg + '\n')
 
         msg = ''
         for y in Y:
-            msg += '%s ' %(y)
-        f.write(msg+'\n')
+            msg += '%s ' % (y)
+        f.write(msg + '\n')
 
         msg = ''
         for z in Z:
-            msg += '%s ' %(z)
-        f.write(msg+'\n')
+            msg += '%s ' % (z)
+        f.write(msg + '\n')
+
 
 class LaWGS(object):
     modelType = 'LaWGS'
-    def __init__(self,filename='tmx1242.wgs'):
+
+    def __init__(self, filename='tmx1242.wgs'):
         self.filename = filename
-    
+
     def readLaWGS(self):
-        lawgs = open(self.filename,'r')
+        lawgs = open(self.filename, 'r')
         lines = lawgs.readlines()
-        
+
         nlines = len(lines)
         groups = {}
         name = ''
         group = []
         header = ''
-        
-        i=1
-        while i<nlines:
+
+        i = 1
+        while i < nlines:
             line = lines[i].rstrip()
-            if line.strip()=='':
+            if line.strip() == '':
                 #print "found blank line, breaking..."
                 break
             #print line
             if "'" in line:
                 #print "if"
-                groups[name] = [header,group]
+                groups[name] = [header, group]
                 name = line.strip("' \t\r\n")
-                header = lines[i+1].rstrip()
+                header = lines[i + 1].rstrip()
                 group = []
-                i+=1
+                i += 1
             else:
                 group.append(line)
-            i+=1
+            i += 1
         ###
-        groups[name] = [header,group]
-        
+        groups[name] = [header, group]
+
         del groups['']
         self.panels = {}
-        for key,headerGroup in sorted(groups.iteritems()):
-            header,group = headerGroup
+        for key, headerGroup in sorted(groups.iteritems()):
+            header, group = headerGroup
             #if key=='BODY':
             if 1:
-                panel = LaWGS_Panel(key,header,group)
+                panel = LaWGS_Panel(key, header, group)
                 panel.updatePoints()
                 self.panels[key] = panel
         ###
@@ -235,10 +237,10 @@ class LaWGS(object):
     def getPointsElements(self):
         points = []
         elements = []
-        pointI=0
-        for (name,panel) in sorted(self.panels.iteritems()):
-            (pointsI,pointi) = panel.getPoints()
-            (elementsI,n) = panel.getElements(pointI)
+        pointI = 0
+        for (name, panel) in sorted(self.panels.iteritems()):
+            (pointsI, pointi) = panel.getPoints()
+            (elementsI, n) = panel.getElements(pointI)
             #print "elementsI = ",elementsI
             points += pointsI
             elements += elementsI
@@ -247,18 +249,18 @@ class LaWGS(object):
         ###
         #for point in points:
             #print point
-        return points,elements
+        return points, elements
 
-    def writeAsPlot3D(self,p3dname):
-        f = open(p3dname,'wb')
+    def writeAsPlot3D(self, p3dname):
+        f = open(p3dname, 'wb')
 
-        f.write('%s\n' %(len(self.panels)))
-        for (name,panel) in sorted(self.panels.iteritems()):
-            f.write('%s %s 1\n' %(panel.nRows,panel.nCols))
+        f.write('%s\n' % (len(self.panels)))
+        for (name, panel) in sorted(self.panels.iteritems()):
+            f.write('%s %s 1\n' % (panel.nRows, panel.nCols))
 
-        for (name,panel) in sorted(self.panels.iteritems()):
+        for (name, panel) in sorted(self.panels.iteritems()):
             panel.writeAsPlot3D(f)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     lawgs = LaWGS('tmx1242.wgs')
     lawgs.readLaWGS()
--- trunk/pyNastran/converters/panair/panairIO.py
+++ trunk/pyNastran/converters/panair/panairIO.py
@@ -2,6 +2,7 @@ import vtk
 from vtk import vtkQuad
 from panairGrid import PanairGrid
 
+
 class PanairIO(object):
     def __init__(self):
         pass
@@ -9,12 +10,12 @@ class PanairIO(object):
 #    lawgs = LaWGS('tmx1242.wgs')
 #    lawgs.run()
 
-    def loadPanairGeometry(self,panairFileName,dirname,isNodal,isCentroidal):
+    def loadPanairGeometry(self, panairFileName, dirname, isNodal, isCentroidal):
         self.isNodal = isNodal
         self.isCentroidal = isCentroidal
         #key = self.caseKeys[self.iCase]
         #case = self.resultCases[key]
-        
+
         skipReading = self.removeOldGeometry(panairFileName)
         if skipReading:
             return
@@ -22,8 +23,8 @@ class PanairIO(object):
         model = PanairGrid(panairFileName)
         self.modelType = model.modelType
         model.readGrid()
-        
-        nodes,elements = model.getPointsElements()
+
+        nodes, elements = model.getPointsElements()
         #for nid,node in enumerate(nodes):
             #print "node[%s] = %s" %(nid,str(node))
 
@@ -31,7 +32,7 @@ class PanairIO(object):
         self.nElements = len(elements)
 
         #print "nNodes = ",self.nNodes
-        print "nElements = ",self.nElements
+        print "nElements = ", self.nElements
 
         self.grid.Allocate(self.nElements, 1000)
         self.gridResult.SetNumberOfComponents(self.nElements)
@@ -39,15 +40,15 @@ class PanairIO(object):
 
         points = vtk.vtkPoints()
         points.SetNumberOfPoints(self.nNodes)
-        self.gridResult.Allocate(self.nNodes,1000)
+        self.gridResult.Allocate(self.nNodes, 1000)
         #vectorReselt.SetNumberOfComponents(3)
         self.nidMap = {}
         #elem.SetNumberOfPoints(nNodes)
         if 0:
-            fraction = 1./nNodes # so you can color the nodes by ID
-            for nid,node in sorted(nodes.iteritems()):
-                points.InsertPoint(nid-1, *point)
-                self.gridResult.InsertNextValue(nid*fraction)
+            fraction = 1. / nNodes  # so you can color the nodes by ID
+            for nid, node in sorted(nodes.iteritems()):
+                points.InsertPoint(nid - 1, *point)
+                self.gridResult.InsertNextValue(nid * fraction)
                 #print str(element)
 
                 #elem = vtk.vtkVertex()
@@ -55,12 +56,12 @@ class PanairIO(object):
                 #self.aQuadGrid.InsertNextCell(elem.GetCellType(), elem.GetPointIds())
                 #vectorResult.InsertTuple3(0, 0.0, 0.0, 1.0)
 
-        for nid,node in enumerate(nodes):
+        for nid, node in enumerate(nodes):
             points.InsertPoint(nid, *node)
         #print "nid = ",nid
 
-        for eid,element in enumerate(elements):
-            (p1,p2,p3,p4) = element
+        for eid, element in enumerate(elements):
+            (p1, p2, p3, p4) = element
             #print "element = ",element
             elem = vtkQuad()
             elem.GetPointIds().SetId(0, p1)
@@ -69,7 +70,7 @@ class PanairIO(object):
             elem.GetPointIds().SetId(3, p4)
             self.grid.InsertNextCell(elem.GetCellType(), elem.GetPointIds())
         ###
-        print "eid = ",eid
+        print "eid = ", eid
         self.grid.SetPoints(points)
         #self.grid2.SetPoints(points2)
         #self.grid.GetPointData().SetScalars(self.gridResult)
@@ -89,23 +90,24 @@ class PanairIO(object):
         self.scalarBar.VisibilityOn()
         self.scalarBar.Modified()
 
-        self.iSubcaseNameMap = {1:['Cart3d','']}
+        self.iSubcaseNameMap = {1: ['Cart3d', '']}
         cases = {}
         ID = 1
-        
+
         #print "nElements = ",nElements
-        cases = self.fillCart3dCase(cases,ID,regions,loads)
+        cases = self.fillCart3dCase(cases, ID, regions, loads)
 
         self.resultCases = cases
         self.caseKeys = sorted(cases.keys())
         #print "caseKeys = ",self.caseKeys
         #print "type(caseKeys) = ",type(self.caseKeys)
         self.iCase = -1
-        self.nCases = len(self.resultCases)-1 # number of keys in dictionary
-        self.cycleResults() # start at nCase=0
+        self.nCases = len(self.resultCases) - 1  # number of keys in dictionary
+        self.cycleResults()  # start at nCase=0
 
-if __name__=='__main__':
+if __name__ == '__main__':
     print ''
+
     def removeOldGeometry(self):
         pass
 
@@ -113,4 +115,4 @@ if __name__=='__main__':
     test.removeOldGeometry = removeOldGeometry
 
     #test.loadPanairGeometry('SWB.INP','',True,True)
-    test.loadPanairGeometry('models/NAC6.INP','',True,True)
+    test.loadPanairGeometry('models/NAC6.INP', '', True, True)
