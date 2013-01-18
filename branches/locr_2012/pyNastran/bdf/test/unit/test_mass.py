c0ecf6dab205ddc959b5b9feae6b68541fe428f5
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2013-01-18 00:43:53 +0000

    fixed path to be accessible from anywhere

--- branches/locr_2012/pyNastran/bdf/test/unit/test_mass.py
+++ branches/locr_2012/pyNastran/bdf/test/unit/test_mass.py
@@ -1,13 +1,17 @@
-from numpy import array, allclose
 import unittest
 
+import os
+import pyNastran
 from pyNastran.bdf.bdf import BDF
 
+rootpath = pyNastran.__path__[0]
+testpath = os.path.join(rootpath, 'bdf', 'test', 'unit')
 
 class TestMass(unittest.TestCase):
     def test_mass1(self):  # passes
-        mesh = BDF(debug=True, log=None) 
-        mesh.readBDF('test_mass.dat', includeDir=None, xref=True) 
+        mesh = BDF(debug=True, log=None)
+        bdfname = os.path.join(testpath, 'test_mass.dat')
+        mesh.readBDF(bdfname, includeDir=None, xref=True) 
         self.assertAlmostEqual(mesh.elements[1].Mass(),0.12)
 
 if __name__ == '__main__':
