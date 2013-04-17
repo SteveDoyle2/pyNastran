418177246a20af4dc92026e05f5837ac519a4bff
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2013-04-17 01:23:10 +0000

    fixed more paths to assign_type fixed bug in BDF file checker

--- pyNastran/bdf/test/all_tests.py
+++ pyNastran/bdf/test/all_tests.py
@@ -4,7 +4,7 @@ import unittest
 from pyNastran.bdf.test.test_field_writer import TestFieldWriter
 from pyNastran.bdf.test.bdf_unit_tests import TestBDF
 from pyNastran.bdf.test.unit.test_coords import TestCoords
-
+from pyNastran.bdf.test.cards.test_constraints import *
 
 if __name__ == "__main__":
     unittest.main()
