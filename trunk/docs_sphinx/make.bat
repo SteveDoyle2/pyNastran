680be965e2ac5b8a816711f486bfcf18286e719e
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2013-04-25 08:37:35 +0000

    adding script to create the index folder

--- trunk/docs_sphinx/make.bat
+++ trunk/docs_sphinx/make.bat
@@ -3,7 +3,7 @@
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build2
+	set SPHINXBUILD=sphinx-build
 )
 set BUILDDIR=_build
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
