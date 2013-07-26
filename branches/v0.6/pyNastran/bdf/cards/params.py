fec3ab131236833ebd649ee7f8b0f1eda8a44d68
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2013-07-26 15:50:45 +0000

    - removing self.value, self.value1, self.value2 from PARAM  - data list (for PARAM) is now converted to a card object so there's only one path for data checking (which prevents double type checking when the user creates a PARAM object)  - added update_value method to PARAM; still need to type check and possibly rename

--- branches/v0.6/pyNastran/bdf/cards/params.py
+++ branches/v0.6/pyNastran/bdf/cards/params.py
@@ -27,6 +27,7 @@ from __future__ import (nested_scopes, generators, division, absolute_import,
                         print_function, unicode_literals)
 
 from pyNastran.bdf.cards.baseCard import BaseCard
+from pyNastran.bdf.bdfInterface.BDF_Card import BDFCard
 from pyNastran.bdf.bdfInterface.assign_type import (integer_or_blank,
     double_or_blank, string, string_or_blank,
     integer_double_or_string)
@@ -37,49 +38,56 @@ class PARAM(BaseCard):
     def __init__(self, card, data=None, comment=''):
         if comment:
             self._comment = comment
-        if card:
-            self.key = string(card, 1, 'key')
-            n = 1
-            if self.key == 'ACOUT':
-                self.value = string_or_blank(card, 2, 'value', 'PEAK')
-            elif self.key == 'ACOWEAK':
-                self.value = string_or_blank(card, 2, 'value', 'NO')
-            elif self.key == 'ACSYM':
-                self.value = string_or_blank(card, 2, 'value', 'YES')
-            elif self.key == 'ADJMETH':
-                self.value = integer_or_blank(card, 2, 'value', 0)
-            elif self.key == 'ADMPOST':
-                self.value = string_or_blank(card, 2, 'value', 0)
-            elif self.key == 'ADSTAT':
-                self.value = string_or_blank(card, 2, 'value', 'YES')
-            elif self.key in ['ALPHA1', 'ALPHA2']:
-                self.value1 = double_or_blank(card, 2, 'value1', 0.0)
-                self.value2 = double_or_blank(card, 2, 'value2', 0.0)
-                n = 2
-            elif self.key in ['ALPHA1FL', 'ALPHA2FL']:
-                self.value1 = double_or_blank(card, 2, 'value1', 0.0)
-                self.value2 = double_or_blank(card, 3, 'value2', 0.0)
-                n = 2
-            elif self.key in ['CB1', 'CB2', 'CK1', 'CK2', 'CK3', 'CM1', 'CM2', 'CP1', 'CP2']:
-                self.value1 = double_or_blank(card, 2, 'value1', 1.0)
-                self.value2 = double_or_blank(card, 3, 'value2', 0.0)
-                n = 2
-            else:
-                self.value = integer_double_or_string(card, 2, 'value')
+        if data:
+            card = BDFCard(['PARAM'] + data)
 
-            if hasattr(self, 'value1'):
-                self.values = [self.value1, self.value2]
-            else:
-                self.values = [self.value]
-
-            if n == 1:
-                assert len(card) == 3, 'len(PARAM card)=%i card=%r' % (len(card), card)
-            else:
-                assert len(card) == 4, 'len(PARAM card)=%i card=%r' % (len(card), card)
+        self.key = string(card, 1, 'key')
+        n = 1
+        value = None
+        if self.key == 'ACOUT':
+            value = string_or_blank(card, 2, 'value', 'PEAK')
+        elif self.key == 'ACOWEAK':
+            value = string_or_blank(card, 2, 'value', 'NO')
+        elif self.key == 'ACSYM':
+            value = string_or_blank(card, 2, 'value', 'YES')
+        elif self.key == 'ADJMETH':
+            value = integer_or_blank(card, 2, 'value', 0)
+        elif self.key == 'ADMPOST':
+            value = string_or_blank(card, 2, 'value', 0)
+        elif self.key == 'ADSTAT':
+            value = string_or_blank(card, 2, 'value', 'YES')
+        elif self.key in ['ALPHA1', 'ALPHA2']:
+            value1 = double_or_blank(card, 2, 'value1', 0.0)
+            value2 = double_or_blank(card, 2, 'value2', 0.0)
+            n = 2
+        elif self.key in ['ALPHA1FL', 'ALPHA2FL']:
+            value1 = double_or_blank(card, 2, 'value1', 0.0)
+            value2 = double_or_blank(card, 3, 'value2', 0.0)
+            n = 2
+        elif self.key in ['CB1', 'CB2', 'CK1', 'CK2', 'CK3', 'CM1', 'CM2', 'CP1', 'CP2']:
+            value1 = double_or_blank(card, 2, 'value1', 1.0)
+            value2 = double_or_blank(card, 3, 'value2', 0.0)
+            n = 2
         else:
-            self.key = data[0]
-            self.value = data[1]
+            value = integer_double_or_string(card, 2, 'value')
 
+        if value is None:
+            self.values = [value1, value2]
+        else:
+            self.values = [value]
+
+        if n == 1:
+            if len(card) != 3:
+                raise RuntimeError('len(PARAM card)=%i card=%r' % (len(card), card))
+        else:
+            if len(card) != 4:
+                raise RuntimeError('len(PARAM card)=%i card=%r' % (len(card), card))
+
+    def update_value(value1, value2=None):
+        self.values = [value1]
+        if value2 is not None:
+            self.values.append(value2)
+            
     #def isSameCard(self, param, debug=False):
         #fields1 = [self.key, self.value ]
         #fields2 = [param.key, param.value]
