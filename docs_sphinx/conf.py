bbddfbcb692573b0227de4ec70b7dc5577a6835a
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2014-01-03 00:03:41 +0000

    - adding mock object for packages

--- docs_sphinx/conf.py
+++ docs_sphinx/conf.py
@@ -38,6 +38,30 @@ sys.path.append(os.path.join(pkg_path, 'f06'))
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+# ---3rd party modules don't work, so we hack them in --------------------------
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'vtk', 'wx', 'PyQt4', 'PySide', 'docopt']
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
