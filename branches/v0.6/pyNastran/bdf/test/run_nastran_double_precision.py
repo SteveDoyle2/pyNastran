1db3ff4be620f56455ad0b07b211698ac12aaedd
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2014-01-08 19:16:47 +0000

    - adding comments and preliminary check

--- branches/v0.6/pyNastran/bdf/test/run_nastran_double_precision.py
+++ branches/v0.6/pyNastran/bdf/test/run_nastran_double_precision.py
@@ -6,13 +6,31 @@ from pyNastran.f06.f06 import F06
 
 
 if __name__ == '__main__':
-    model = BDF()
-    model.read_bdf(sys.argv[1])
+    bdf_name = sys.argv[1]
+    base, ext = os.path.splitext(bdf_name)
 
-    out_bdf = 'out.bdf'
-    model.write_bdf(out_bdf, size=16, precision='double')
+    #===========================
+    # run nastran and verify the starting model is correct
+    os.system('nastran scr=yes bat=no news=no %s' % bdf_name)
 
-    os.system('nastran scr=yes bat=no news=no %s' % out_bdf)
-    out_f06 = 'out.f06'
-    model2 = F06(out_f06)
+    f06_name = base + '.f06'
+    model2 = F06(f06_name)
     model2.read_f06()
+
+    #===========================
+    # read/write the model in double precision
+    out_bdf = 'out.bdf'
+    model3 = BDF()
+    model3.read_bdf(bdf_name)
+    model3.write_bdf(out_bdf, size=16, precision='double')
+
+    #===========================
+    # run nastran again
+    os.system('nastran scr=yes bat=no news=no %s' % out_bdf)
+    #===========================
+    # verify it's correct
+    out_f06 = 'out.f06'
+    model4 = F06(out_f06)
+    model4.read_f06()
+
+    print('\n\npassed!!')
