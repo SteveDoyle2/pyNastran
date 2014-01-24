e647deaa3dd8371d879359d4868f5b321e10a69e
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2014-01-24 02:17:09 +0000

    creating dummy one-based array class to help debug (doesn't work yet)

--- branches/v0.6/pyNastran/utils/mathematics.py
+++ branches/v0.6/pyNastran/utils/mathematics.py
@@ -329,3 +329,32 @@ def gauss(n):
 
     raise NotImplementedError('The current implementation only supports up to '
                               '5 quadrature points')
+
+class Zeros(object):
+    def __init__(self, size, dtype='i'):
+        if isinstance(size, int):
+            size = [size]
+        else:
+            size = list(size)
+        size = tuple([sizei+1 for sizei in size])
+        self.data = zeros(size, dtype=dtype)
+
+    def __getitem__(self, key):
+        print "key_get =", str(key)
+        key0 = key[0] - 1
+        key1 = key[1]
+        key = (key0, key1)
+        print "key_get", key
+        print "---"
+        return self.data[key0, key1]
+
+    def __setitem__(self, key, value):
+        print "key_set1 =", str(key)
+        key0 = key[0] - 1
+        key1 = key[1]
+        key = (key0, key1)
+        print "key_set2", key
+        print "value_set =", value - 1
+        print "---"
+        self.data[key] = value - 1
+
