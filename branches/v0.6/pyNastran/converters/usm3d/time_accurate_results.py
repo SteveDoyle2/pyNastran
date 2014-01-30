9238ae4079a3c55a3002b222a4240ca24fa0b7fc
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2014-01-30 18:46:25 +0000

    - multiprocessing & preallocation for usm3d parser

--- branches/v0.6/pyNastran/converters/usm3d/time_accurate_results.py
+++ branches/v0.6/pyNastran/converters/usm3d/time_accurate_results.py
@@ -1,8 +1,9 @@
 import os
 from collections import defaultdict
-from numpy import savetxt, arange
+from numpy import savetxt, arange, zeros
 
 from pyNastran.converters.usm3d.usm3d_reader import Usm3dReader
+import multiprocessing as mp
 
 def get_flo_files(dirname, model_name, include_dirname_in_path=True):
     """
@@ -17,6 +18,9 @@ def get_flo_files(dirname, model_name, include_dirname_in_path=True):
     for flo_filename in flo_filenames:
         base, ext = os.path.splitext(flo_filename)
         if ext == '.flo':
+            if '_' not in flo_filename:
+                print flo_filename
+                continue
             n = int(base.split('_')[-1])
             n_list.append(n)
 
@@ -28,15 +32,14 @@ def get_flo_files(dirname, model_name, include_dirname_in_path=True):
     return flo_filenames
 
 
-def main():
+def run(dirname, model_name, node_ids, nstart=0, nlimit=None, num_cpus=8):
     model = Usm3dReader()
-    node_ids = [517]
-
-    dirname = r'W:\usm3d\usm3d\spw4'
-    model_name = 'spw4'
 
     flo_filenames = get_flo_files(dirname, model_name)
+    flo_filenames = flo_filenames[nstart:nlimit]
     #print "flo_filenames =", flo_filenames
+    nlimit = len(flo_filenames)
+    #print "nmax =", nlimit
 
     Cp = {}
     Mach = {}
@@ -46,34 +49,64 @@ def main():
     W = {}
     p = {}
     rhoU = {}
+
     for node_id in node_ids:
-        Cp[node_id] = []
-        Mach[node_id] = []
-        T[node_id] = []
-        U[node_id] = []
-        V[node_id] = []
-        W[node_id] = []
-        p[node_id] = []
-        rhoU[node_id] = []
+        Cp[node_id] = zeros(nlimit, 'float64')
+        Mach[node_id] = zeros(nlimit, 'float64')
+        T[node_id] = zeros(nlimit, 'float64')
+        U[node_id] = zeros(nlimit, 'float64')
+        V[node_id] = zeros(nlimit, 'float64')
+        W[node_id] = zeros(nlimit, 'float64')
+        p[node_id] = zeros(nlimit, 'float64')
+        rhoU[node_id] = zeros(nlimit, 'float64')
 
     #flo_filenames = flo_filenames[:10]
-    for iflo, flo_filename in enumerate(flo_filenames):
-        if iflo % 100 == 0:
-            print('iflo=%s flo_filename=%s' % (iflo, flo_filename))
 
-        node_ids2, loads = model.read_flo(flo_filename, node_ids=node_ids)
-        for i, node_id in enumerate(node_ids):
-            Cp[node_id].append(loads['Cp'][i])
-            Mach[node_id].append(loads['Mach'][i])
-            T[node_id].append(loads['T'][i])
-            U[node_id].append(loads['U'][i])
-            V[node_id].append(loads['V'][i])
-            W[node_id].append(loads['W'][i])
-            p[node_id].append(loads['p'][i])
-            rhoU[node_id].append(loads['rhoU'][i])
+    if num_cpus == 1:
+        model = Usm3dReader()
+        for flo_filename in flo_filenames:
+            node_ids2, loads = model.read_flo(flo_filename, node_ids=node_ids)
+            for i, node_id in enumerate(node_ids):
+                Cp[node_id][i] = loads['Cp'][i]
+                Mach[node_id][i] = loads['Mach'][i]
+                T[node_id][i] = loads['T'][i]
+                U[node_id][i] = loads['U'][i]
+                V[node_id][i] = loads['V'][i]
+                W[node_id][i] = loads['W'][i]
+                p[node_id][i] = loads['p'][i]
+                rhoU[node_id][i] = loads['rhoU'][i]
+    else:
+        pool = mp.Pool(num_cpus)
+        result = pool.imap(_loads_func, [(flo_filename, node_ids) for flo_filename in flo_filenames])
+        n = 0
+        for j, loads in enumerate(result):
+            if n % 500 == 0:
+                print "n =", n
+            for i, node_id in enumerate(node_ids):
+                #print "Cp[node=%s] =%s" % (node_id, loads['Cp'][i])
+                Cp[node_id][j] = loads['Cp'][i]
+                Mach[node_id][j] = loads['Mach'][i]
+                T[node_id][j] = loads['T'][i]
+                U[node_id][j] = loads['U'][i]
+                V[node_id][j] = loads['V'][i]
+                W[node_id][j] = loads['W'][i]
+                p[node_id][j] = loads['p'][i]
+                rhoU[node_id][j] = loads['rhoU'][i]
+                n += 1
+        pool.close()
+        pool.join()
+    return Cp, Mach, T, U, V, W, p, rhoU
 
+def _loads_func(data):
+    flo_filename, node_ids = data
+    model = Usm3dReader()
+    node_ids2, loads = model.read_flo(flo_filename, node_ids=node_ids)
+    return loads
+
+def write_loads(csv_filename, loads):
+    (Cp, Mach, T, U, V, W, p, rhoU) = loads
     #print "loads.keys() = ", sorted(loads.keys())
-    f = open('usm3d.csv', 'wb')
+    f = open(csv_filename, 'wb')
     dt = 1.0
     t = arange(len(Cp[node_id])) * dt
     f.write('time\t');  savetxt(f, t, delimiter='', newline=',')
@@ -84,6 +117,14 @@ def main():
         f.write('\np[%s],' % node_id); savetxt(f, p[node_id], delimiter='', newline=',')
         f.write('\n\n')
     f.close()
+
+
+def main():
+    dirname = r'W:\usm3d\usm3d\spw4'
+    model_name = 'spw4'
+    node_ids = [517]
+    loads = run(dirname, model_name, node_ids)
+    write_loads('usm3d.csv', loads)
 
 
 if __name__ == '__main__':
