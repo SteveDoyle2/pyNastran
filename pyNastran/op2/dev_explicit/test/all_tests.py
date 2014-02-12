ee3780c8b0e578587323dba242164246d7010d41
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2014-02-12 06:44:03 +0000

    - adding test_op2.py that's called by test_op2 so I can stop messing with the other script

---
+++ pyNastran/op2/dev_explicit/test/all_tests.py
@@ -0,0 +1,5 @@
+from pyNastran.op2.test.op2_unit_tests import TestOP2
+
+if __name__ == "__main__":
+    import unittest
+    unittest.main()
---
+++ pyNastran/op2/dev_explicit/test/op2_test.py
@@ -0,0 +1,101 @@
+import os
+import sys
+
+from pyNastran.op2.test.test_op2 import get_failed_files, run_lots_of_files
+from pyNastran.utils import get_files_of_type
+
+
+def parse_skipped_cards(fname):
+    f = open(fname,'r')
+    lines = f.readlines()
+    f.close()
+
+    results = {}
+    for line in lines:
+        if 'OES' in line[0:3]:
+            (fore,aft) = line.strip().split('->')
+            (oes,form,elementTypeNum) = fore.strip().split(' ')
+            (element_type,eType) = elementTypeNum.strip().split('=')
+            (msg,fpath) = aft.strip().split('-')
+            #print("fpath=%r" % fpath)
+            fpath = fpath.lstrip()[6:]
+            eName = msg.split(' ')[0]
+            #print "eName=%s eType=%s form=%s fpath=|%s|" %(eName,eType,form,fpath)
+            key = (eName,eType,form)
+            if key not in results:
+                results[key] = fpath
+
+    filesToAnalyze = []
+    for (key,value) in sorted(results.iteritems()):
+        filesToAnalyze.append(value)
+
+    f = open('newElements.in','wb')
+    for fname in filesToAnalyze:
+        f.write(fname+'\n')
+    f.close()
+    return filesToAnalyze
+
+
+def get_all_files(foldersFile,fileType):
+    f = open(foldersFile,'r')
+    lines = f.readlines()
+    files2 = []
+    for line in lines:
+        moveDir = os.path.join('r"'+line.strip()+'"')
+        moveDir = line.strip()
+        if moveDir and moveDir[0] != '#':
+            print("moveDir = %s" % moveDir)
+            assert os.path.exists(moveDir), '%s doesnt exist' % (moveDir)
+            files2 += get_files_of_type(moveDir, fileType, maxSize=4.2)
+    return files2
+
+
+def main():
+    # works
+    files = get_files_of_type('tests','.op2')
+
+    foldersFile = 'tests/foldersRead.txt'
+
+    iSubcases = []
+    debug     = False
+    make_geom  = False
+    write_bdf  = False
+    write_f06  = True
+    write_matlab = True
+
+    delete_f06 = True
+    saveCases = True
+    regenerate = False
+    stopOnFailure = False
+    getSkipCards = False
+
+    if getSkipCards:
+        files2 = parse_skipped_cards('skippedCards.out')
+    elif regenerate:
+        files2 = get_all_files(foldersFile,'.op2')
+        files2 += files
+        files2.sort()
+    else:
+        files2 = get_failed_files('failedCases.in')
+    files = files2
+
+    skipFiles = ['nltrot99.op2','rot12901.op2','plan20s.op2'] # giant
+
+    nStart = 0
+    nStop = 10000
+    try:
+        os.remove('skippedCards.out')
+    except:
+        pass
+
+    print("nFiles = %s" % len(files))
+    import time
+    t0 = time.time()
+    run_lots_of_files(files,make_geom,write_bdf,write_f06,write_matlab,delete_f06,
+                   debug,saveCases,skipFiles,stopOnFailure,
+                   nStart,nStop)
+    print("dt = %f" %(time.time() - t0))
+    sys.exit('final stop...')
+
+if __name__=='__main__':
+    main()
