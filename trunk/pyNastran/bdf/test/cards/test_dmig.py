1ae58f862132836d96e6593d3c60f7096a6f282c
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2014-05-10 04:50:29 +0000

    - adding polar support to DMIG/DMI/etc.    - added isPolar    - read/write reads in as real/imaginary and writes as mag/phase (slight loss of precision, but it's less than Nastran's formatting)    - more tests

--- trunk/pyNastran/bdf/test/cards/test_dmig.py
+++ trunk/pyNastran/bdf/test/cards/test_dmig.py
@@ -4,45 +4,118 @@ import os
 import pyNastran
 from pyNastran.bdf.bdf import BDF
 
+from numpy import array, array_equal, sqrt, sin, cos, radians
+
 root_path = pyNastran.__path__[0]
 test_path = os.path.join(root_path, 'bdf', 'test')
 
-class TestSolids(unittest.TestCase):
+class TestDMIG(unittest.TestCase):
 
     def test_dmig_1(self):
         """
         Tests DMIG reading
         """
-        from numpy import array_equal
         model = BDF()
         bdf_name = os.path.join(test_path, 'cards', 'dmig.bdf')
         model.read_bdf(bdf_name, xref=False, punch=True)
-        out = model.dmigs['MAAX'].getMatrix(is_sparse=False)
-        MAAX_actual, rowsReversed, colsReversed = out
-        print "---MAAX_actual---\n", MAAX_actual
+        out = model.dmigs['REALS'].getMatrix(isSparse=False)
+
+        REALS_actual, rowsReversed, colsReversed = out
+        #print "---REALS_actual---\n", REALS_actual
         #print "---out---\n", out
 
-        MAAX_expected = [
+        REALS_expected = [
             [1.0,  0.5,  0.25],
             [0.5,  2.0,  0.75],
             [0.25, 0.75, 3.0 ],
         ]
-        self.assertTrue(array_equal(MAAX_expected, MAAX_actual))
+        self.assertTrue(array_equal(REALS_expected, REALS_actual))
 
-        #--------------------------------
-        out = model.dmigs['BAAX'].getMatrix(is_sparse=False)
-        BAAX_actual, rowsReversed, colsReversed = out
-        print "---BAAX_actual---\n", BAAX_actual
-        BAAX_expected = [
+    def test_dmig_2(self):
+        model = BDF()
+        bdf_name = os.path.join(test_path, 'cards', 'dmig.bdf')
+        model.read_bdf(bdf_name, xref=False, punch=True)
+
+        out = model.dmigs['REAL'].getMatrix(isSparse=False)
+        REAL_actual, rowsReversed, colsReversed = out
+        #print "---REAL_actual---\n", REAL_actual
+        REAL_expected = [
             [1.0, 0.5, 0.25],
             [0.0, 2.0, 0.75],
             [0.0, 0.0, 3.0 ],
         ]
-        self.assertTrue(array_equal(BAAX_expected, BAAX_actual))
+        self.assertTrue(array_equal(REAL_expected, REAL_actual))
 
         #model2 = BDF()
         #bdf_name = os.path.join(test_path, 'include_dir', 'include.inc')
         #model2.read_bdf(bdf_name, xref=False, punch=True)
+
+    def test_dmig_3(self):
+        model = BDF()
+        bdf_name = os.path.join(test_path, 'cards', 'dmig.bdf')
+        model.read_bdf(bdf_name, xref=False, punch=True)
+        out = model.dmigs['IMAG'].getMatrix(isSparse=False)
+
+        IMAG_actual, rowsReversed, colsReversed = out
+        #print "---IMAG_actual---\n", IMAG_actual
+        IMAG_expected_real = [
+            [1.0, 0.5, 0.25],
+            [0.0, 2.0, 0.75],
+            [0.0, 0.0, 3.0 ],
+        ]
+        IMAG_expected_imag = [
+            [1.1, 0.51, 0.251],
+            [0.0, 2.1,  0.751],
+            [0.0, 0.0,  3.1 ],
+        ]
+        IMAG_expected = array(IMAG_expected_real) + array(IMAG_expected_imag)*1j
+        self.assertTrue(array_equal(IMAG_expected, IMAG_actual))
+
+    def test_dmig_4(self):
+        model = BDF()
+        bdf_name = os.path.join(test_path, 'cards', 'dmig.bdf')
+        model.read_bdf(bdf_name, xref=False, punch=True)
+
+        out = model.dmigs['IMAGS'].getMatrix(isSparse=False)
+        IMAGS_actual, rowsReversed, colsReversed = out
+        #print "---IMAGS_actual---\n", IMAGS_actual
+        IMAGS_expected_real = [
+            [1.0,  0.5,  0.25],
+            [0.5,  2.0,  0.75],
+            [0.25, 0.75, 3.0 ],
+        ]
+        IMAGS_expected_imag = [
+            [1.1,   0.51, 0.251],
+            [0.51,  2.1,  0.751],
+            [0.251, 0.751, 3.1  ],
+        ]
+        IMAGS_expected = array(IMAGS_expected_real) + array(IMAGS_expected_imag)*1j
+        msg  = '\n%s_actual\n%s\n\n----' % ('IMAGS', IMAGS_actual)
+        msg += '\n%s_expected\n%s\n----' % ('IMAGS', IMAGS_expected)
+        msg += '\n%s_delta\n%s\n----' % ('IMAGS', IMAGS_actual-IMAGS_expected)
+        self.assertTrue(array_equal(IMAGS_expected, IMAGS_actual), msg)
+
+    def test_dmig_5(self):
+        model = BDF()
+        bdf_name = os.path.join(test_path, 'cards', 'dmig.bdf')
+        model.read_bdf(bdf_name, xref=False, punch=True)
+        out = model.dmigs['POLE'].getMatrix(isSparse=False)
+
+        POLE_actual, rowsReversed, colsReversed = out
+        #print "---POLE_actual---\n", POLE_actual
+        mag_expected = array([
+            [1.0, 4.0, 5.0],
+            [0.0, 2.0, 6.0],
+            [0.0, 0.0, 3.0],
+        ])
+        A_expected = mag_expected * cos(radians(45))
+        B_expected = mag_expected * sin(radians(45))
+        POLE_expected = A_expected + B_expected * 1j
+
+        msg  = '\n%s_actual\n%s\n\n----' % ('POLE', POLE_actual)
+        msg += '\n%s_expected\n%s\n----' % ('POLE', POLE_expected)
+        msg += '\n%s_delta\n%s\n----' % ('POLE', POLE_actual-POLE_expected)
+        self.assertTrue(array_equal(POLE_expected, POLE_actual), msg)
 
 
 if __name__ == '__main__':
