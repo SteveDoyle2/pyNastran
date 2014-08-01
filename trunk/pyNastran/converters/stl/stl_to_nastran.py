a3852a822e04ed77deebe9b36f0bdc4ecfe5d38f
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2014-08-01 00:14:53 +0000

    - added node, element offset capability  - removed load writing (what was it used for)

--- trunk/pyNastran/converters/stl/stl_to_nastran.py
+++ trunk/pyNastran/converters/stl/stl_to_nastran.py
@@ -1,11 +1,12 @@
 from pyNastran.converters.stl.stl_reader import STLReader
 from pyNastran.bdf.fieldWriter import print_card
 
-def stl_to_nastran_filename(stl_filename, bdf_filename, log=None):
+def stl_to_nastran_filename(stl_filename, bdf_filename,
+                            nnodes_offset=0, nelements_offset=0, log=None):
     model = STLReader(log=log)
     model.read_stl(stl_filename)
 
-    nid = 1
+    nid = nnodes_offset + 1
     cid = None
     pid = 100
     mid = 200
@@ -15,19 +16,19 @@ def stl_to_nastran_filename(stl_filename, bdf_filename, log=None):
 
     bdf = open(bdf_filename, 'wb')
     bdf.write('CEND\n')
-    bdf.write('LOAD = %s\n' % load_id)
+    #bdf.write('LOAD = %s\n' % load_id)
     bdf.write('BEGIN BULK\n')
     for (x, y, z) in model.nodes:
         card = ['GRID', nid, cid, x, y, z]
         bdf.write(print_card(card))
 
-        nx, ny, nz = nodal_normals[nid - 1]
-        card = ['FORCE', load_id, nid, cid, 100, nx, ny, nz]
-        bdf.write(print_card(card))
+        #nx, ny, nz = nodal_normals[nid - 1]
+        #card = ['FORCE', load_id, nid, cid, 100, nx, ny, nz]
+        #bdf.write(print_card(card))
         nid += 1
 
-    eid = 1
-    for (n1, n2, n3) in model.elements + 1:
+    eid = nelements_offset + 1
+    for (n1, n2, n3) in (model.elements + (nnodes_offset + 1)):
         card = ['CTRIA3', eid, pid, n1, n2, n3]
         bdf.write(print_card(card))
         eid += 1
@@ -46,6 +47,6 @@ def stl_to_nastran_filename(stl_filename, bdf_filename, log=None):
     bdf.close()
 
 if __name__ == '__main__':
-    bdf_filename = 'g278.bdf'
-    stl_filename = 'g278.stl'
+    bdf_filename = 'threePlugs.bdf'
+    stl_filename = 'threePlugs.stl'
     stl_to_nastran_filename(stl_filename, bdf_filename)
