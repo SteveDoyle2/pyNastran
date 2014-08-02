c31652d6a23ae95d27c426c898b114cd5d97b7ce
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2014-08-02 17:43:02 +0000

    actually testing code

--- trunk/pyNastran/bdf/test/cards/test_sets.py
+++ trunk/pyNastran/bdf/test/cards/test_sets.py
@@ -11,7 +11,26 @@ class TestSets(unittest.TestCase):
             [[1, 2, 3], True], # these are all integers
         ]
         msg = print_int_card_blocks(fields_blocks)
-        self.assertTrue('SET1           a      1.       3       1       2       3\n', msg)
+        self.assertEqual('SET1           a      1.       3       1       2       3\n', msg)
+
+        fields_blocks = [
+            'SET1',
+            [['a', 1.0, 3], False], # these are not all integers
+            [[1, 2, 3, 5, 4], True], # these are all integers
+        ]
+        msg2 = print_int_card_blocks(fields_blocks)
+        #print '%r' % msg2
+        self.assertEqual('SET1           a      1.       3       1       2       3       5       4\n', msg2)
+
+        fields_blocks = [
+            'SET1',
+            [['a', 1.0, 3], False], # these are not all integers
+            [[1, 2, 3, 5, 4, 6], True], # these are all integers
+        ]
+        msg3 = print_int_card_blocks(fields_blocks)
+        print '%r' % msg3
+        self.assertEqual('SET1           a      1.       3       1       2       3       5       4\n'
+                         '               6\n', msg3)
 
 if __name__ == '__main__':
     unittest.main()
