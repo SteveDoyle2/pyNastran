0d63a34e5b7b6525c7f64b37e1448ec7f72fa908
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2014-09-13 22:23:59 +0000

    updating versions

--- setup.py
+++ setup.py
@@ -38,7 +38,10 @@ setup(name='pyNastran',
                    'Intended Audience :: Science/Research',
                    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
                    'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 3'], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author=pyNastran.__author__,
       author_email=pyNastran.__email__,
