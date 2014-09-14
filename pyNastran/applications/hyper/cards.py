c300bedf765adb7dabbcf69c452ede38eb456ad2
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2014-09-14 07:11:51 +0000

    angle of panel formula should be correct now

--- pyNastran/applications/hyper/cards.py
+++ pyNastran/applications/hyper/cards.py
@@ -1,6 +1,6 @@
 from pyNastran.bdf.cards.baseCard import BaseCard
 from pyNastran.bdf.bdfInterface.assign_type import double, double_or_blank, string, integer
-from numpy import array, cross, sin, cos, dot
+from numpy import array, cross, sin, cos, dot, radians
 from numpy.linalg import norm
 
 
@@ -14,21 +14,42 @@ class FLOW(BaseCard):
             self.Vy = double(card, 4, 'Vy')
             self.Vz = double(card, 5, 'Vz')
         elif 'MACH' == self.mode:
-            self.Ma = double(card, 3, 'Ma')
-            self.alpha = double(card, 4, 'alpha')
-            self.beta = double(card, 5, 'beta')
-        self.p = double_or_blank(card, 6, 'p', 0.)
-        self.q = double_or_blank(card, 7, 'q', 0.)
-        self.r = double_or_blank(card, 8, 'r', 0.)
+            self.Nx = double(card, 3, 'Nx')
+            self.Ny = double(card, 4, 'Ny')
+            self.Nz = double(card, 5, 'Nz')
+
+            self.Ma = double(card, 6, 'Ma')
+            self.alpha = double(card, 7, 'alpha')
+            self.beta = double_or_blank(card, 8, 'beta', 0.)
+            self.phi = double_or_blank(card, 9, 'phi', 0.)
+        self.p = double_or_blank(card, 10, 'p', 0.)
+        self.q = double_or_blank(card, 11, 'q', 0.)
+        self.r = double_or_blank(card, 12, 'r', 0.)
+
+    def get_omega(self):
+        # p. 110
+        return array([self.p, self.q, self.r], dtype='float32')
 
     def get_V(self):
         a = 1.0
         if 'VELO' == self.mode:
-            V = array([self.Vx, self.Vy, self.Vz], dtype='float32')        
+            V = array([self.Vx, self.Vy, self.Vz], dtype='float32')
             M = V / a
         elif 'MACH' == self.mode:
-            M = array([1.0, 0., 0.]) * self.Ma
-            V = M * a * array([sin(beta), cos(alpha)*cos(beta), sin(alpha)], dtype='float32')
+            # p. 111
+            ca = cos(radians(self.alpha))
+            cb = cos(radians(self.beta))
+            cp = cos(radians(self.phi))
+
+            sa = sin(radians(self.alpha))
+            sb = sin(radians(self.beta))
+            sp = sin(radians(self.phi))
+            V = self.Ma * a * array([ca*cb,
+                                     sp*sa*cb+cp*sb,
+                                     cp*sa*cb-sp*sb], dtype='float32')
+            n = array([self.Nx, self.Ny, self.Nz])
+            n /= norm(n)
+            #V = dot(V, n)
         return V, a
 
     def get_Ma(self):
@@ -36,9 +57,9 @@ class FLOW(BaseCard):
 
     def rawFields(self):
         if self.mode == 'VELO':
-            cards = ['$FLOW', self.flow_id, self.mode, self.Vx, self.Vy, self.Vz, self.p, self.q, self.r]
+            cards = ['$FLOW', self.flow_id, self.mode, self.Vx, self.Vy, self.Vz, None, None, None, self.p, self.q, self.r]
         else:
-            cards = ['$FLOW', self.flow_id, self.mode, self.Ma, self.alpha, self.beta, self.p, self.q, self.r]
+            cards = ['$FLOW', self.flow_id, self.mode, self.Nx, self.Ny, self.Nz, self.Ma, self.alpha, self.beta, self.phi, self.p, self.q, self.r]
         return cards
 
 
@@ -68,7 +89,7 @@ class HYPER(BaseCard):
             #if self.Type not in ['NEWTON','PRANDTL-MEYER', 'CP']:
                 #raise RuntimeError('Type=%r' % Type)
             self.gammas.append(double_or_blank(card, i+2, 'gamma', 1.4))
-        
+
             _cp = None
             if Type == 'CP':
                 _cp = double(card, i+3, 'Cp')
