cdf97f6c1a90ec1cf0030f6ae85bd7f04c8c98e0
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2014-10-18 07:01:22 +0000

    fixing bar tests

--- pyNastran/bdf/test/cards/test_bars.py
+++ pyNastran/bdf/test/cards/test_bars.py
@@ -17,8 +17,13 @@ class TestBars(unittest.TestCase):
         lines = card.split('\n')
         card = bdf.process_card(lines)
         card2 = BDFCard(card)
-        with self.assertRaises(AssertionError):  # A=0, I12=0, K1=0
-            pbar = PBAR(card2)
+        pbar = PBAR(card2)
+        self.assertEqual(pbar.A, 0.), pbar.A
+        self.assertEqual(pbar.i12, 0.), pbar.i12
+        self.assertEqual(pbar.K1, None), pbar.K1
+        self.assertEqual(pbar.K2, None), pbar.K2
+        #with self.assertRaises(AssertionError):  # A=0, I12=0, K1=0
+            #pbar = PBAR(card2)
 
     def test_pbar_02(self):
         pid = 1
@@ -36,9 +41,8 @@ class TestBars(unittest.TestCase):
         card = print_card(fields)
         lines = card.split('\n')
         card = bdf.process_card(lines)
-        #print(card)
         card2 = BDFCard(card)
-        #print(card2)
+
         pbar = PBAR(card2)
         self.assertEqual(pbar.pid, 1)
         self.assertEqual(pbar.mid, 2)
@@ -54,8 +58,43 @@ class TestBars(unittest.TestCase):
         self.assertEqual(pbar.D2, 0.0)
         self.assertEqual(pbar.E1, 0.0)
         self.assertEqual(pbar.E2, 0.0)
-        self.assertEqual(pbar.K1, 1e8)
-        self.assertEqual(pbar.K2, 1e8)
+        self.assertEqual(pbar.K1, None)
+        self.assertEqual(pbar.K2, None)
+
+        #--------------------------------------------------------
+        A = 6.
+        I1 = 5.
+        I2 = 4.
+        J = 3.
+        nsm = 2.
+        c1=c2=d1=d2=e1=e2=f1=f2=None
+        k1=k2=1e2
+        i12 = 0.
+        fields = ['PBAR', pid, mid, A, I1, I2, J, nsm, None,
+                          c1, c2, d1, d2, e1, e2, f1, f2,
+                          k1, k2, i12]
+        card = print_card(fields)
+        lines = card.split('\n')
+        card = bdf.process_card(lines)
+        card3 = BDFCard(card)
+
+        pbar = PBAR(card3)
+        self.assertEqual(pbar.pid, 1)
+        self.assertEqual(pbar.mid, 2)
+        self.assertEqual(pbar.A, 6.0)
+        self.assertEqual(pbar.i1, 5.0)
+        self.assertEqual(pbar.i2, 4.0)
+        self.assertEqual(pbar.j, 3.0)
+        self.assertEqual(pbar.nsm, 2.0)
+        self.assertEqual(pbar.i12, 0.0)
+        self.assertEqual(pbar.C1, 0.0)
+        self.assertEqual(pbar.C2, 0.0)
+        self.assertEqual(pbar.D1, 0.0)
+        self.assertEqual(pbar.D2, 0.0)
+        self.assertEqual(pbar.E1, 0.0)
+        self.assertEqual(pbar.E2, 0.0)
+        self.assertEqual(pbar.K1, 1e2)
+        self.assertEqual(pbar.K2, 1e2)
 
 
 if __name__ == '__main__':  # pragma: no cover
