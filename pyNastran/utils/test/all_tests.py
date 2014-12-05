79a7e811d305825042b723e9a6719af7e6d438f2
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2014-12-05 21:23:34 +0000

    missed a file

---
+++ pyNastran/utils/test/all_tests.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+from pyNastran.utils.test.test_log import *
+from pyNastran.utils.test.test_utils import *
+
+
+if __name__ == "__main__":
+    import unittest
+    unittest.main()
+
