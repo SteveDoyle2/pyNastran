6ef1bb0207add862d0b0c0b6c2811d108132e6dd
mesheb82 <mesheb82@abe5364a-6225-a519-111c-932ebcde5b3b>
2015-03-03 21:31:38 +0000

    stl writer now uses the stl class so the writer is shorter and more capable

--- trunk/pyNastran/converters/nastran/nastran_to_stl.py
+++ trunk/pyNastran/converters/nastran/nastran_to_stl.py
@@ -1,23 +1,27 @@
 from six import iteritems
-from numpy import cross
-from numpy.linalg import norm
+from numpy import zeros, array
 from pyNastran.bdf.bdf import BDF
-
+from pyNastran.converters.stl.stl_reader import STLReader
 
 def nastran_to_stl_filename(bdf_filename, stl_filename, log=None):
     model = BDF(log=log)
     model.read_bdf(bdf_filename)
     #log.info('card_count = %s' % model.card_count)
 
-    f = open(stl_filename, 'wb')
-    positions = {}
+    nnodes = len(model.nodes)
+    nodes = zeros((nnodes, 3), dtype='float64')
+    elements = []
 
+    i = 0
+    nodeid_to_i_map = {}
     for node_id, node in sorted(iteritems(model.nodes)):
-        positions[node_id] = node.Position()
+        xyz = node.Position()
+        if xyz[0] < 0.001:
+            print(xyz, node_id)
+        nodes[i, :] = xyz
+        nodeid_to_i_map[node_id] = i
+        i += 1
 
-    f.write('solid nastran_model\n')
-
-    element_id = 1
     for eid, element in sorted(iteritems(model.elements)):
         if element.type in ['CQUADR']:
             continue
@@ -29,66 +33,23 @@ def nastran_to_stl_filename(bdf_filename, stl_filename, log=None):
             continue
         elif element.type in ['CQUAD4']:
             n1, n2, n3, n4 = element.nodeIDs()
-            n1 = positions[n1]
-            n2 = positions[n2]
-            n3 = positions[n3]
-            n4 = positions[n4]
-
-            #n = element.Normal()
-            n = cross(n2-n1, n3-n1)
-            n /= norm(n)
-
-            # 1-2-3
-            f.write('  facet normal %f %f %f %i\n' % ( n[0], n[1], n[2], element_id ))
-            f.write('    outer loop\n')
-            f.write('      vertex %f %f %f\n' % ( n1[0], n1[1], n1[2] ))
-            f.write('      vertex %f %f %f\n' % ( n2[0], n2[1], n2[2] ))
-            f.write('      vertex %f %f %f\n' % ( n3[0], n3[1], n3[2] ))
-            f.write('    endloop\n')
-            f.write('  endfacet\n')
-            element_id += 1
-
-            # 3-4-1
-            f.write('  facet normal %f %f %f %i\n' % ( n[0], n[1], n[2], element_id ))
-            f.write('    outer loop\n')
-            f.write('      vertex %f %f %f\n' % ( n3[0], n3[1], n3[2] ))
-            f.write('      vertex %f %f %f\n' % ( n4[0], n4[1], n4[2] ))
-            f.write('      vertex %f %f %f\n' % ( n1[0], n1[1], n1[2] ))
-            f.write('    endloop\n')
-            f.write('  endfacet\n')
-            element_id += 1
+            i1, i2, i3, i4 = nodeid_to_i_map[n1], nodeid_to_i_map[n2], nodeid_to_i_map[n3], nodeid_to_i_map[n3]
+            elements.append([i1, i2, i3])
+            elements.append([i3, i4, i1])
         elif element.type in ['CTRIA3', 'CTRIAR']:
             n1, n2, n3 = element.nodeIDs()
-            #solid nastran_model
-            #   facet normal -6.665299e-001 6.795624e-001 3.064844e-001
-            #      outer loop
-            #         vertex 8.142845e-002 2.731541e-001 1.190024e+001
-            #         vertex 8.186898e-002 2.727136e-001 1.190215e+001
-            #         vertex 8.467505e-002 2.754588e-001 1.190215e+001
-            #      endloop
-            #   endfacet
-            n1 = positions[n1]
-            n2 = positions[n2]
-            n3 = positions[n3]
-
-            #n = element.Normal()
-            n = cross(n2-n1, n3-n1)
-            n /= norm(n)
-
-            f.write('  facet normal %f %f %f %i\n' % ( n[0], n[1], n[2], element_id ))
-            f.write('    outer loop\n')
-            f.write('      vertex %f %f %f\n' % ( n1[0], n1[1], n1[2] ))
-            f.write('      vertex %f %f %f\n' % ( n2[0], n2[1], n2[2] ))
-            f.write('      vertex %f %f %f\n' % ( n3[0], n3[1], n3[2] ))
-            f.write('    endloop\n')
-            f.write('  endfacet\n')
-            element_id += 1
+            i1, i2, i3 = nodeid_to_i_map[n1], nodeid_to_i_map[n2], nodeid_to_i_map[n3]
+            elements.append([i1, i2, i3])
         else:
             print(element.type)
-    f.write('endsolid\n')
-    f.close()
+    elements = array(elements, dtype='int32')
+    stl = STLReader()
+    stl.nodes = nodes
+    stl.elements = elements
+    stl.write_stl(stl_filename)
+
 
 if __name__ == '__main__':  # pragma: no cover
-    bdf_filename = 'g278.bdf'
-    stl_filename = 'g278.stl'
+    bdf_filename = 'threePlugs.bdf'
+    stl_filename = 'threePlugs.stl'
     nastran_to_stl_filename(bdf_filename, stl_filename)
