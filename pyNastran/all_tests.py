db9059be30b27571d2e0689dc59bbdc4dd06cc73
SteveDoyle2 <mesheb82@gmail.com>
2015-04-16 23:04:18 -0700

    removing f06 tests

--- pyNastran/all_tests.py
+++ pyNastran/all_tests.py
@@ -5,7 +5,10 @@ from pyNastran.bdf.test.all_tests import *
 from pyNastran.op2.test.all_tests import *
 
 #f06
-from pyNastran.f06.test.all_tests import *
+try:
+    from pyNastran.f06.test.all_tests import *
+except ImportError:
+    pass
 
 #op4
 from pyNastran.op4.test.op4_test import TestOP4
