df0c3f3e1521a5ff5a68e5b6ada5061b05cbf1f2
SteveDoyle2 <mesheb82@gmail.com>
2015-04-25 13:09:36 -0700

    v0.7.2

--- pyNastran/__init__.py
+++ pyNastran/__init__.py
@@ -7,7 +7,7 @@ def get_git_revision_short_hash():
         ghash = 'no_checksum_error'
     return 'dev-%s' % ghash
 
-revision = get_git_revision_short_hash()
+#revision = get_git_revision_short_hash()
 __author__  = 'Steven Doyle, Al Danials, Marcin Gasiorek'
 __email__   = 'mesheb82@gmail.com'
 __desc__    = 'Nastran BDF/F06/OP2/OP4 File reader/editor/writer/viewer'
@@ -16,8 +16,8 @@ __copyright__   = 'Copyright %s; 2011-2015' % __license__
 __pyqt_copyright__ = 'Copyright GPLv3; 2011-2015'
 __releaseDate__ = '2015/4/xx'
 __releaseDate2__ = 'APRIL xx, 2015'
-__version__     = '0.7.2_%s' % revision
-#__version__     = '0.7.2'
+#__version__     = '0.7.2_%s' % revision
+__version__     = '0.7.2'
 __website__     = 'https://github.com/SteveDoyle2/pyNastran'
 
 is_release = True  ## turns on skipping of tables that aren't supported
