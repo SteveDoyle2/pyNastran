6f5fe03e9c92a2c553e88b91410e9967e241b8b8
SteveDoyle2 <mesheb82@gmail.com>
2015-05-05 00:03:53 -0700

    workin on binary parser

---
+++ pyNastran/converters/openvsp/adb_reader.py
@@ -0,0 +1,133 @@
+from pyNastran.op2.fortran_format import FortranFormat
+from struct import unpack
+from numpy import degrees
+
+class ADB_Reader(FortranFormat):
+    def __init__(self, log=None, debug=None):
+        self.log = log
+        self.debug = debug
+        self.n = 0
+
+    def read_adb(self, adb_filename):
+        self.f = open(adb_filename, 'rb')
+        n = 4
+        data = self.f.read(4)
+        version_code_small, = unpack('>i', data)
+        version_code_large, = unpack('<i', data)
+        if version_code_small == -123789456:
+            endian = '>'
+        elif version_code_large == -123789456:
+            endian = '<'
+        else:
+            print(version_code_small, version_code_large)
+        n = 40
+        data = self.f.read(n)
+        nnodes, ntris, nmach, nalpha, sref, cref, bref, xcg, ycg, zcg = unpack(endian + '4i6f', data)
+        print('nnodes=%s ntris=%s nmach=%s nalpha=%s' % (nnodes, ntris, nmach, nalpha))
+        print('sref=%s cref=%s bref=%s cg=(%s, %s, %s)' % (sref, cref, bref, xcg, ycg, zcg))
+        self.n += 40
+
+        nbeta = 1
+
+        fmt = '%s%if' % (endian, nmach)
+        data = self.f.read(4 * nmach)
+        machs = unpack(fmt, data)
+        self.n += 4 * nmach
+
+        fmt = '%s%if' % (endian, nalpha)
+        data = self.f.read(4 * nalpha)
+        alphas = unpack(fmt, data)
+        self.n += 4 * nalpha
+
+        fmt = '%s%if' % (endian, nbeta)
+        data = self.f.read(4 * nbeta)
+        betas = unpack(fmt, data)
+        self.n += 4 * nbeta
+        print('machs = %s' % machs)
+        print('alphas = %s' % degrees(alphas))
+        print('betas = %s' % betas)
+
+        self.show_ndata(200, types='ifs')
+        data = self.f.read(4)
+        self.n += 4
+        nwings, = unpack(endian + 'i', data)
+
+        data = self.f.read(4)
+        self.n += 4
+        nwings, = unpack(endian + 'i', data)
+
+        fmt = endian + 'i'
+        assert nwings < 10, nwings
+        print('nwings=%s' % nwings)
+        for i in range(nwings):
+            data = self.f.read(4)
+            j, = unpack(endian + 'i', data)
+            print('j = %r' % j)
+            assert j < 1000, j
+            self.n += 4
+
+            #data = self.f.read(j * 4)
+            #fmt = '%s%is' % (endian, j * 4)
+            #wing_name = unpack(fmt, data)
+            #print('wing_name = %r' % wing_name)
+
+            data = self.f.read(100)
+            fmt = '%s%is' % (endian, 100)
+            wing_name = unpack(fmt, data)
+            print('wing_name2 = %r' % wing_name)
+            self.n += j * 4
+
+        data = self.f.read(4)
+        self.n += 4
+        nbodies, = unpack(endian + 'i', data)
+        print('nbodies=%s' % nbodies)
+        assert nbodies < 10, nbodies
+        for i in range(nbodies):
+            data = self.f.read(4)
+            j, = unpack(endian + 'i', data)
+            print('j = %r' % j)
+            self.n += 4
+            assert j < 1000, j
+
+            data = self.f.read(j * 4)
+            fmt = '%s%is' % (endian, j * 4)
+            body_name = unpack(fmt, data)
+            self.n += j * 4
+
+        n = 64
+        data = self.f.read(n)
+        self.n += n
+        self.show_data(data, types='ifs')
+        print('--------')
+
+        if 0:
+            npath = 68
+            data = self.f.read(npath)
+            #self.show_data(data, types='s')
+            p = unpack('68s', data)
+            print('p = %r' % p)
+            print('--------')
+
+            n = 36
+            data = self.f.read(n)
+            self.show_data(data, types='ifs')
+
+            print('--------')
+            data = self.f.read(npath)
+            p = unpack('68s', data)
+            print('p = %r' % p)
+            self.show_data(data, types='s')
+            print('----------------')
+
+
+            self.show_ndata(300, types='ifs')
+
+
+def main():
+    adb_filename = 'model_DegenGeom.adb'
+    a = ADB_Reader()
+    a.read_adb(adb_filename)
+
+if __name__ == '__main__':
+    main()
+#WingGeom\x00tran/pyNastran/OpenVSP-3.1.0-win32/model_DegenGeom.history
