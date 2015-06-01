1bc567e786847752bb2f08a23bb4615ea91c8333
SteveDoyle2 <mesheb82@gmail.com>
2015-05-31 17:00:22 -0700

    minor updates

--- setup_user.py
+++ setup_user.py
@@ -39,6 +39,7 @@ setup(name='pyNastran',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
                    ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author=pyNastran.__author__,
