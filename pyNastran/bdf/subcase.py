7d3b1ed9d5bcccc93869f5bab30fdc48d9afe517
SteveDoyle2 <mesheb82@gmail.com>
2016-06-28 22:09:24 -0700

    BDF:  - fixed CTRIAX6 node ordering, which screwed up edges  - fixed case control set parsing bug
    minor cleanup for other files

--- pyNastran/bdf/subcase.py
+++ pyNastran/bdf/subcase.py
@@ -9,6 +9,7 @@ from numpy import ndarray
 from pyNastran.utils import integer_types
 from pyNastran.bdf.cards.base_card import deprecated
 from pyNastran.bdf.cards.collpase_card import collapse_thru_packs
+from pyNastran.bdf.bdf_interface.assign_type import interpret_value
 
 int_cards = (
     # these are cards that look like:
@@ -1239,25 +1240,33 @@ def expand_thru_case_control(set_value):
             set_value2.add(ivalue)
             continue
         ivalue = ivalue.strip()
-        if ivalue.isdigit():
-            ivalue = int(ivalue)
+        #print('  ivalue = %r' % ivalue)
+        ivalue = interpret_value(ivalue, card=str(set_value))
+        if isinstance(ivalue, integer_types):
+            #print('  isdigit')
+            #ivalue = int(ivalue)
             if not imin < ivalue < imax:
                 add_mode = True
-                #print('break...')
+                #print('  break...\n')
             if add_mode:
-                #print('adding %s' % ivalue)
+                #print('  adding %s' % ivalue)
                 set_value2.add(ivalue)
             else:
-                #print('removing %s' % ivalue)
+                #print('  removing %s' % ivalue)
                 set_value2.remove(ivalue)
                 imin = ivalue
-        else:
+        elif isinstance(ivalue, float):
+            assert add_mode is True, add_mode
+            set_value2.add(ivalue)
+        elif isinstance(ivalue, str):
+            #print('  not digit')
             if set_value is 'EXCLUDE':
                 msg = ('EXCLUDE is not supported on CaseControlDeck '
                        'SET card\n')
                 raise RuntimeError(msg)
             elif 'THRU' in ivalue:
                 svalues = ivalue.split()
+                #print('  svalues=%s' % svalues)
                 if len(svalues) == 3:
                     assert add_mode is True, add_mode
                     imin, thru, imax = svalues
