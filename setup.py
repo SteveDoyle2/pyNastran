1785455f1cb5aba29519ba7defdeb011d7e2e1d9
SteveDoyle2 <mesheb82@gmail.com>
2016-07-30 14:49:07 -0700

    bad package format

--- setup.py
+++ setup.py
@@ -97,14 +97,14 @@ try:
     ver = scipy.version.short_version
     if ver < '0.16.0':
         print("scipy.version.short_version = %r < '0.16.0'" % scipy.version.short_version)
-        py_packages.append('scipy >= 0.16.0, scipy < 0.18.0')
+        py_packages.append('scipy >= 0.16.0, < 0.18.0')
     elif ver == '0.18.0':  # v0.18.1 is not released
-        py_packages.append('scipy >= 0.16.0, scipy < 0.18.0')
+        py_packages.append('scipy >= 0.16.0, < 0.18.0')
     #elif ver == '0.18.0':  # v0.18.1 is released
         #py_packages.append('scipy > 0.18.0')
         #raise NotImplementedError('scipy has a bug in v0.18.0')
 except ImportError:
-    py_packages.append('scipy >= 0.16.0, scipy < 0.18.0')
+    py_packages.append('scipy >= 0.16.0, < 0.18.0')
 
 try:
     import six
