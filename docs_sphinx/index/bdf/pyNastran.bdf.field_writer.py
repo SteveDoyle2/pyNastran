41f8ea1eb7951b52014c28215233560d6d04cd06
Buran1988 <volk@buran.fr>
2016-08-08 17:46:52 +0300

    Revert "Python 2 to Python 3 compatibility"
    This reverts commit a797226e8aa6881b9cbcf3caa618472edafd7e65.

--- docs_sphinx/index/bdf/pyNastran.bdf.field_writer.py
+++ docs_sphinx/index/bdf/pyNastran.bdf.field_writer.py
@@ -1,48 +1,48 @@
-#bdf/field_writer Package
-#=================
+bdf/field_writer Package
+=================
 
-#This is the pyNastran.bdf.rst file.
+This is the pyNastran.bdf.rst file.
 
-#:mod:`fieldWriter` Module
-#-------------------------
+:mod:`fieldWriter` Module
+-------------------------
 
-#.. inheritance-diagram:: pyNastran.bdf.fieldWriter
+.. inheritance-diagram:: pyNastran.bdf.fieldWriter
 
-#.. automodule:: pyNastran.bdf.fieldWriter
-#    :members:
-#    :private-members:
-#    :undoc-members:
-#    :show-inheritance:
+.. automodule:: pyNastran.bdf.fieldWriter
+    :members:
+    :private-members:
+    :undoc-members:
+    :show-inheritance:
 
-#:mod:`field_writer_8` Module
-#----------------------------
+:mod:`field_writer_8` Module
+----------------------------
 
-#.. inheritance-diagram:: pyNastran.bdf.field_writer_8
+.. inheritance-diagram:: pyNastran.bdf.field_writer_8
 
-#.. automodule:: pyNastran.bdf.field_writer_8
-#    :members:
-#    :private-members:
-#    :undoc-members:
-#    :show-inheritance:
+.. automodule:: pyNastran.bdf.field_writer_8
+    :members:
+    :private-members:
+    :undoc-members:
+    :show-inheritance:
 
-#:mod:`field_writer_16` Module
-#-----------------------------
+:mod:`field_writer_16` Module
+-----------------------------
 
-#.. inheritance-diagram:: pyNastran.bdf.field_writer_16
+.. inheritance-diagram:: pyNastran.bdf.field_writer_16
 
-#.. automodule:: pyNastran.bdf.field_writer_16
-#    :members:
-#    :private-members:
-#    :undoc-members:
-#    :show-inheritance:
+.. automodule:: pyNastran.bdf.field_writer_16
+    :members:
+    :private-members:
+    :undoc-members:
+    :show-inheritance:
 
-#:mod:`field_writer_double` Module
-#---------------------------------
+:mod:`field_writer_double` Module
+---------------------------------
 
-#.. inheritance-diagram:: pyNastran.bdf.field_writer_double
+.. inheritance-diagram:: pyNastran.bdf.field_writer_double
 
-#.. automodule:: pyNastran.bdf.field_writer_double
-#    :members:
-#    :private-members:
-#    :undoc-members:
-#    :show-inheritance:
+.. automodule:: pyNastran.bdf.field_writer_double
+    :members:
+    :private-members:
+    :undoc-members:
+    :show-inheritance:
