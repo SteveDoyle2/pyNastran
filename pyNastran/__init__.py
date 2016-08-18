493d32e1da0049e53139d2057a1ce7a1702e460c
SteveDoyle2 <mesheb82@gmail.com>
2016-08-18 00:43:18 -0700

    updating

--- pyNastran/__init__.py
+++ pyNastran/__init__.py
@@ -3,32 +3,9 @@ import sys
 
 # this variable is automatically set by the .spec file; should be False
 is_pynastrangui_exe = False
-if is_pynastrangui_exe:
-    # pyInstaller
-    from pyNastran.version import __version__, __releaseDate__
-else:
-    import subprocess
-
-    def get_git_revision_short_hash():
-        try:
-            #ghash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
-
-            # independent of pyNastran location as long as there is a git folder
-            #   what about if you use setup_user.py install?
-            #   what about if you don't have git?
-            # can raise a subprocess.CalledProcessError, which means the return code != 0
-            ghash = subprocess.check_output(['git', 'describe', '--always'], cwd=os.path.dirname(__file__))
-
-            ghash = ghash.decode('utf-8').rstrip()
-        except:
-            # git isn't installed
-            ghash = 'no.checksum.error'
-        return 'dev.%s' % ghash
-
-    revision = get_git_revision_short_hash()
-    __version__ = '0.8.0+%s' % revision
-    __releaseDate__ = '2016/4/xx'
-    __releaseDate2__ = 'APRIL xx, 2016'
+__version__ = '0.8.0'
+__releaseDate__ = '2016/8/21'
+__releaseDate2__ = 'AUGUST 21, 2016'
 
 __author__  = 'Steven Doyle, Al Danials, Marcin Gasiorek, hurlei, saullocastro, Paul Blelloch, Nikita Kalutsky'
 __email__   = 'mesheb82@gmail.com'
