84e4f0184aed65f38c5d265a69fa723c9ab66dbc
SteveDoyle2 <mesheb82@gmail.com>
2016-09-07 09:52:24 -0700

    adding option to set subcases to read_op2 and read_op2_geom

--- pyNastran/op2/op2.py
+++ pyNastran/op2/op2.py
@@ -124,7 +124,7 @@ from pyNastran.op2.errors import SortCodeError, DeviceCodeError, FortranMarkerEr
 from pyNastran.op2.op2_f06_common import Op2F06Attributes
 
 
-def read_op2(op2_filename=None, combine=True,
+def read_op2(op2_filename=None, combine=True, subcases=None,
              log=None, debug=True, debug_file=None, build_dataframe=False,
              skip_undefined_matrices=True, mode='msc', encoding=None):
     """
@@ -138,6 +138,9 @@ def read_op2(op2_filename=None, combine=True,
         True : objects are isubcase based
         False : objects are (isubcase, subtitle) based;
                 will be used for superelements regardless of the option
+    subcases : List[int, ...] / int; default=None->all subcases
+        list of [subcase1_ID,subcase2_ID]
+
     build_dataframe : bool; default=False
         builds a pandas DataFrame for op2 objects
     skip_undefined_matrices : bool; default=False
@@ -163,6 +166,7 @@ def read_op2(op2_filename=None, combine=True,
                does not have so many methods
     """
     model = OP2(log=log, debug=debug, debug_file=debug_file, mode=mode)
+    model.set_subcases(subcases)
     model.read_op2(op2_filename=op2_filename, build_dataframe=build_dataframe,
                    skip_undefined_matrices=skip_undefined_matrices, combine=combine,
                    encoding=encoding)
@@ -571,7 +575,7 @@ class OP2(OP2_Scalar):
             result = getattr(self, result_type)
             case_keys = list(result.keys())
             try:
-                case_keys = sorted(case_keys)
+                case_keys = sorted(case_keys)  # TODO: causes DeprecationWarning
             except TypeError:
                 self.log.error('result.keys() = %s' % case_keys)
 
