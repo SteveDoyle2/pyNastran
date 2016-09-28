e817dceecd1ad5bde085fa14da2002a6203afeef
SteveDoyle2 <mesheb82@gmail.com>
2016-09-28 00:26:16 -0700

    working on PKNL

--- pyNastran/f06/parse_flutter.py
+++ pyNastran/f06/parse_flutter.py
@@ -77,17 +77,19 @@ class FlutterResponse(object):
 
         self.subcase = subcase
         self.configuration = configuration
-        self.mach = mach
-        print('mach=%s' % mach)
-        self.xysym = xysym
-        self.xzsym = xzsym
+        if method == 'PK':
+            self.mach = mach
+            self.xysym = xysym
+            self.xzsym = xzsym
+            self.density_ratio = density_ratio
+            print('mach=%s' % mach)
+
         self.method = method
         self.modes = np.asarray(modes, dtype='int32')
-        self.density_ratio = density_ratio
         rho_ref = 1.
 
         self.ikfreq = 0
-        self.iinvkfreq = 1
+        self.ikfreq_inv = 1
 
         #print(results)
         results = np.asarray(results, dtype='float64')
@@ -103,7 +105,6 @@ class FlutterResponse(object):
             results[:, :, self.ivelocity] *= kvel
 
         elif self.method == 'PKNL':
-            raise NotImplementedError(method)
             self.idensity = 2
             self.imach = 3
             self.ivelocity = 4
@@ -127,17 +128,22 @@ class FlutterResponse(object):
             altitude_units = self.out_units['altitude']
             #density_units1 = self.out_units['density']
 
-            kdensity = self._get_altitude_unit_factor(density_units1, 'slug/ft^3')
+            kdensity = self._get_density_unit_factor(density_units1, 'slug/ft^3')
             #kdensity = self._get_altitude_unit_factor(density_units2, 'slug/ft^3')
             kalt = self._get_altitude_unit_factor('ft', altitude_units)
-            kpressure = self._get_unit_factor('dynamic_pressure')[0]
+            kvel = self._get_unit_factor('velocity')[0]
+            #kpressure = self._get_unit_factor('dynamic_pressure')[0]
+            kpressure = kdensity * kvel ** 2
 
-            alt = np.array(
-                [get_alt_for_density(densityi, nmax=20) * kalt
-                 for densityi in rho.ravel() * kdensity], dtype='float64').reshape(vel.shape)
+            make_alt = False
+            if make_alt:
+                alt = np.array(
+                    [get_alt_for_density(densityi, nmax=20) * kalt
+                     for densityi in rho.ravel() * kdensity], dtype='float64').reshape(vel.shape)
 
-            # TODO: this is wrong...
-            self.results = np.hstack([results, eas, q * kpressure, alt])
+                self.results = np.dstack([results, eas, q * kpressure, alt])
+            else:
+                self.results = np.dstack([results, eas, q * kpressure])
         else:
             raise NotImplementedError(method)
         #print(self.results.shape)
@@ -287,7 +293,7 @@ class FlutterResponse(object):
         if unit_in not in ['slinch/in^3', 'slug/ft^3']:
             msg = 'unit_in=%r not in [slinch/in^3, slug/ft^3]' % unit_in
             raise NotImplementedError(msg)
-        if unit_out not in ['ft', 'm']:
+        if unit_out not in ['slinch/in^3', 'slug/ft^3']:
             msg = 'unit_out=%r not in [slinch/in^3, slug/ft^3]' % unit_out
             raise NotImplementedError(msg)
 
@@ -387,26 +393,41 @@ class FlutterResponse(object):
             modes = np.asarray(modes)
 
         if fig is None:
-            plt.figure()
+            fig = plt.figure()
+        axes = fig.add_subplot(111)
 
         self._set_xy_limits(xlim, ylim)
         imodes = np.searchsorted(self.modes, modes)
         symbols = self.symbols
 
         for i, imode, mode in zip(count(), imodes, modes):
+            symbol = symbols[i]
             real = self.results[imode, :, self.ieigr].ravel()
             imag = self.results[imode, :, self.ieigi].ravel()
-            plt.plot(real, imag, symbols[i], label='Mode %i' % mode)
+            axes.plot(real, imag, symbol, label='Mode %i' % mode, markersize=0)
 
-        plt.grid(True)
-        plt.xlabel('Eigenvalue (Real)')
-        plt.ylabel('Eigenvalue (Imaginary)')
+            s = np.linspace(.75, 50., len(real))
+            #assert symbol[2] == '-', symbol
+            axes.scatter(real, imag, s=s, color=symbol[0], marker=symbol[1])
+
+        axes.grid(True)
+        axes.set_xlabel('Eigenvalue (Real)')
+        axes.set_ylabel('Eigenvalue (Imaginary)')
 
         title = 'Subcase %i' % self.subcase
         if png_filename:
             title += '\n%s' % png_filename
-        plt.suptitle(title)
-        self._finalize_plot(legend, show, png_filename, clear)
+        fig.suptitle(title)
+        #self._finalize_plot(legend, show, png_filename, clear)
+        if legend:
+            axes.legend()
+
+        if show:
+            plt.show()
+        if png_filename:
+            plt.savefig(png_filename)
+        if clear:
+            plt.clear()
 
     def plot_vg_vf(self, fig=None, modes=None, show=None, png_filename=None,
                    clear=False, legend=None,
@@ -424,7 +445,7 @@ class FlutterResponse(object):
         else:
             modes = np.asarray(modes)
         if fig is None:
-            fig = plt.figure(self.subcase) # figsize=(12,9)
+            fig = plt.figure() # figsize=(12,9), self.subcase
             gridspeci = gridspec.GridSpec(2, 4)
             damp_axes = fig.add_subplot(gridspeci[0, :3])
             freq_axes = fig.add_subplot(gridspeci[1, :3], sharex=damp_axes)
@@ -434,6 +455,8 @@ class FlutterResponse(object):
         symbols = self.symbols
 
         _kvelocity, velocity_units = self._get_unit_factor('velocity')
+
+        legend_items = ['Mode %i' % mode for mode in modes]
         for i, imode, mode in zip(count(), imodes, modes):
             vel = self.results[imode, :, self.ivelocity].ravel()
             damping = self.results[imode, :, self.idamping].ravel()
@@ -452,6 +475,7 @@ class FlutterResponse(object):
         freq_axes.set_xlabel('Velocity [%s]' % velocity_units)
         freq_axes.set_ylabel('Frequency [Hz]')
         freq_axes.grid(True)
+
         if xlim is not None:
             freq_axes.set_xlim(xlim)
         if ylim_freq is not None:
@@ -466,10 +490,13 @@ class FlutterResponse(object):
 
 #self._finalize_plot(legend, show, png_filename, clear)
         if legend:
-            #damp_axes.legend(legend_items, fontsize=10, bbox_to_anchor=(1.125, 1.), loc=2)
+            damp_axes.legend(legend_items, fontsize=10, bbox_to_anchor=(1.125, 1.), loc=2)
             #fig.subplots_adjust(hspace=0.25)
             #fig.subplots_adjust(hspace=.5)
-            plt.legend()
+            #plt.legend()
+            #damp_axes.legend(legend_items, bbox_to_anchor=anchor, ncol=2)
+            #fig.subplots_adjust(hspace=0.25)
+            #fig.subplots_adjust(hspace=.5)
 
         if show:
             plt.show()
@@ -501,8 +528,8 @@ def plot_flutter_f06(f06_filename, modes=None,
     TODO: support multiple subcases
     TODO: support long tables
     """
-    f06_units = {'velocity' : 'in/s'}
-    out_units = {'velocity' : 'in/s'}
+    f06_units = {'velocity' : 'in/s', 'density' : 'slinch/in^3'}
+    out_units = {'velocity' : 'in/s', 'density' : 'slug/ft^3', 'altitude' : 'ft', 'dynamic_pressure' : 'psf'}
 
     flutters = []
     with open(f06_filename, 'r') as f06_file:
@@ -562,10 +589,20 @@ def plot_flutter_f06(f06_filename, modes=None,
 
             point_sline = f06_file.readline().split()
             mode = int(point_sline[2])
-            mach = float(point_sline[6])
-            density_ratio = float(point_sline[10])
-            method = point_sline[13]
-            print(mode, mach, density_ratio, method)
+            method = point_sline[-1]  # 13 for PN, 5 for PK
+            #print(point_sline)
+            if method == 'PK':
+                mach = float(point_sline[6])
+                density_ratio = float(point_sline[10])
+                #method = point_sline[13]
+                print(mode, mach, density_ratio, method)
+            elif method == 'PKNL':
+                mach = None
+                density_ratio = None
+                print(mode, method)
+                f06_file.readline()
+            else:
+                raise NotImplementedError(point_sline)
             if 1:
                 if mode in modes:
                     print('found existing mode...')
@@ -590,9 +627,12 @@ def plot_flutter_f06(f06_filename, modes=None,
             sline = [None] * nvalues
             while len(sline) == nvalues:
                 sline = f06_file.readline().split()
-                if 'PAGE' not in sline and 'INFORMATION' not in sline and 'EIGENVALUE' not in sline:
+                if (sline
+                    and 'PAGE' not in sline
+                    and 'INFORMATION' not in sline
+                    and 'EIGENVALUE' not in sline):
+                    #print('sline = %s' % sline)
                     lines.append(sline)
-                    #print(sline)
 
             results.append(lines)
             #print('')
