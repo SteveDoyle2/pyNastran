bf69f448f7503ae2abf86a92574a66347b7f9521
SteveDoyle2 <mesheb82@gmail.com>
2016-12-08 15:57:19 -0800

    simplifying object printer

--- pyNastran/utils/dev.py
+++ pyNastran/utils/dev.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import os
 from six import string_types, iteritems
 
-from numpy import array, ndarray
+import numpy as np
 
 from pyNastran.utils import object_attributes
 
@@ -36,8 +36,8 @@ def get_files_of_type(dirname, extension='.txt', max_size=100., limit_file='no_d
     allow_digging = True
     if limit_file in filenames:
         allow_digging = False
-    for f in filenames:
-        filename = os.path.join(dirname, f)
+    for filenamei in filenames:
+        filename = os.path.join(dirname, filenamei)
         if os.path.isdir(filename):
             if allow_digging:
                 filenames2 += get_files_of_type(filename, extension, max_size)
@@ -45,7 +45,7 @@ def get_files_of_type(dirname, extension='.txt', max_size=100., limit_file='no_d
             else:
                 print('no digging in filename=%s; dirname=%s' % (filename, dirname))
         elif (os.path.isfile(filename) and
-              os.path.splitext(f)[1].endswith(extension) and
+              os.path.splitext(filenamei)[1].endswith(extension) and
               os.path.getsize(filename) / 1048576. <= max_size):
             filenames2.append(filename)
     return filenames2
@@ -79,7 +79,8 @@ def list_print(lst, float_fmt='%-4.2f'):
             print("parameter = %r" % val)
             raise
 
-    try: # TODO: remove try block and fix bug in OP2 code or add a warning message
+    try:
+        # TODO: remove try block and fix bug in OP2 code or add a warning message
         if len(lst) == 0:
             return '[]'
 
@@ -92,7 +93,9 @@ def list_print(lst, float_fmt='%-4.2f'):
         return _print(lst)
 
 
-def write_class(name, obj, nspaces=0, nbase=0):
+def write_class(obj, nspaces=0):
+    """write a class' public and private members"""
+    #print('write_class')
     object_type = obj.__class__.__name__
     obj_attrs = object_attributes(obj, 'both')
     if not obj_attrs:
@@ -100,79 +103,115 @@ def write_class(name, obj, nspaces=0, nbase=0):
 
     spaces = ' ' * nspaces
     nspaces2 = nspaces + 4
-    #spaces2 = nspaces2 * ' '
+    if nspaces == 0:
+        nspaces2 = 4
+    else:
+        nspaces2 = nspaces + 4
+    spaces2 = ' ' * nspaces2
     msg = "%s(\n" % object_type
-    for attr in obj_attrs[:-1]:
+    for attr in obj_attrs:
         value = getattr(obj, attr)
-        #msg += '?'
-        msg += write_object_attributes(attr, value, nspaces2, nbase, is_class=True)
-    attr = obj_attrs[-1]
-    value = getattr(obj, attr)
-    msg += write_object_attributes(attr, value, nspaces2, nbase, is_class=True)
-    msg += '%s)' % spaces
-
-    #print("dir(obj) =", dir(obj))
-    #print("obj_attrs =", obj_attrs)
+        msg += '%s%s = ' % (spaces2, attr)
+        msg += write_value(value, nspaces2) + ',\n'
+    msg += '%s)' % (spaces)
     return msg
 
 
-def write_object_attributes(attr, obj, nspaces, nbase=0, is_class=False):
-    msg = ''
+def write_class_attribute(attr, obj, nspaces=0):
+    """writes a class attribute"""
+    spaces = nspaces * ' '
+    if isinstance(attr, str):
+        msg = '%s = ' % str(attr)
+    else:
+        msg = '%s = ' % str(attr)
+
     if isinstance(obj, (int, float)) or obj is None:
-        msg += '%s' % (str(obj))
+        msg += '%s%s,\n' % (spaces, obj)
     elif isinstance(obj, string_types):
-        msg += "'%s'" % obj
-    elif isinstance(obj, unicode):
-        msg += "u'%s'" % obj
+        msg += "%s%r,\n" % (spaces, obj)
     elif isinstance(obj, list):
-        msg += write_list(obj, nspaces, nbase, is_class)
+        msg += write_list(obj, nspaces+4) + ',\n'
     elif isinstance(obj, tuple):
-        msg += write_tuple(obj, nspaces, nbase, is_class)
+        msg += write_tuple(obj, nspaces+4) + ',\n'
     elif isinstance(obj, dict):
-        msg += write_dict(obj, nspaces, nbase, is_class)
+        msg += write_dict(obj, nspaces) + ',\n'
+    elif isinstance(obj, np.ndarray):
+        msg += str(obj) + ',\n'
     else:
         object_type = type(obj)
-        #raise RuntimeError('object_type=%s is not supported; value=%s' % (objec_type, obj))
+        raise RuntimeError('object_type=%s is not supported; value=%s' % (object_type, obj))
     return msg
 
 
-def write_dict(obj, nspaces, nbase, is_class):
-    spaces = (nbase+nspaces) * ' '
-    nspaces2 = nspaces + 4
+def write_dict(obj, nspaces):
+    """writes a dict"""
+    spaces = (nspaces) * ' '
     if len(obj) == 0:
         return '{}'
 
+    spaces2 = (nspaces + 4) * ' '
     msg = '{\n'
     for key, value in sorted(iteritems(obj)):
-        #msg += '#'
-        msg += write_object_attributes(key, value, nspaces2, nbase, is_class=False)
-    msg += '%s}' % spaces
+        if isinstance(key, string_types):
+            msg += '%s%r : ' % (spaces2, key)
+        elif isinstance(key, tuple):
+            msg += '%s%s : ' % (spaces2, write_value(key, nspaces))
+        else:
+            msg += '%s%s : ' % (spaces2, key)
+        msg += write_value(value, nspaces+4) + ',\n'
+    msg += '%s}' % (spaces)
     return msg
 
 
-def write_list(obj, nspaces, nbase, is_class):
+def write_list(obj, nspaces):
+    """writes a list"""
     if len(obj) == 0:
         return '[]'
-    return ' ' * (nspaces + nbase) + "???"  # don't choke on long numpy arrays
 
-    #spaces = ' ' * (nspaces + nbase)
-    #msg = '[\n%s    ' % spaces
-    #for value in obj[:-1]:
-        #msg += write_value(value, nspaces+4, nbase, is_class) + ', '
-    #msg += write_value(obj[-1], nspaces+4, nbase, is_class) + '\n%s]' % spaces
-    #return msg
-
-
-def write_tuple(obj, nspaces, nbase, is_class):
-    msg = '('
-    for value in obj:
-        msg += write_value(value, nspaces, nbase, is_class) + ', '
-    msg += ')'
+    spaces = ' ' * (nspaces)
+    spaces2 = ' ' * (nspaces+4)
+    msg = '[\n%s' % (spaces2)
+    for value in obj[:-1]:
+        msg += '%s, ' % write_value(value, nspaces+4)
+    msg += write_value(obj[-1], nspaces+4) + ',\n%s]' % spaces
     return msg
 
 
+def write_tuple(obj, nspaces):
+    """writes a tuple"""
+    msg = '('
+    spaces = ' ' * (nspaces)
+    for value in obj[:-1]:
+        msg += write_value(value, nspaces+4) + ', '
+    msg += write_value(obj[-1], nspaces+4) + ')'
+    return msg
+
+
+def write_value(value, nspaces):
+    """writes a generalized value (int, float, string, dict, tuple, list, ndarray, object)"""
+    if isinstance(value, string_types):
+        out = '%r' % value
+    elif isinstance(value, (int, float)):
+        out = '%s' % value
+    elif isinstance(value, dict):
+        out = write_dict(value, nspaces)
+    elif isinstance(value, tuple):
+        out = write_tuple(value, nspaces)
+    elif isinstance(value, list):
+        out = write_list(value, nspaces)
+    elif isinstance(value, np.ndarray):
+        out = write_array(value, nspaces)
+    elif value is None:
+        out = 'None'
+    elif isinstance(value, object):
+        out = write_class(value, nspaces)
+    else:
+        raise NotImplementedError(type(value))
+    return out
+
 def write_array(a, nspaces=0):
-    return ' '*nspaces + '[???]'
+    """writes a numpy ndarray"""
+    return '[???]'
     #shape = a.shape
     #dtype = a.dtype
     #if len(shape) == 1:
@@ -221,31 +260,34 @@ def write_array(a, nspaces=0):
     #return msg
 
 def main():
+    """the test function"""
     from numpy import zeros
     class C(object):
+        """dummy class"""
         def __init__(self):
             pass
     class B(object):
+        """dummy class"""
         def __init__(self, x=None, e=None):
             self.x = 4
             self.e = C()
 
     class A(object):
+        """dummy class"""
         def __init__(self, a=None, b=None, c=None, d=None):
             self.a = a
             self.b = b
             self.c = c
             self.d = {
-                'a' : 4,
-                'b' : [1, 2, 3],
-                'c' : {1 : 2},
-                'd' : B(),
+                'd1' : 4,
+                'd2' : [1, 2, 3],
+                'd3' : {1 : 2},
+                'd4' : B(),
                 (1, 2) : 4,
             }
 
     z = zeros(2, dtype='float64')
-    #print(z)
-    #print(z.dtype)
+
     dict_a = {
         'strString' : 'a string',
         'strFloat' : 1.0,
@@ -255,19 +297,19 @@ def main():
         'strClass' : A('a', 'b', 'c'),
         'strList' : [1, 2, 3],
         'nullList' : [],
-        'nullArray' : array([]),
-        'stringArray' : array(['s']),
-        'stringArray2' : array(['a', 'b']),
+        'nullArray' : np.array([]),
+        'stringArray' : np.array(['s']),
+        'stringArray2' : np.array(['a', 'b']),
         'nullDict' : {},
         u'unicodStr' : u'',
         'ListOfLists' : [[[], [[]], 2, {'a':3}]],
         1 : 1,
         None : 4,
-        1.0 : 5,
+        1.01 : 5,
         (1, 2) : 6,
-        'strArray' : array([4, 5, 6]),
-        'strArray2' : zeros((2, 2)),
-        'strArray3' : zeros((2, 2, 2)),
+        'strArray' : np.array([4, 5, 6]),
+        'strArray2' : np.zeros((2, 2)),
+        'strArray3' : np.zeros((2, 2, 2)),
     }
     dict_b = {
         'string2' : 'a string',
@@ -275,7 +317,6 @@ def main():
         'int2': 2,
         'dictA' : dict_a,
     }
-
 
     dict_c = {
         'dictA' : {
@@ -292,12 +333,12 @@ def main():
         'float2' : 1.0,
         'int2' : 2,
         'string2' : 'a string',
+        'dict_b' : dict_b,
     }
     #assert sorted(dictB.items())==sorted(dictC.items())
-    #print(write_object_attributes('dictA', dictA, is_class=False))
+    #print(write_object_attributes('dictA', dictA))
     nspaces = 0
-    msg = write_object_attributes('dictB', dict_b, nspaces, nbase=0)
-    print(msg)
+    msg = write_class_attribute('dictC', dict_c)
     with open('junk.py', 'wb') as file_obj:
         file_obj.write(msg)
 
