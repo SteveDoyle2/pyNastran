d9caf15eeac2cd154d2e3ed75b8e29b833ba561f
SteveDoyle2 <mesheb82@gmail.com>
2016-12-15 13:53:57 -0800

    updating docs

--- docs/html_docs/quick_start/matlab.rst
+++ docs/html_docs/quick_start/matlab.rst
@@ -4,10 +4,116 @@ Matlab
 
  * pyNastran also supports Matlab through the Matlab/Python interface.  [Information about setting up Matlab with Python can be found here.](http://www.mathworks.com/help/matlab/matlab-engine-for-python.html?s_tid=gn_loc_drop)
 
+Working around Matlab's oddities
+================================
+Replace the base redirectstdout.m file (that for my installation is located in the following folder):
+
+    C:\Program Files\MATLAB\MATLAB Production Server\R2015a\toolbox\matlab\external\interfaces\python\+python\+internal\redirectstdout.m
+
+with this file: https://github.com/SteveDoyle2/pyNastran/tree/master/docs/pyNastran_in_MATLAB_example/redirectstdout/redirectstdout.m
+
+Also, instead of imports like:
+
+    >>> import py.pyNastran.op2.op2.OP2
+
+use:
+
+    >>> import py.pyNastran.op2.op2.OP2.*
+    >>> clear
+    >>> import py.pyNastran.op2.op2.OP2.*
+    >>> clear import
+    >>> import py.pyNastran.op2.op2.OP2
+
+If you don't need all this insanity, please post and say what you did.
 
 Example 1 - BDF
 ===============
-Not done...
+This example demonstrates how to call the BDF class and extract velocity, machs, and densities (FLFACT cards) from a SOL 145 deck
+
+.. code-block:: matlab
+
+    ..
+
+        >>> rho, velocity, mach, nmodes = get_flutter_bdf("model_145.bdf");
+
+    function [Density,Velocity,Mach,Nmodes] = get_flutter_bdf(filenamebdf)
+        %get_flutter_bdf Reads bdf and provides Flutter condition from FLFACT
+
+        import py.pyNastran.bdf.bdf.BDF % import BDF class
+        import py.numpy.asarray % import as array function (convert list into a ndarray)
+
+        % Instantiate an BDF class
+        bdf = BDF();
+
+        %% Read the BDF
+        bdf.read_bdf(filenamebdf);
+
+        % NMODES (is valid only if RESVEC = NO, that is no residual augmentation)
+        EIGRL_CARDS_ID = ndarray2mat(asarray(py.list(bdf.methods.keys())));
+        RESVEC_tuple = bdf.case_control_deck.get_subcase_parameter(0,'RESVEC');
+        RESVEC_cell = RESVEC_tuple.cell;
+        RESVEC_RH = char(RESVEC_tuple{1}); % RH side of the RESVEC case command
+
+
+        if numel(EIGRL_CARDS_ID) ~= 1
+            errordlg('If bdf contains more than 1 EIGRL cards, the software does not work. (Things are much more complicated)');
+            return
+        elseif strcmp(RESVEC_RH,'NO')~=1
+                errordlg('If bdf does not contain RESVEC = NO commands, the software does not work. (Things are much more complicated)');
+            return
+        end
+
+        Nmodes = double(bdf.Method(EIGRL_CARDS_ID).nd);
+
+
+        % FLUTTER SETUP
+
+        FLUTTER_CARDS_ID = ndarray2mat(asarray(py.list(bdf.flutters.keys())));
+
+        if numel(FLUTTER_CARDS_ID) ~= 1
+
+            errordlg('If bdf contains more than 1 FLUTTER cards, the software does not work. (Things are much more complicated)');
+            return
+
+        end
+
+
+        % DENSITY
+        Density_fact = ndarray2mat(bdf.FLFACT(1).factors);
+        % Density_fact = [1 2];
+
+        % MACH
+        Mach_fact = ndarray2mat(bdf.FLFACT(2).factors);
+        % Mach_fact = [1 2];
+
+
+
+        % VELOCITY
+        Velocity_fact = ndarray2mat(bdf.FLFACT(3).factors);
+        Velocity_fact(Velocity_fact>0) = [];
+        Velocity_fact = -1*Velocity_fact; % In the op2 NASTRAN gives only the eigenvalues/eigenvector associated to negative velocity (sic!)
+
+
+
+        if strcmp(char(bdf.flutters{FLUTTER_CARDS_ID}.method),'PK')
+
+            tmpdensity = repmat(Density_fact(:),numel(Velocity_fact)*numel(Mach_fact),1);
+            Density = reshape(tmpdensity,numel(Density_fact)*numel(Velocity_fact)*numel(Mach_fact),1);
+            tmpvelocity = repmat(Velocity_fact(:),numel(Density_fact),numel(Mach_fact));
+            Velocity = reshape(tmpvelocity',numel(Density_fact)*numel(Velocity_fact)*numel(Mach_fact),1);
+            tmpmach = repmat(Mach_fact(:),1,numel(Density_fact)*numel(Velocity_fact));
+            Mach = reshape(tmpmach',numel(Density_fact)*numel(Velocity_fact)*numel(Mach_fact),1);
+
+        else
+            strcmp(char(bdf.flutters{FLUTTER_CARDS_ID}.method),'PKNL');
+            Density = Density_fact;
+            Velocity = Velocity_fact;
+            Mach = Mach_fact;
+        end
+
+    end
+
+
 
 Example 2 - OP2
 ===============
@@ -18,9 +124,7 @@ This example demonstrates how to call the OP2 class and extract the eigenvectors
 
     ..
 
-        >>> from pyNastran.bdf.bdf import BDF
-        >>> model = BDF()
-        >>> model.read_bdf(bdf_filename)
+        >>> eigs, eigvs = get_eigenvalues_eigenvectors("model_145.op2");
 
     function [eigs,eigvs] = get_eigenvalues_eigenvectors(filenameop2)
 
--- docs/pyNastran_in_MATLAB_example/get_flutter_bdf.m
+++ docs/pyNastran_in_MATLAB_example/get_flutter_bdf.m
@@ -1,77 +1,67 @@
 function [Density,Velocity,Mach,Nmodes] = get_flutter_bdf(filenamebdf)
-%get_flutter_bdf Reads bdf and provides Flutter condition from FLFACT
+    %get_flutter_bdf Reads bdf and provides Flutter condition from FLFACT
 
-import py.pyNastran.bdf.bdf.BDF % import BDF class
-import py.numpy.asarray % import as array function (convert list into a ndarray)
+    import py.pyNastran.bdf.bdf.BDF % import BDF class
+    import py.numpy.asarray % import as array function (convert list into a ndarray)
 
-% Instantiate an BDF class
-bdf = BDF();
+    % Instantiate an BDF class
+    bdf = BDF();
 
-%% Read the BDF
-bdf.read_bdf(filenamebdf);
+    %% Read the BDF
+    bdf.read_bdf(filenamebdf);
 
-% NMODES (is valid only if RESVEC = NO, that is no residual augmentation)
-EIGRL_CARDS_ID = ndarray2mat(asarray(py.list(bdf.methods.keys())));
-RESVEC_tuple = bdf.case_control_deck.get_subcase_parameter(0,'RESVEC');
-RESVEC_cell = RESVEC_tuple.cell;
-RESVEC_RH = char(RESVEC_tuple{1}); % RH side of the RESVEC case command
+    % NMODES (is valid only if RESVEC = NO, that is no residual augmentation)
+    EIGRL_CARDS_ID = ndarray2mat(asarray(py.list(bdf.methods.keys())));
+    RESVEC_tuple = bdf.case_control_deck.get_subcase_parameter(0,'RESVEC');
+    RESVEC_cell = RESVEC_tuple.cell;
+    RESVEC_RH = char(RESVEC_tuple{1}); % RH side of the RESVEC case command
 
+    if numel(EIGRL_CARDS_ID) ~= 1
+        errordlg('If bdf contains more than 1 EIGRL cards, the software does not work. (Things are much more complicated)');
+        return
+    elseif strcmp(RESVEC_RH,'NO')~=1
+            errordlg('If bdf does not contain RESVEC = NO commands, the software does not work. (Things are much more complicated)');
+        return
+    end
 
-if numel(EIGRL_CARDS_ID) ~= 1
-    errordlg('If bdf contains more than 1 EIGRL cards, the software does not work. (Things are much more complicated)');
-    return
-elseif strcmp(RESVEC_RH,'NO')~=1
-        errordlg('If bdf does not contain RESVEC = NO commands, the software does not work. (Things are much more complicated)');
-    return
-end
-
-Nmodes = double(bdf.Method(EIGRL_CARDS_ID).nd);
-
-
-% FLUTTER SETUP
-
-FLUTTER_CARDS_ID = ndarray2mat(asarray(py.list(bdf.flutters.keys())));
-
-if numel(FLUTTER_CARDS_ID) ~= 1
-
-    errordlg('If bdf contains more than 1 FLUTTER cards, the software does not work. (Things are much more complicated)');
-    return
-
-end
+    Nmodes = double(bdf.Method(EIGRL_CARDS_ID).nd);
 
 
-% DENSITY
-Density_fact = ndarray2mat(bdf.FLFACT(1).factors);
-% Density_fact = [1 2];
+    % FLUTTER SETUP
+    FLUTTER_CARDS_ID = ndarray2mat(asarray(py.list(bdf.flutters.keys())));
 
-% MACH
-Mach_fact = ndarray2mat(bdf.FLFACT(2).factors);
-% Mach_fact = [1 2];
-
-
-
-% VELOCITY
-Velocity_fact = ndarray2mat(bdf.FLFACT(3).factors);
-Velocity_fact(Velocity_fact>0) = [];
-Velocity_fact = -1*Velocity_fact; % In the op2 NASTRAN gives only the eigenvalues/eigenvector associated to negative velocity (sic!)
+    if numel(FLUTTER_CARDS_ID) ~= 1
+        errordlg('If bdf contains more than 1 FLUTTER cards, the software does not work. (Things are much more complicated)');
+        return
+    end
 
 
+    % DENSITY
+    Density_fact = ndarray2mat(bdf.FLFACT(1).factors);
+    % Density_fact = [1 2];
 
-if strcmp(char(bdf.flutters{FLUTTER_CARDS_ID}.method),'PK')
+    % MACH
+    Mach_fact = ndarray2mat(bdf.FLFACT(2).factors);
+    % Mach_fact = [1 2];
 
-    tmpdensity = repmat(Density_fact(:),numel(Velocity_fact)*numel(Mach_fact),1);
-    Density = reshape(tmpdensity,numel(Density_fact)*numel(Velocity_fact)*numel(Mach_fact),1);
-    tmpvelocity = repmat(Velocity_fact(:),numel(Density_fact),numel(Mach_fact));
-    Velocity = reshape(tmpvelocity',numel(Density_fact)*numel(Velocity_fact)*numel(Mach_fact),1);
-    tmpmach = repmat(Mach_fact(:),1,numel(Density_fact)*numel(Velocity_fact));
-    Mach = reshape(tmpmach',numel(Density_fact)*numel(Velocity_fact)*numel(Mach_fact),1);
+    % VELOCITY
+    Velocity_fact = ndarray2mat(bdf.FLFACT(3).factors);
+    Velocity_fact(Velocity_fact>0) = [];
+    Velocity_fact = -1*Velocity_fact; % In the op2 NASTRAN gives only the eigenvalues/eigenvector associated to negative velocity (sic!)
 
-else
-    strcmp(char(bdf.flutters{FLUTTER_CARDS_ID}.method),'PKNL');
-    Density = Density_fact;
-    Velocity = Velocity_fact;
-    Mach = Mach_fact;
-end
 
+    if strcmp(char(bdf.flutters{FLUTTER_CARDS_ID}.method),'PK')
+        tmpdensity = repmat(Density_fact(:),numel(Velocity_fact)*numel(Mach_fact),1);
+        Density = reshape(tmpdensity,numel(Density_fact)*numel(Velocity_fact)*numel(Mach_fact),1);
+        tmpvelocity = repmat(Velocity_fact(:),numel(Density_fact),numel(Mach_fact));
+        Velocity = reshape(tmpvelocity',numel(Density_fact)*numel(Velocity_fact)*numel(Mach_fact),1);
+        tmpmach = repmat(Mach_fact(:),1,numel(Density_fact)*numel(Velocity_fact));
+        Mach = reshape(tmpmach',numel(Density_fact)*numel(Velocity_fact)*numel(Mach_fact),1);
+    else
+        strcmp(char(bdf.flutters{FLUTTER_CARDS_ID}.method),'PKNL');
+        Density = Density_fact;
+        Velocity = Velocity_fact;
+        Mach = Mach_fact;
+    end
 end
 
