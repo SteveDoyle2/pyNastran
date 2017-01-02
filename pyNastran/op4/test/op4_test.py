c803fda3d365899902bc9e23a87b7d4e936b7e73
SteveDoyle2 <mesheb82@gmail.com>
2017-01-02 13:56:16 -0800

    removing old code

--- pyNastran/op4/test/op4_test.py
+++ pyNastran/op4/test/op4_test.py
@@ -12,51 +12,6 @@ import pyNastran.op4.test
 op4_path = pyNastran.op4.test.__path__[0]
 
 
-#def pass_test1():
-    #fh = cOP4(os.path.abspath('mat_b_dn.op4'), 'r')
-    #fh.print_header()
-    ##print fh.nmat = 9
-
-    ## crash
-    #a, b, c = fh.Load(nmat=3, skip=0)
-    #print(a)
-    #print(b)
-    #print(c)
-
-
-#def failed_test1():
-    #fh = cOP4('mat_b_dn.op4', 'r')
-    #fh.print_header()
-    ##print fh.nmat = 9
-
-    #a, b, c = fh.Load(nmat=3, skip=0)
-    #print(a)
-    #print(b)
-    #print(c)
-
-
-#def pass_test2():
-    #fh = cOP4(os.path.abspath('mat_b_dn.op4'), 'r')
-    ##print fh.nmat = 9
-
-    ## crash with "unnamed is sparse, skipping for now"
-    #(a, b, c, d, f, g, h, i) = fh.Load(nmat=9, skip=0)
-    #print(a)
-    #print(b)
-    #print(c)
-
-
-#def failed_test2():
-    #fh = cOP4('mat_b_dn.op4', 'r')
-    ##print fh.nmat = 9
-
-    ## ValueError:  need more than 8 values to unpack
-    #(a, b, c, d, f, g, h, i) = fh.Load(nmat=9, skip=0)
-    #print(a)
-    #print(b)
-    #print(c)
-
-
 class TestOP4(unittest.TestCase):
 
     @staticmethod
