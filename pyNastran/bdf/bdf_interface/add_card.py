a1c8a3b146bdf2521506c32b73ebffa678bbba7d
SteveDoyle2 <mesheb82@gmail.com>
2017-01-13 21:42:41 -0800

    odd...fixing bug

--- pyNastran/bdf/bdf_interface/add_card.py
+++ pyNastran/bdf/bdf_interface/add_card.py
@@ -2011,7 +2011,7 @@ class AddCards(AddMethods):
         self._add_rigid_element_object(elem)
         return elem
 
-    def __init__(self, eid, independent_nid, dependent_nids, dependent_components,
+    def add_rspline(self, eid, independent_nid, dependent_nids, dependent_components,
                  diameter_ratio=0.1, comment=''):
         elem = RSPLINE(eid, independent_nid, dependent_nids, dependent_components,
                        diameter_ratio=diameter_ratio, comment=comment)
