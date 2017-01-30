ed656ef61a2ea5bf23a9f754f42d81d7dff8ec75
SteveDoyle2 <mesheb82@gmail.com>
2017-01-30 14:52:07 -0800

    starting z rotation for PCOMPs

--- pyNastran/bdf/mesh_utils/export_mcids.py
+++ pyNastran/bdf/mesh_utils/export_mcids.py
@@ -8,7 +8,7 @@ import numpy as np
 from pyNastran.bdf.bdf import read_bdf
 
 
-def export_mcids(bdf_filename, csv_filename_out=None):
+def export_mcids(bdf_filename, csv_filename_out=None, iply=0):
     """
     Exports the element material coordinates systems, so you can
     load it into pyNastranGUI.
@@ -19,6 +19,8 @@ def export_mcids(bdf_filename, csv_filename_out=None):
         a bdf filename or BDF model
     csv_filename_out : str
         the path to the output csv
+    iply : int; default=0
+        the ply to consider
 
     TODO: add CTRIA3 support
     """
@@ -56,21 +58,49 @@ def export_mcids(bdf_filename, csv_filename_out=None):
             dxyz43 = np.linalg.norm(xyz4 - xyz3)
             dxyz14 = np.linalg.norm(xyz1 - xyz4)
             dxyz = np.mean([dxyz21, dxyz32, dxyz43, dxyz14]) / 2.
-            centroid, imat, jmat, _normal = elem.material_coordinate_system()
+            centroid, imat, jmat, normal = elem.material_coordinate_system()
+
+            consider_property_rotation = False
+            if consider_property_rotation:
+                # TODO: not tested
+                pid_ref = elem.pid_ref
+                if pid_ref.type == 'PSHELL':
+                    thetad = 0.0
+                    if isinstance(elem.theta_mcid, float):
+                        thetad = elem.theta_mcid
+                elif pid_ref.type == 'PCOMP':
+                    thetad = pid_ref.thetas[0]
+                else:
+                    raise NotImplementedError('property type=%r is not supported\n%s%s' % (elem.pid_ref.type, elem, elem.pid_ref))
+                theta = np.radians(thetad)
+                c = np.cos(theta)
+                s = np.sin(theta)
+
+                R = np.array([
+                    [c, -s, 0.],
+                    [s, c, 0.],
+                    [0., 0., 1.]
+                ], dtype='float64')
+
+                R1 = np.vstack([imat, jmat, _normal])
+                R2 = np.dot(R, R1)
+                imat2 = R2[0, :]
+                jmat2 = R2[1, :]
 
             iaxis = centroid + imat * dxyz
             jaxis = centroid + jmat * dxyz
             nodes.append((nid, centroid[0], centroid[1], centroid[2]))
             nodes.append((nid + 1, iaxis[0], iaxis[1], iaxis[2]))
             nodes.append((nid + 2, jaxis[0], jaxis[1], jaxis[2]))
-            bars.append((eid, nid, nid + 1))
-            bars.append((eid + 1, nid, nid + 2))
+            bars.append((eid, nid, nid + 1))  # x-axis
+            bars.append((eid + 1, nid, nid + 2))  # y-axis
             nid += 3
             eid += 2
+
         elif elem.type in skip_types:
             pass
         else:
-            raise NotImplementedError('element type=%r is not supported' % elem)
+            raise NotImplementedError('element type=%r is not supported\n%s' % (elem.type, elem))
 
     if csv_filename_out:
         with open(csv_filename_out, 'w') as out_file:
