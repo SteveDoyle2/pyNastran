e4eca8cd99cea9139ca4a0d519082eabea01805e
SteveDoyle2 <mesheb82@gmail.com>
2017-02-25 16:22:33 -0800

    bumping year

--- pyNastran/__init__.py
+++ pyNastran/__init__.py
@@ -35,7 +35,7 @@ __email__   = 'mesheb82@gmail.com'
 __desc__    = 'Nastran BDF/F06/OP2/OP4 File reader/editor/writer/viewer'
 __license__     = 'LGPLv3'
 __copyright__   = 'Copyright %s; 2011-2016' % __license__
-__pyqt_copyright__ = 'Copyright GPLv3; 2011-2016'
+__pyqt_copyright__ = 'Copyright GPLv3; 2011-2017'
 __website__     = 'https://github.com/SteveDoyle2/pyNastran'
 
 is_release = True  ## turns on skipping of tables that aren't supported
