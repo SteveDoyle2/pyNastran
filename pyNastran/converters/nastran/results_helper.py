fixing modal solutions (e.g., SOL 103) calculating eigr instead of eign.  The formula to calculate frequency is different...
# in Hz
freq = abs(eigi) / (2*pi)
freq = sqrt(abs(eign)) / (2*pi)

it's not:
freq = sqrt(abs(eigr)) / (2*pi)
which makes sense if you think of a spring-mass damper problem.  The real eigenvalue is related to the damping.  A free vibration problem has a pure imaginary eigenvalue pairs.

pyNastran/converters/nastran/results_helper.py
@@ -516,13 +516,13 @@ class NastranGuiResults(NastranGuiAttributes):
         elif hasattr(case, 'cycles'):
             header += '; freq = %g Hz' % case.cycles[itime]
         elif hasattr(case, 'eigis'):
-            freq = case.eigis[itime]
-            cycle = np.abs(freq) / (2. * np.pi)
+            eigi = case.eigis[itime]
+            cycle = np.abs(eigi) / (2. * np.pi)
             header += '; freq = %g Hz' % cycle
-        #elif hasattr(case, 'eigrs'):  # TODO: is this right; it's what it used to be...
-            #eigr = case.eigrs[itime]
-            #cycle = np.sqrt(np.abs(eigr)) / (2. * np.pi)  # is this right?
-            #header += '; freq = %g Hz' % cycle
+        elif hasattr(case, 'eigns'):  #  eign is not eigr; it's more like eigi
+            eigi2 = case.eigrs[itime] #  but |eigi| = sqrt(|eign|)
+            cycle = np.sqrt(np.abs(eigr2)) / (2. * np.pi)
+            header += '; freq = %g Hz' % cycle
 
         elif hasattr(case, 'lftsfqs') or hasattr(case, 'lsdvmns') or hasattr(case, 'loadIDs'):
             pass

pyNastran/op2/tables/oug/oug.py
@@ -54,7 +54,7 @@ class OUG(OP2Common):
         value = getattr(self, name)
         if value == 0.0:
             #print('table_name=%r mode=%s eigr=%s' % (self.table_name, self.mode, self.eigr))
-            value = np.sqrt(np.abs(self.eigr)) / (2. * np.pi)
+            value = np.sqrt(np.abs(self.eign)) / (2. * np.pi)
             setattr(self, name, value)
             self.data_code[name] = value
 
@@ -97,12 +97,12 @@ class OUG(OP2Common):
         elif self.analysis_code == 2:  # real eigenvalues
             # mode number
             self.mode = self.add_data_parameter(data, 'mode', 'i', 5)
-            # real eigenvalue
-            self.eigr = self.add_data_parameter(data, 'eigr', 'f', 6, False)
+            # eigenvalue
+            self.eign = self.add_data_parameter(data, 'eign', 'f', 6, False)
             # mode or cycle .. todo:: confused on the type - F1???
             self.mode_cycle = self.add_data_parameter(data, 'mode_cycle', 'i', 7, False)
             self.update_mode_cycle('mode_cycle')
-            self.data_names = self.apply_data_code_value('data_names', ['mode', 'eigr', 'mode_cycle'])
+            self.data_names = self.apply_data_code_value('data_names', ['mode', 'eign', 'mode_cycle'])
         #elif self.analysis_code == 3: # differential stiffness
             #self.lsdvmn = self.get_values(data, 'i', 5) ## load set number
             #self.data_code['lsdvmn'] = self.lsdvmn