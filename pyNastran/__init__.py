e1048037e281793a7ef958ad1c18802dfda9d68c
SteveDoyle2 <mesheb82@gmail.com>
2017-05-24 22:09:31 -0700

    updating license

--- pyNastran/__init__.py
+++ pyNastran/__init__.py
@@ -7,35 +7,17 @@ if is_pynastrangui_exe:
     # pyInstaller
     from pyNastran.version import __version__, __releaseDate__
 else:
-    import subprocess
-
-    def get_git_revision_short_hash():
-        try:
-            #ghash = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
-
-            # independent of pyNastran location as long as there is a git folder
-            #   what about if you use setup_user.py install?
-            #   what about if you don't have git?
-            # can raise a subprocess.CalledProcessError, which means the return code != 0
-            ghash = subprocess.check_output(['git', 'describe', '--always'], cwd=os.path.dirname(__file__))
-
-            ghash = ghash.decode('utf-8').rstrip()
-        except:
-            # git isn't installed
-            ghash = 'no.checksum.error'
-        return 'dev.%s' % ghash
-
-    revision = get_git_revision_short_hash()
-    __version__ = '0.9.0+%s' % revision
-    __releaseDate__ = '2016/8/xx'
-    __releaseDate2__ = 'AUGUST xx, 2016'
+    __version__ = '1.0.0'
+    __releaseDate__ = '2017/5/25'
+    __releaseDate2__ = 'MAY 25, 2017'
 
 __author__  = 'Steven Doyle, hurlei,  Saullo Castro, Paul Blelloch, Nikita Kalutsky'
 __email__   = 'mesheb82@gmail.com'
 __desc__    = 'Nastran BDF/F06/OP2/OP4 File reader/editor/writer/viewer'
-__license__     = 'LGPLv3'
-__copyright__   = 'Copyright %s; 2011-2016' % __license__
-__pyqt_copyright__ = 'Copyright GPLv3; 2011-2017'
+__license__     = 'BSD-3'
+__copyright__   = 'Copyright %s; 2011-2017' % __license__
+__pyside_copyright__ = 'Copyright LGPLv3 - pySide'
+__pyqt_copyright__ = 'Copyright GPLv3 - PyQt'
 __website__     = 'https://github.com/SteveDoyle2/pyNastran'
 
 is_release = True  ## turns on skipping of tables that aren't supported
