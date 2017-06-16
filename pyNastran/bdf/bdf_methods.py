9366983edb828efe1804a0d85f6e84bc9bfd0616
SteveDoyle2 <mesheb82@gmail.com>
2017-06-16 12:56:58 -0700

    reorganizing skin solid elements

--- pyNastran/bdf/bdf_methods.py
+++ pyNastran/bdf/bdf_methods.py
@@ -16,7 +16,6 @@ from __future__ import (nested_scopes, generators, division, absolute_import,
                         print_function, unicode_literals)
 from collections import defaultdict
 from copy import deepcopy
-from codecs import open
 
 from six import iteritems, PY2
 from typing import List, Tuple, Any, Union, Dict
@@ -28,9 +27,7 @@ from pyNastran.bdf.methods.mass_properties import (
     _mass_properties_elements_init, _mass_properties_no_xref, _apply_mass_symmetry,
     _mass_properties, _mass_properties_new)
 from pyNastran.bdf.methods.loads import sum_forces_moments, sum_forces_moments_elements
-
-from pyNastran.bdf.field_writer_8 import print_card_8
-from pyNastran.bdf.field_writer_16 import print_card_16
+from pyNastran.bdf.mesh_utils.skin_solid_elements import write_skin_solid_faces
 
 
 class BDFMethods(BDFAttributes):
@@ -419,31 +416,12 @@ class BDFMethods(BDFAttributes):
     def _mass_properties_new(self, element_ids=None, mass_ids=None,
                              reference_point=None,
                              sym_axis=None, scale=None, xyz_cid0=None):  # pragma: no cover
+        """not done"""
         mass, cg, I = _mass_properties_new(
             self, element_ids=element_ids, mass_ids=mass_ids,
             reference_point=reference_point,
             sym_axis=sym_axis, scale=scale, xyz_cid0=xyz_cid0)
         return (mass, cg, I)
-
-    def resolve_grids(self, cid=0):
-        """
-        Puts all nodes in a common coordinate system (mainly for cid testing)
-
-        Parameters
-        ----------
-        cid : int; default=0
-            the cid to resolve the nodes to
-
-        .. note::
-
-           loses association with previous coordinate systems so to go
-           back requires another FEM
-        """
-        assert cid in self.coords, ('cannot resolve nodes to '
-                                    'cid=%r b/c it doesnt exist' % cid)
-        for nid, node in sorted(iteritems(self.nodes)):
-            p = node.get_position_wrt(self, cid)
-            node.set_position(self, p, cid)
 
     #def __gravity_load(self, loadcase_id):
         #"""
@@ -618,60 +596,6 @@ class BDFMethods(BDFAttributes):
                         eid_faces.append((eid, face))
         return eid_faces
 
-    def get_solid_skin_faces(self):
-        """
-        Gets the elements and faces that are skinned from solid elements.
-        This doesn't include internal faces or existing shells.
-
-        Returns
-        -------
-        eid_set : Dict[tuple(int, int, ...)] = List[int]
-           key : sorted face
-           value : list of element ids with that face
-        face_map : Dict[tuple(int, int, ...)] = List[int]
-           key : sorted face
-           value : unsorted face
-        """
-        eid_faces = self.get_element_faces()
-        face_set = defaultdict(int)
-        eid_set = defaultdict(list)
-        face_map = {}
-        for eid, face in eid_faces:
-            #print(eid, face)
-            raw_face = deepcopy(face)
-            try:
-                face.sort()
-            except:
-                print('face = %s' % str(face))
-                raise
-            tface = tuple(face)
-            #print(tface)
-            face_set[tface] += 1
-            eid_set[tface].append(eid)
-            face_map[tface] = raw_face
-
-        #print('eid_set:')
-        #for tface, eidset in iteritems(eid_set):
-            #print(tface, eidset)
-
-        #print('face_set:')
-        #for tface, faceset in iteritems(face_set):
-            #print(tface, faceset)
-
-        #print('face_map:')
-        #for tface, facemap in iteritems(face_map):
-            #print(tface, facemap)
-
-        del_faces = []
-        for face, face_count in iteritems(face_set):
-            if face_count == 2:
-                del_faces.append(face)
-
-        for face in del_faces:
-            del face_set[face]
-            del eid_set[face]
-        return eid_set, face_map
-
     def write_skin_solid_faces(self, skin_filename,
                                write_solids=False, write_shells=True,
                                size=8, is_double=False, encoding=None):
@@ -695,204 +619,7 @@ class BDFMethods(BDFAttributes):
         encoding : str; default=None -> system default
             the string encoding
         """
-        if(len(self.element_ids) == 0 or len(self.material_ids) == 0 or
-           len(self.property_ids) == 0):
-            return
-        eid_set, face_map = self.get_solid_skin_faces()
-        if len(eid_set) == 0:
-            return
-
-        eid_set_to_write = set([])
-        nid_set_to_write = set([])
-        mid_set_to_write = set([])
-        if write_solids:
-            for face, eids in iteritems(eid_set):
-                eid_set_to_write.update(eids)
-                for eid in eids:
-                    elem = self.elements[eid]
-                    pid = elem.Pid()
-                    prop = self.properties[pid] # PSOLID
-                    mid = prop.Mid()
-                    #print(prop)
-                    nid_set_to_write.update(elem.node_ids)
-                    mid_set_to_write.add(mid)
-                    #print('added_mid (a) =', mid)
-        elif write_shells:
-            for face, eids in iteritems(eid_set):
-                eid_set_to_write.update(eids)
-                nid_set_to_write.update(face)
-                for eid in eids:
-                    elem = self.elements[eid]
-                    pid = elem.Pid()
-                    prop = self.properties[pid] # PSOLID
-                    if prop.type in ['PSOLID', 'PLSOLID']:
-                        mid = prop.Mid()
-                    elif prop.type == 'PCOMPS':
-                        mid = prop.mids[0]
-                    else:
-                        raise NotImplementedError(prop)
-                    #except TypeError:
-                        #self.log.warning('TypeError: skipping:%s' % prop)
-                        #raise
-                    #except AttributeError:
-                        #self.log.warning('skipping:%s' % prop)
-                        #continue
-                    mid_set_to_write.add(mid)
-                    #print('added eid=%s pid=%s mid=%s (b)' % (eid, pid, mid))
-        else:
-            raise RuntimeError('write_solids=False write_shells=False')
-
-        eids_to_write = list(eid_set_to_write)
-        nids_to_write = list(nid_set_to_write)
-        mids_to_write = list(mid_set_to_write)
-
-        #element_ids_to_delete = set(self.element_ids) - eids_to_write
-
-        eid_shell = max(self.elements) + 1
-        pid_shell = max(self.properties) + 1
-        mid_shell = max(self.materials) + 1
-        self._write_skin_solid_faces(skin_filename, face_map,
-                                     nids_to_write, eids_to_write, mids_to_write, eid_set,
-                                     eid_shell, pid_shell, mid_shell,
-                                     write_solids=write_solids, write_shells=write_shells,
-                                     size=size, is_double=is_double, encoding=encoding)
-
-    def _write_skin_solid_faces(self, skin_filename, face_map,
-                                nids_to_write, eids_to_write, mids_to_write, eid_set,
-                                eid_shell, pid_shell, mid_shell,
-                                write_solids=False, write_shells=True,
-                                size=8, is_double=False, encoding=None):
-        """
-        helper method for ``write_skin_solid_faces``
-
-        Parameters
-        ----------
-        skin_filename : str
-            the file to write
-        face_map : dict[sorted_face] : face
-            sorted_face : List[int, int, int] / List[int, int, int, int]
-            face : List[int, int, int] / List[int, int, int, int]
-
-        nids_to_write : List[int, int, ...]
-            list of node ids to write
-        eids_to_write : List[int, int, ...]
-            list of element ids to write
-        mids_to_write : List[int, int, ...]
-            list of material ids to write
-        eid_set : ???
-            ???
-
-        eid_shell : int
-            the next id to use for the shell id
-        pid_shell : int
-            the next id to use for the shell property
-        mid_shell : int
-            the next id to use for the shell material
-
-        write_solids : bool; default=False
-            write solid elements that have skinned faces
-        write_shells : bool; default=True
-            write shell elements
-        size : int; default=8
-            the field width
-        is_double : bool; default=False
-            double precision flag
-        encoding : str; default=None -> system default
-            the string encoding
-        """
-        encoding = self.get_encoding(encoding)
-        if PY2:
-            wb = 'wb'
-        else:
-            wb = 'w'
-        with open(skin_filename, wb, encoding=encoding) as bdf_file:
-            bdf_file.write('$ pyNastran: punch=True\n')
-            for nid in sorted(nids_to_write):
-                if nid is None:
-                    continue
-                node = self.nodes[nid]
-                bdf_file.write(node.write_card(size=size, is_double=is_double))
-
-            for cid, coord in iteritems(self.coords):
-                if cid == 0:
-                    continue
-                bdf_file.write(coord.write_card(size=size, is_double=is_double))
-
-            if write_solids:
-                for eid in sorted(eids_to_write):
-                    elem = self.elements[eid]
-                    bdf_file.write(elem.write_card(size=size))
-                for pid, prop in iteritems(self.properties):
-                    bdf_file.write(prop.write_card(size=size, is_double=is_double))
-                for mid in sorted(mids_to_write):
-                    material = self.materials[mid]
-                    bdf_file.write(material.write_card(size=size, is_double=is_double))
-                del eid, pid, mid
-
-            if write_shells:
-                mids_to_write.sort()
-                for imid, mid in enumerate(mids_to_write):
-                    card = ['PSHELL', pid_shell + imid, mid_shell + imid, 0.1]
-                    try:
-                        msg = print_card_8(card)
-                    except RuntimeError:
-                        msg = print_card_16(card)
-                    bdf_file.write(msg)
-
-                    card = ['MAT1', mid_shell + imid, 3.e7, None, 0.3]
-                    #bdf_file.write(self.materials[mid].comment)
-                    try:
-                        msg = print_card_8(card)
-                    except RuntimeError:
-                        msg = print_card_16(card)
-                    bdf_file.write(msg)
-
-                for face, eids in iteritems(eid_set):
-                    face_raw = face_map[face]
-                    nface = len(face)
-                    #assert len(eids) == 1, eids
-                    #for eid in sorted(eids):
-                        #elem = self.elements[eid]
-                        #print(elem)
-                        #break
-
-                    #elem = next(itervalues(self.elements)) # old
-                    elem = self.elements[eids[0]]
-                    #pid = next(iterkeys(self.properties))
-                    pid = elem.Pid()
-                    prop = self.properties[pid]
-                    try:
-                        mid = prop.Mid()
-                    except AttributeError:
-                        continue
-                    #print('mids_to_write = %s' % mids_to_write)
-                    #print('mids = ', self.materials.keys())
-                    imid = mids_to_write.index(mid)
-
-                    if nface == 3:
-                        card = ['CTRIA3', eid_shell, pid_shell + imid] + list(face_raw)
-                    elif nface == 4:
-                        card = ['CQUAD4', eid_shell, pid_shell + imid] + list(face_raw)
-                    elif nface == 4:
-                        card = ['CQUAD4', eid_shell, pid_shell + imid] + list(face_raw)
-                    elif nface == 6:
-                        card = ['CTRIA6', eid_shell, pid_shell + imid] + list(face_raw)
-                    elif nface == 8:
-                        card = ['CQUAD8', eid_shell, pid_shell + imid] + list(face_raw)
-                    else:
-                        raise NotImplementedError('face=%s len(face)=%s' % (face, nface))
-                    try:
-                        msg = print_card_8(card)
-                    except RuntimeError:
-                        msg = print_card_16(card)
-                    bdf_file.write(msg)
-                    eid_shell += 1
-
-                    #elem = self.elements[eid]
-                    #bdf_file.write(elem.write_card(size=size))
-                #for pid, prop in iteritems(self.properties):
-                    #bdf_file.write(prop.write_card(size=size, is_double=is_double))
-            bdf_file.write('ENDDATA\n')
-        #if 0:
-            #model = self.__class__.__init__()
-            #model.read_bdf(skin_filename)
+        return write_skin_solid_faces(
+            self, skin_filename,
+            write_solids=write_solids, write_shells=write_shells,
+            size=size, is_double=is_double, encoding=encoding)
