8aa781d729c3bcece654a115611da7996ca27b14
SteveDoyle2 <mesheb82@gmail.com>
2017-06-16 09:11:27 -0700

    removing aero_panel_buckling; bit too specific

--- pyNastran/applications/all_tests.py
+++ pyNastran/applications/all_tests.py
@@ -1,7 +1,6 @@
 import unittest
 
 from pyNastran.applications.test.test_rainflow import TestRainflow
-from pyNastran.applications.aero_panel_buckling.test_panel_buckling import TestPanelBuckling
 
 
 if __name__ == '__main__':
