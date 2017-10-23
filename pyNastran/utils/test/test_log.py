ffbec8aebe3c7f11ced13fdb4bbabac5add76b00
SteveDoyle2 <mesheb82@gmail.com>
2017-10-22 23:03:09 -0700

    tests

--- pyNastran/utils/test/test_log.py
+++ pyNastran/utils/test/test_log.py
@@ -1,27 +1,31 @@
-# -*- coding: utf-8 -*-
+"""tests log.py"""
 
 import os
 import unittest
 
+from pyNastran.utils.log import make_log
 import pyNastran
 from pyNastran.bdf.bdf import BDF
-pkg_path = pyNastran.__path__[0]
 
-bdf_filename = os.path.join(pkg_path, '..', 'models', 'solid_bending', 'solid_bending.bdf')
+PKG_PATH = pyNastran.__path__[0]
+BDF_FILENAME = os.path.join(PKG_PATH, '..', 'models', 'solid_bending', 'solid_bending.bdf')
 
 class TestLog(unittest.TestCase):
 
     @staticmethod
     def test_log_01():
         model = BDF(debug=True)
-        model.read_bdf(bdf_filename)
+        model.read_bdf(BDF_FILENAME)
 
         model2 = BDF(debug=False)
-        model2.read_bdf(bdf_filename)
+        model2.read_bdf(BDF_FILENAME)
 
         model3 = BDF(debug=None)
-        model3.read_bdf(bdf_filename)
+        model3.read_bdf(BDF_FILENAME)
 
-if __name__ == "__main__":
+    def test_make_log(self):
+        """tests make_log"""
+        make_log(display=False)
+
+if __name__ == "__main__":  # pragma: no cover
     unittest.main()
-
