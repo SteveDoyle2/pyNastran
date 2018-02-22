4827525f1b59a685585381eb1356c3eabb833d3e
MRedAero <stress.redmond@gmail.com>
2018-02-22 11:05:59 -0500

    h5Nastran: fixing issue 481; new implementation for ImmutableDict that            doesn't require MappingProxyType

--- h5Nastran/h5Nastran/utilities/immutable_dict.py
+++ h5Nastran/h5Nastran/utilities/immutable_dict.py
@@ -3,46 +3,42 @@ This is used mainly so the data contained in the dict isn't inadvertently mutate
 
 """
 
-from collections import Mapping
 
 try:
-    from types import MappingProxyType
+    import collections.abc as collections_abc
 except ImportError:
-    # workaround for https://github.com/SteveDoyle2/pyNastran/issues/481
-    # might just get rid of the ImmutableDict in the future, if you change the data in the dict, user beware
-    def MappingProxyType(obj):
-        return obj
+    import collections as collections_abc
 
 
-class ImmutableDict(Mapping):
+class ImmutableDict(collections_abc.Mapping):
     """
-    Copies a dict and proxies it via types.MappingProxyType to make it immutable.
-    https://stackoverflow.com/questions/9997176/immutable-dictionary-only-use-as-a-key-for-another-dictionary/39673094#39673094
+    An immutable, hashable key-value mapping accessible via bracket-notation
+    (i.e. ``__getitem__``).
+
+    :param args: Position arguments in the same form as the :py:class:`dict` constructor.
+    :param kwargs: Keyword arguments in the same form as the :py:class:`dict` constructor.
+
+    derived from https://github.com/pcattori/maps/blob/master/maps/frozenmap.py
+
     """
-    def __init__(self, somedict):
-        dictcopy = dict(somedict)  # make a copy
-        self._dict = MappingProxyType(dictcopy)  # lock it
+
+    def __init__(self, *args, **kwargs):
+        self._data = dict(*args, **kwargs)
         self._hash = None
 
     def __getitem__(self, key):
-        return self._dict[key]
-
-    def __len__(self):
-        return len(self._dict)
+        return self._data[key]
 
     def __iter__(self):
-        return iter(self._dict)
+        return iter(self._data)
+
+    def __len__(self):
+        return len(self._data)
 
     def __hash__(self):
         if self._hash is None:
-            self._hash = hash(frozenset(self._dict.items()))
+            self._hash = hash(frozenset(self.items()))
         return self._hash
 
-    def __eq__(self, other):
-        try:
-            return self._dict == other._dict
-        except AttributeError:
-            return False
-
-    def __repr__(self):
-        return str(self._dict)
+    def __repr__(self): # pragma: no cover
+        return '{}({!r})'.format(type(self).__name__, self._data)
