892dfa1c09cbda5cfb5fbfadab0b4c96123dc8ee
SteveDoyle2 <mesheb82@gmail.com>
2018-06-12 08:17:47 -0700

    fixing tests moving the contour filter

--- pyNastran/converters/aflr/ugrid/test_ugrid_gui.py
+++ pyNastran/converters/aflr/ugrid/test_ugrid_gui.py
@@ -53,19 +53,6 @@ class TestUgridGui(unittest.TestCase):
         #test.load_ugrid_geometry(ugrid_filename, name='main', plot=True)
         #test.load_ugrid3d_geometry(ugrid_filename, name='main', plot=True)
 
-    def test_ugrid_gui_02(self):
-        """tests plate_with_circular_hole"""
-        bdf_filename = os.path.join(NASTRAN_PATH, 'plate_with_circular_hole', 'a101x.dat')
-        ugrid_filename = os.path.join(NASTRAN_PATH, 'plate_with_circular_hole', 'a101x.b8.ugrid')
-        log = get_logger(level='warning')
-
-        with self.assertRaises(RuntimeError):
-            nastran_to_ugrid(bdf_filename, ugrid_filename_out=ugrid_filename,
-                             properties=None, check_shells=True, check_solids=False, log=log)
-        #assert os.path.exists(ugrid_filename), ugrid_filename
-        #test = UGRID_GUI()
-        #test.load_ugrid_geometry(ugrid_filename, name='main', plot=True)
-
     def test_ugrid2d_gui(self):
         """simple UGRID2D model"""
         ugrid_filename = 'quad_tri.ugrid'
