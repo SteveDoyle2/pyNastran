d9cdbdc6c897739eddce4e57588c59cf1e5401f6
SteveDoyle2 <mesheb82@gmail.com>
2019-02-11 10:37:20 -0800

    adding tests

--- pyNastran/op2/test/all_tests.py
+++ pyNastran/op2/test/all_tests.py
@@ -4,6 +4,7 @@ from pyNastran.op2.test.examples.test_op2_in_material_coord import TestMaterialC
 from pyNastran.op2.test.examples.test_op2_in_material_coord_panel_SOL_108 import TestMaterialCoordComplex
 from pyNastran.op2.tables.geom.test.test_geom import TestOP2GeomUnit
 from pyNastran.op2.tables.test.test_grid_point_forces import TestGridPointForces
+from pyNastran.op2.dev.test_op2_writer import TestOP2Writer
 
 
 if __name__ == "__main__":  # pragma: no cover
