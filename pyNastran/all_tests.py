80d8eadd37d211a700d21112bce006cbc8d6fa72
SteveDoyle2 <mesheb82@gmail.com>
2019-04-08 00:46:53 -0700

    disabling gui test

--- pyNastran/all_tests.py
+++ pyNastran/all_tests.py
@@ -16,8 +16,8 @@ else:  # pragma: no cover
 from pyNastran.gui import IS_DEV
 import pyNastran.gui.formats
 import pyNastran.gui.gui_common
-if not IS_DEV:
-    from pyNastran.gui.menus.test_groups import *
+#if not IS_DEV:
+    #from pyNastran.gui.menus.test_groups import *
 from pyNastran.all_tests_no_gui import *
 from pyNastran.converters.test_gui_formats import *
 
