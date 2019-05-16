bdf:  - fixing issue in [Q] rotation (bad [T] formula)
gui:
 - adding custom timer capability

pyNastran/bdf/cards/properties/shell.py
@@ -16,7 +16,6 @@ from __future__ import (nested_scopes, generators, division, absolute_import,
 import  warnings
 from itertools import count
 from typing import List, Optional, Union, Any
-#from numpy import array
 import numpy as np
 
 from pyNastran.utils.numpy_utils import integer_types
@@ -402,7 +401,7 @@ class CompositeShellProperty(ShellProperty):
         for thick in self.get_thicknesses():
             zi += thick
             z.append(zi)
-        return array(z)
+        return np.array(z)
 
     def get_mass_per_area(self, iply='all', method='nplies'):
         r"""
@@ -1024,11 +1023,32 @@ class PCOMP(CompositeShellProperty):
         #print(ABD)
         return ABD
 
-    def get_Q_matrix(self, mid_ref, thetai):
+    def get_Q_matrix(self, mid_ref, theta):
         """theta must be in radians"""
         S2, unused_S3 = get_mat_props_S(mid_ref)
-        ct = np.cos(thetai)
-        st = np.sin(thetai)
+        T = self._get_2d_transform(theta)
+
+        #R = np.array([
+            #[1., 0., 0.],
+            #[0., 1., 0.],
+            #[0., 0., 2.],
+        #])
+        Tinv = np.linalg.inv(T)
+        Q = np.linalg.inv(S2)
+
+        # [Qbar] = [T^-1][Q][T^-T]
+        # [T^-T] = [R][T][R^-1] = [T^-1].T
+        Qbar = np.linalg.multi_dot([Tinv, Q, Tinv.T])
+
+        # [T.T] = [R][T^-1][R^-1]
+        #Sbar = np.linalg.multi_dot([T.T, S2, T])
+        #Qbar = np.linalg.inv(Sbar)
+        return Qbar
+
+    def _get_2d_transform(self, theta):
+        """theta must be in radians"""
+        ct = np.cos(theta)
+        st = np.sin(theta)
         ct2 = ct ** 2
         st2 = st ** 2
         cst = st * ct
@@ -1057,23 +1077,7 @@ class PCOMP(CompositeShellProperty):
             #[     st2,     ct2,   2 * cst],
             #[-ct * st, ct * st, ct2 - st2],
         #])
-
-        #R = np.array([
-            #[1., 0., 0.],
-            #[0., 1., 0.],
-            #[0., 0., 2.],
-        #])
-        Tinv = np.linalg.inv(T)
-        Q = np.linalg.inv(S2)
-
-        # [Qbar] = [T^-1][Q][T^-T]
-        # [T^-T] = [R][T][R^-1] = [T^-1].T
-        Qbar = np.linalg.multi_dot([Tinv, Q, Tinv.T])
-
-        # [T.T] = [R][T^-1][R^-1]
-        #Sbar = np.linalg.multi_dot([T.T, S2, T])
-        #Qbar = np.linalg.inv(Sbar)
-        return Qbar
+        return T
 
     def _verify(self, xref):
         pid = self.Pid()
@@ -2168,7 +2172,7 @@ class PSHELL(ShellProperty):
 
     def get_z_locations(self):
         """returns the locations of the bottom and top surface of the shell"""
-        z = array([self.z1, self.z2])
+        z = np.array([self.z1, self.z2])
         return z
 
     def materials(self):

pyNastran/bdf/cards/test/test_shells.py
@@ -9,7 +9,7 @@ from numpy import array
 from pyNastran.bdf.bdf import PCOMP, MAT1, BDF
 from pyNastran.bdf.cards.materials import get_mat_props_S
 from pyNastran.bdf.cards.test.utils import save_load_deck
-
+from pyNastran.bdf.cards.elements.plot import plot_material_properties_vs_theta
 
 class TestShells(unittest.TestCase):
     def test_pshell(self):
@@ -1243,6 +1243,7 @@ class TestShells(unittest.TestCase):
         assert model2.elements[11].rstrip() == 'CQUAD4        11     100       1       2       3       4       0'
 
     def test_abd(self):
+        """tests some ABD matrix functionality for a PCOMP"""
         model = BDF(debug=False, log=None, mode='msc')
         model.add_grid(1, [0., 0., 0.])
         model.add_grid(2, [1., 0., 0.])
@@ -1294,81 +1295,16 @@ class TestShells(unittest.TestCase):
                                  souts=None, nsm=0., sb=0., ft=None, tref=0., ge=0.,
                                  lam=None, z0=0., comment='')
 
-
         model.pop_parse_errors()
         model.cross_reference()
         model.pop_xref_errors()
         ABD = pcomp.get_ABD_matrices()
 
-        thetad = np.linspace(0., 180., num=91)
-        plot_material_properties_vs_theta(pcomp8, mat8, thetad)
-
-def plot_material_properties_vs_theta(pcomp, mid_ref, thetad, plot=False, show=False):
-    e22 = mid_ref.e22
-    g12 = mid_ref.g12
-    theta = np.radians(thetad)
-
-    Ex = []
-    Ey = []
-    Gxy = []
-    Q66 = []
-    nu_xy = []
-    for thetai in  theta:
-        Qbar = pcomp.get_Q_matrix(mid_ref, thetai)
-        Sbar = np.linalg.inv(Qbar)
-        Exi = 1 / Sbar[0, 0]
-        Eyi = 1 / Sbar[1, 1]
-        Gxyi = 1 / Sbar[2, 2]
-        Q66i = Qbar[2, 2]
-        nu_xyi = -Sbar[0, 1] * Exi
-
-        #Gxyi = 1 / Q66i
-        Ex.append(Exi)
-        Ey.append(Eyi)
-        Gxy.append(Gxyi)
-        Q66.append(Q66i)
-        nu_xy.append(nu_xyi)
-    Ex = np.array(Ex)
-    Ey = np.array(Ey)
-    Gxy = np.array(Gxy)
-    Q66 = np.array(Q66)
-    nu_xy = np.array(nu_xy)
-
-    min_max_theta = [thetad.min(), thetad.max()]
-
-    if plot:
-        from pyNastran.gui.matplotlib_backend import matplotlib_backend
-        import matplotlib
-        matplotlib.use(matplotlib_backend)
-        import matplotlib.pyplot as plt
-        fig = plt.figure(1)
-        ax = fig.gca()
-
-        #ax.plot(thetad, Q66/Q66.max(), label='Q66=%g' % Q66.max())
-        #ax.plot(thetad, Ex/Ex.max(), label='Ex=%g' % Ex.max())
-        #ax.plot(thetad, Ey/Ey.max(), label='Ey=%g' % Ey.max())
-        #ax.plot(thetad, Gxy/Gxy.max(), label='Gxy=%g' % Gxy.max())
-        ax.plot(thetad, Ex/e22, label='Ex/E2=%g' % Ex.max())
-        ax.plot(thetad, Ey, label='Ey=%g' % Ey.max())
-        ax.plot(thetad, Gxy/g12, label='Gxy/G12=%g' % Gxy.max())
-        #ax.set_xlim(min_max_theta)
-        ax.legend()
-        ax.grid()
-        ax.set_xlabel('Q66')
-        ax.set_xlabel('theta')
-        #----------------------------
-        fig = plt.figure(2)
-        ax = fig.gca()
-        ax.plot(thetad, nu_xy, label='\nu xy=%g' % nu_xy.max())
-        ax.set_xlim(min_max_theta)
-        ax.legend()
-        ax.grid()
-        ax.set_xlabel('\nu xy')
-        ax.set_xlabel('theta')
-        if show:
-            plt.show()
+        thetad = np.linspace(0., 90., num=91)
+        plot_material_properties_vs_theta(pcomp8, mat8, thetad, show=False)
 
 def make_dvcrel_optimization(model, params, element_type, eid, i=1):
+    """makes a series of DVCREL1 and a DESVAR"""
     j = i
     for ii, (name, desvar_value) in enumerate(params):
         j = i + ii
@@ -1382,6 +1318,7 @@ def make_dvcrel_optimization(model, params, element_type, eid, i=1):
     return j + 1
 
 def make_dvprel_optimization(model, params, prop_type, pid, i=1):
+    """makes a series of DVPREL1 and a DESVAR"""
     j = i
     for ii, (name, desvar_value) in enumerate(params):
         j = i + ii
@@ -1395,6 +1332,7 @@ def make_dvprel_optimization(model, params, prop_type, pid, i=1):
     return j + 1
 
 def make_dvmrel_optimization(model, params, material_type, mid, i=1):
+    """makes a series of DVMREL1 and a DESVAR"""
     j = i
     for ii, (name, desvar_value) in enumerate(params):
         j = i + ii

pyNastran/bdf/mesh_utils/test/test_cutting_plane.py
@@ -82,8 +82,11 @@ class TestCuttingPlane(unittest.TestCase):
     def test_cut_bwb(self):
         """recover element ids"""
         log = SimpleLogger(level='warning', encoding='utf-8', log_func=None)
-        #bdf_filename = os.path.join(MODEL_PATH, 'bwb', 'bwb_saero.bdf')  # ymax~=1262.0
-        bdf_filename = r'C:\NASA\asm\all_modes_mach_0.85\flutter.bdf'  # ymax=1160.601
+        is_bwb = True
+        if is_bwb:
+            bdf_filename = os.path.join(MODEL_PATH, 'bwb', 'bwb_saero.bdf')  # ymax~=1262.0
+        else:  #  pragma: no cover
+            bdf_filename = r'C:\NASA\asm\all_modes_mach_0.85\flutter.bdf'  # ymax=1160.601
         normal_plane = np.array([0., 1., 0.])
 
         model = read_bdf(bdf_filename, log=log)
@@ -106,20 +109,20 @@ class TestCuttingPlane(unittest.TestCase):
         I = []
         EI = []
         avg_centroid = []
-        is_bwb = True
-        
+
         for i in range(2000):
             if is_bwb:
                 dy = 100. * i + 1.  #  bwb
-                coord = CORD2R(1, rid=0, origin=[0., dy, 0.], zaxis=[0., dy, 1], xzplane=[1., dy, 0.],
-            else:
+                coord = CORD2R(1, rid=0, origin=[0., dy, 0.], zaxis=[0., dy, 1], xzplane=[1., dy, 0.])
+            else:  #  pragma: no cover
                 dy = 4. * i + 1.  #  CRM
-                origin = np.array([0., dy, 0.])
+                coord = CORD2R(1, rid=0, origin=[0., dy, 0.], zaxis=[0., dy, 1], xzplane=[1., dy, 0.])
+                #origin = np.array([0., dy, 0.])
                 #xzplane = origin + dx
-                xzplane = np.array([1., dy, 0.])
+                #xzplane = np.array([1., dy, 0.])
                 #coord = CORD2R.add_axes(cid, rid=0, origin=p1, xaxis=p2-p1, yaxis=None, zaxis=None,
                                          #xyplane=None, yzplane=None, xzplane=None, comment='')
-            print(coord)
+                print(coord)
             model.coords[1] = coord
             plane_bdf_filename = 'plane_face_%i.bdf' % i
             cut_face_filename = 'cut_face_%i.csv' % i
@@ -208,25 +211,15 @@ class TestCuttingPlane(unittest.TestCase):
                                  n1a=0., n2a=None, n1b=0., n2b=None, comment='')
         beam_model.write_bdf('equivalent_beam_model.bdf')
 
-        #def vstack(data):
-            #for i, datai in enumerate(data):
-                #print(i, datai.shape)
-            #X = np.vstack(data)
-            #return X
-        #def hstack(data):
-            #for i, datai in enumerate(data):
-                #print(i, datai.shape)
-            #X = np.hstack(data)
-            #return X
-        #print('len(y)=', len(y))
         X = np.vstack([y, A]).T
         Y = np.hstack([X, I, EI, avg_centroid])
-        np.savetxt('cut_data_vs_span.csv', Y, header='y, A, Ix, Iz, Ixz, Ex*Ix, Ex*Iz, Ex*Ixz, xcentroid, ycentroid, zcentroid', delimiter=',')
+        header = 'y, A, Ix, Iz, Ixz, Ex*Ix, Ex*Iz, Ex*Ixz, xcentroid, ycentroid, zcentroid'
+        np.savetxt('cut_data_vs_span.csv', Y, header=header, delimiter=',')
 
 
         show = True
         #show = False
-        if IS_MATPLOTLIB or 1:
+        if IS_MATPLOTLIB:
             plot_inertia(y, A, I, EI, avg_centroid, show=show)
 
         #bdf_merge(plane_bdf_filenames, bdf_filename_out='merge.bdf', renumber=True,