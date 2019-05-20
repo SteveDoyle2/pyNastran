11e48657f594345a09e0d49f8af11a53f7ff5f2b
SteveDoyle2 <mesheb82@gmail.com>
2019-05-20 16:26:40 -0700

    docs

--- docs/html_docs/manual/notebook_to_markdown.py
+++ docs/html_docs/manual/notebook_to_markdown.py
@@ -1,6 +1,10 @@
+"""
+First you must:
+ - pip install nbconvert
+ - install pandoc: https://pandoc.org/installing.html
+"""
 from __future__ import print_function
 import os
-import shutil
 
 import pyNastran
 from pyNastran.utils import print_bad_path
@@ -12,7 +16,7 @@ def create_rst_from_ipython_notebooks():
     notebook_dir = os.path.join(pkg_path, '..', 'docs', 'quick_start', 'demo')
     pydocs_dir = os.path.join(pkg_path, '..', 'docs', 'html_docs', 'quick_start')
 
-    assert os.path.exists(pydocs_dir), print_bad_path(quick_start_pydocs_dir)
+    assert os.path.exists(pydocs_dir), print_bad_path(pydocs_dir)
     assert os.path.exists(notebook_dir), print_bad_path(notebook_dir)
     os.chdir(notebook_dir)
     for fname in os.listdir(notebook_dir):
@@ -24,7 +28,7 @@ def create_rst_from_ipython_notebooks():
             continue
         if not fnamei.endswith('.ipynb'):
             continue
-        os.system('ipython nbconvert --to rst %s' % fname)
+        os.system('jupyter nbconvert --to rst %s' % fname)
         if not os.path.exists(fname2):
             print('%s was not made...' % fname2)
             continue
@@ -36,4 +40,3 @@ def create_rst_from_ipython_notebooks():
             os.rename(fname2, moved_fname2)
         except:
             pass
-
