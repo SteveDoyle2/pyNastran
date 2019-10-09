54e45861b7faa6ae4658a565b46c5f0eda1df1c9
Steve Doyle <mesheb82@gmail.com>
2019-10-08 23:51:52 -0700

    adding su2 tests  - solid test  - rewrite the reader to be more robust  - added preliminary reader support for multiple zones/markers (reads and throws away extra stuff)

--- pyNastran/converters/su2/su2_reader.py
+++ pyNastran/converters/su2/su2_reader.py
@@ -2,12 +2,14 @@ import numpy as np
 
 
 def read_su2(su2_filename, log=None, debug=False):
+    """reads a 2d/3d SU2 single zone model"""
     model = SU2Reader(log=log, debug=debug)
     unused_ndim, nodes, elements, regions = model.read_su2(su2_filename)
     #model.to_cart3d()
     return model, nodes, elements, regions
 
 class SU2Reader:
+    """SU2 reader/writer"""
     etype_nnodes_map = {
         #Line       3
         #Triangle   5
@@ -25,13 +27,14 @@ class SU2Reader:
         14 : 5,
     }
     def __init__(self, log=None, debug=False):
+        """initializes the SU2Reader object"""
         self.log = log
         self.debug = debug
         self.su2_filename = None
 
-    def read_2d(self, su2_file, ndim):
+    def read_2d(self, lines, i, ndim, nelem):
+        """reads a 2d SU2 zone"""
         # elements
-        nelem = int(su2_file.readline().split('=')[1])
         tris = []
         quads = []
         for unused_ne in range(nelem):
@@ -43,9 +46,9 @@ class SU2Reader:
             #Hexahedral     12
             #Wedge          13
             #Pyramid        14
-            data = su2_file.readline().split()[:-1]
-            etype = data[0]
-            nodes = data[1:]
+            line = lines[i]
+            data = line.split()[:-1]
+            etype, *nodes = data
             if etype == '9':
                 assert len(nodes) == 4, nodes
                 quads.append(nodes)
@@ -54,62 +57,68 @@ class SU2Reader:
                 tris.append(nodes)
             else:
                 raise NotImplementedError(etype)
+            i += 1
 
         tris = np.asarray(tris, dtype='int32')
         quads = np.asarray(quads, dtype='int32')
-        #print('tris =', tris)
-        #print('quads =', quads)
 
-        nnodes = int(su2_file.readline().split('=')[1])
-        nodes = np.zeros((nelem, 2), dtype='int32')
+        i, points_dict = read_header(lines, i, self.log)
+        nnodes = points_dict['npoints']
+        nodes = np.zeros((nelem, 2), dtype='float32')
         for inode in range(nnodes):
-            sline = su2_file.readline().split()
+            line = lines[i]
+            sline = line.split()
             assert len(sline) == 3, sline
             x, y, unused_z = sline
-            #print(x, y, z)
-            nodes[inode, :] = [float(x), float(y)]
+            nodes[inode, :] = [x, y]
+            i += 1
 
         # boundary conditions
-        nmark = int(su2_file.readline().split('=')[1])
-        for unused_imark in range(nmark):
-            unused_marker = su2_file.readline().split('=')[1].strip()
-            nelements_mark = int(su2_file.readline().split('=')[1])
+        i, bcs = read_header(lines, i, self.log)
+        nmark = bcs['nmark']
+        for imark in range(nmark):
+            nelements_mark = bcs['marker_elems']
 
-            if ndim == 2:
-                lines = []
-                #np.zeros((nelements_mark, 2), dtype='int32')
-                for unused_ne in range(nelements_mark):
-                    # what are the 3 slots?
-                    #Line          (2D)     3
-                    #Triangle      (3D)     5
-                    #Quadrilateral (3D)     9
-                    sline = su2_file.readline().split()
-                    etype = int(sline[0])
-                    if etype == 3:
-                        etype, n1, n2 = sline
-                        lines.append([n1, n2])
-                    else:
-                        raise NotImplementedError(etype)
+            lines2d = []
+            #np.zeros((nelements_mark, 2), dtype='int32')
+            for unused_ne in range(nelements_mark):
+                # what are the 3 slots?
+                #Line          (2D)     3
+                #Triangle      (3D)     5
+                #Quadrilateral (3D)     9
+                line = lines[i]
+                sline = line.split()
+
+                etype, *nodes = sline
+                if etype == '3':
+                    assert len(nodes) == 2, nodes
+                    lines2d.append(nodes)
+                else:
+                    raise NotImplementedError(etype)
+                i += 1
+
+            lines2d = np.array(lines2d, dtype='int32')
+            if imark != nmark - 1:
+                i, bcs = read_header(lines, i, self.log)
 
         assert len(tris) > 0 or len(quads) > 0
         elements = {
             5 : tris,
             9 : quads,
         }
-        regions = {3 : lines}
-        return nodes, elements, regions
+        regions = {3 : lines2d}
+        return i, nodes, elements, regions
 
-    def read_3d(self, su2_file, unused_ndim):
-        nelem = int(su2_file.readline().split('=')[1])
+    def read_3d(self, lines, i, unused_ndim, nelem):
+        """reads a 3d SU2 zone"""
         tets = []
         hexs = []
         wedges = []
         #pents = []
         pyramids = []
         for unused_ne in range(nelem):
-            data = su2_file.readline().split()[1:-1]
-            etype = data[0]
-            nodes = data[1:]
+            line = lines[i]
+            etype, *nodes = line.split()[:-1]
             if etype == '10':
                 tets.append(nodes)
             elif etype == '12':
@@ -119,7 +128,8 @@ class SU2Reader:
             elif etype == '14':
                 pyramids.append(nodes)
             else:
-                raise NotImplementedError(etype)
+                raise NotImplementedError(f'etype={etype} data={data}')
+            i += 1
         tets = np.array(tets, dtype='int32')
         #pents = np.array(pents, dtype='int32')
         wedges = np.array(wedges, dtype='int32')
@@ -131,26 +141,33 @@ class SU2Reader:
             14 : pyramids,
         }
 
-        nnodes = int(su2_file.readline().split('=')[1])
-        nodes = np.zeros((nelem, 2), dtype='int32')
+        i, points_dict = read_header(lines, i, self.log)
+        nnodes = points_dict['npoints']
+        nodes = np.zeros((nelem, 3), dtype='float32')
         for inode in range(nnodes):
-            x, y, z = su2_file.readline().split()[:-1]
+            line = lines[i]
+            x, y, z = line.split()[:-1]
             nodes[inode, :] = [x, y, z]
+            i += 1
 
-        nmark = int(su2_file.readline().split('=')[1])
-        for unused_imark in range(nmark):
-            unused_marker = su2_file.readline().split('=')[1].strip()
-            nelements_mark = int(su2_file.readline().split('=')[1])
+        i, bcs = read_header(lines, i, self.log)
+        nmark = bcs['nmark']
+        for imark in range(nmark):
+            nelements_mark = bcs['marker_elems']
             tris = []
             quads = []
             for unused_ne in range(nelements_mark):
-                data = su2_file.readline().split()[1:-1]
-                etype = data[0]
-                nodes = data[1:]
+                etype, *nodes = line.split()[:-1]
                 if etype == '9':
+                    assert nodes == 4, nodes
                     quads.append(nodes)
                 elif etype == '5':
+                    assert nodes == 3, nodes
                     tris.append(nodes)
+                i += 1
+            if imark != nmark - 1:
+                i, bcs = read_header(lines, i, self.log)
+
             tris = np.array(tris, dtype='int32')
             quads = np.array(quads, dtype='int32')
 
@@ -158,19 +175,30 @@ class SU2Reader:
             5 : tris,
             9 : quads,
         }
-        return nodes, elements, regions
+        return i, nodes, elements, regions
 
     def read_su2(self, su2_filename):
+        """reads a 2d/3d SU2 single zone model"""
         self.su2_filename = su2_filename
+
         with open(su2_filename, 'r') as su2_file:
-            ndim = int(su2_file.readline().split('=')[1])
+            lines = su2_file.readlines()
+
+        i = 0
+        i, zone = read_header(lines, i, self.log)
+        nzones = zone['nzones']
+        for izone in range(nzones):
+            ndim = zone['ndim']
+            ndim = zone['ndim']
+            nelem = zone['nelem']
 
             if ndim == 2:
-                nodes, elements, regions = self.read_2d(su2_file, ndim)
+                i, nodes, elements, regions = self.read_2d(lines, i, ndim, nelem)
             elif ndim == 3:
-                nodes, elements, regions = self.read_3d(su2_file, ndim)
+                i, nodes, elements, regions = self.read_3d(lines, i, ndim, nelem)
             else:
                 raise RuntimeError(ndim)
+            break
         return ndim, nodes, elements, regions
 
     def write_su2(self, su2_filename, nodes, elements, unused_regions):
@@ -192,3 +220,42 @@ class SU2Reader:
                 su2_file.write('NPOINTS = %i\n' % nnodes)
                 for inode, node in enumerate(nodes):
                     su2_file.write('%i %i %i %i\n' % (node[0], node[1], node[2], inode))
+
+def read_header(lines, i, log):
+    """reads the header and stores it as a dictionary"""
+    i0 = i
+    headers = {}
+    line = lines[i]
+
+    while '=' in line:
+        word, value = line.split('=')
+        if word == 'NDIME':
+            headers['ndim'] = int(value)
+        elif word == 'NELEM':
+            headers['nelem'] = int(value)
+        elif word == 'NZONE':
+            headers['nzones'] = int(value)
+        elif word == 'IZONE':
+            headers['izone'] = int(value)
+
+        elif word == 'NMARK':
+            headers['nmark'] = int(value)
+        elif word == 'MARKER_TAG':
+            headers['marker_tag'] = value.strip()
+        elif word == 'MARKER_ELEMS':
+            headers['marker_elems'] = int(value)
+
+        elif word == 'NPOIN':
+            headers['npoints'] = int(value)
+        else:
+            raise NotImplementedError('word=%r' % word)
+        i += 1
+        line = lines[i]
+
+    assert len(headers) > 0, headers
+    if i0 == 0 and 'nzones' not in headers and ('izone' in headers or'ndim' in headers):
+        headers['nzones'] = 1
+
+    line = lines[i]
+    log.debug(str(headers))
+    return i, headers
