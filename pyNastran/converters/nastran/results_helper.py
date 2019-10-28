nastran gui:  - adding more gui headers (labels in left corner)  - fixing strain energy density gui bug (wrong case number)  - fixing eign calculation (calling eigr instead of eign)
op2:
 - saving eigr, eigi, freq for the applicable cases

pyNastran/converters/nastran/results_helper.py
@@ -623,7 +623,7 @@ class NastranGuiResults(NastranGuiAttributes):
                 sed_res = GuiResult(subcase_id, header='Strain Energy Density: ' + header,
                                     title='Strain Energy Density', data_format='%.3e',
                                     location='centroid', scalar=strain_energy_density)
-                cases[icase + 1] = (sed_res, (subcase_id, 'Strain Energy Density'))
+                cases[icase] = (sed_res, (subcase_id, 'Strain Energy Density'))
                 form_dict[(key, itime)].append(('Strain Energy Density', icase, []))
                 icase += 1
 
@@ -1065,6 +1065,7 @@ class NastranGuiResults(NastranGuiAttributes):
         form0 = (word, None, [])
         unused_formis = form0[2]
         subcase_id = key[2]
+        header = header_dict[(key, itime)]
         if is_stress and itime == 0:
             if is_element_on.min() == 0:  # if all elements aren't on
                 print_empty_elements(self.model, self.element_ids, is_element_on, self.log_error)
@@ -1081,7 +1082,6 @@ class NastranGuiResults(NastranGuiAttributes):
 
         #print('max/min', max_principal.max(), max_principal.min())
         # header = _get_nastran_header(case, dt, itime)
-        header = header_dict[(key, itime)]
         if np.any(np.isfinite(oxx)):
             oxx_res = GuiResult(subcase_id, header=word + f'XX: {header}', title=word + 'XX',
                                 location='centroid', scalar=oxx, data_format=fmt)

pyNastran/op2/result_objects/stress_object.py
@@ -322,10 +322,14 @@ def _get_nastran_header(case: Any, dt: Union[int, float], itime: int) -> str:
     except KeyError:
         return 'Static'
 
-    if isinstance(dt, float):
+    if isinstance(dt, (float, np.float32)):
         header = ' %s = %.4E' % (code_name, dt)
-    else:
+    elif isinstance(dt, (int, np.int32)):
         header = ' %s = %i' % (code_name, dt)
+    elif dt is None:
+        return 'Static'
+    else:  # pragma: no cover
+        print(case, dt, type(dt))
 
     # cases:
     #   1. lsftsfqs
@@ -341,15 +345,36 @@ def _get_nastran_header(case: Any, dt: Union[int, float], itime: int) -> str:
         cycle = np.abs(eigi) / (2. * np.pi)
         header += '; freq = %g Hz' % cycle
     elif hasattr(case, 'eigns'):  #  eign is not eigr; it's more like eigi
-        eigi = case.eigrs[itime] #  but |eigi| = sqrt(|eign|)
-        cycle = np.sqrt(np.abs(eigi)) / (2. * np.pi)
-        header += '; freq = %g Hz' % cycle
-    elif hasattr(case, 'dt'):
+        eigi = case.eigns[itime] #  but |eigi| = sqrt(|eign|)
+        freq = np.sqrt(np.abs(eigi))
+        cycle = freq / (2. * np.pi)
+        header += '; freq = %g Hz' % freq
+    elif hasattr(case, 'freqs'):
+        header += '; freq = %g Hz' % case.freqs[itime]
+
+    elif hasattr(case, 'times'):
         time = case._times[itime]
         header += '; time = %g sec' % time
-    elif hasattr(case, 'lftsfqs') or hasattr(case, 'lsdvmns') or hasattr(case, 'loadIDs'):
-        pass
-        #raise RuntimeError(header)
+    elif hasattr(case, 'dts'):
+        time = case._times[itime]
+        header += '; time = %g sec' % time
+        #print(header)
+
+    elif hasattr(case, 'lftsfqs'):
+        step = case.lftsfqs[itime]
+        header += '; step = %g' % step
+    elif hasattr(case, 'lsdvmns'):
+        step = case.lsdvmns[itime]
+        header += '; step = %g' % step
+    elif hasattr(case, 'loadIDs'):
+        step = case.loadIDs[itime]
+        header += '; step = %g' % step
+    elif hasattr(case, 'loadFactors'):
+        step = case.loadFactors[itime]
+        header += '; step = %g' % step
+    elif hasattr(case, 'load_steps'):
+        step = case.load_steps[itime]
+        header += '; step = %g' % step
     else:
         msg = 'unhandled case; header=%r\n%s' % (header, str(case))
         print(msg)