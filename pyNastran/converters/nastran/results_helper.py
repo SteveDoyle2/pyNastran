gui:  - fixing bug in get_element_ids_list_with_pids  - fixing eigenvector scaling
op2:
 - FractionalMassResponse support

pyNastran/converters/nastran/results_helper.py
@@ -154,24 +154,29 @@ class NastranGuiResults(NastranGuiAttributes):
                                               scales,
                                               uname='NastranResult')
 
-            dmax = []
+            #dmax = []
             for itime in range(ntimes):
                 dt = case._times[itime]
 
-                if name == 'Displacement':
+                #if name == 'Displacement':
                     # (6673, )
-                    normiii = np.linalg.norm(t123[itime, :, :], axis=1)
+                    #normiii = np.linalg.norm(t123[itime, :, :], axis=1)
                     #print(normiii.shape)
                     #print('Displacement; itime=%s time=%s tnorm=%s' % (
                         #itime, dt, normiii.max()))
-                    dmax.append(normiii.max())
+                    #dmax.append(normiii.max())
+
+                if name == 'Eigenvectors':
+                    # independent
+                    tnorm_abs_max = np.linalg.norm(t123[itime, :, :], axis=1).max()
+                else:
+                    tnorm_abs_max = tnorm.max()
                 # mode = 2; freq = 75.9575 Hz
                 header = _get_nastran_header(case, dt, itime)
                 header_dict[(key, itime)] = header
                 keys_map[key] = (case.subtitle, case.label,
                                  case.superelement_adaptivity_index, case.pval_step)
 
-                tnorm_abs_max = tnorm.max()
                 #if tnorm_abs_max == 0.0:
                     #scale = self.displacement_scale_factor
                 #else:
@@ -179,7 +184,7 @@ class NastranGuiResults(NastranGuiAttributes):
 
                 scale = self.gui.settings.dim_max
                 if tnorm_abs_max > 0.0:
-                    scale = self.gui.settings.dim_max / tnorm_abs_max * 0.25
+                    scale = self.gui.settings.dim_max / tnorm_abs_max * 0.10
                 scales.append(scale)
                 titles.append(title1)
                 headers.append(f'{title1}: {header}')
@@ -188,10 +193,10 @@ class NastranGuiResults(NastranGuiAttributes):
                 form_dict[(key, itime)].append(formii)
                 icase += 1
 
-            if name == 'Displacement':
+            #if name == 'Displacement':
                 # Displacement; itime=361 time=3.61 tnorm=1.46723
                 #print('dmax = ', max(dmax))
-                pass
+                #pass
             nastran_res.save_defaults()
         else:
             nastran_res = ForceTableResults(subcase_idi, titles, headers,
@@ -1194,6 +1199,14 @@ def _get_t123_tnorm(case, nids, nnodes: int, t123_offset: int=0):
         0 : translations / forces
         3 : rotations / moments
 
+    Returns
+    -------
+    t123 : (ntimes, nnodes, 3) float ndarray
+       the translations or rotations
+    tnorm : (ntimes, 3) float ndarray
+        ???
+    ntimes : int
+       number of times
     """
     assert case.is_sort1, case.is_sort1
 

pyNastran/op2/result_objects/design_response.py
@@ -5,16 +5,19 @@ class Responses:
     def __init__(self):
         self.convergence_data = None
         self.weight_response = None
+        self.displacement_response = None
         self.stress_response = None
         self.strain_response = None
         self.force_response = None
         self.composite_stress_response = None
         self.composite_strain_response = None
         self.flutter_response = None
+        self.fractional_mass_response = None
 
     def get_stats(self, short=False):
         objects = [
             self.convergence_data,
+            self.displacement_response,
             self.weight_response,
             self.stress_response,
             self.strain_response,
@@ -22,6 +25,7 @@ class Responses:
             self.composite_stress_response,
             self.composite_strain_response,
             self.flutter_response,
+            self.fractional_mass_response,
         ]
         msg = []
         for obj in objects:
@@ -159,7 +163,7 @@ class WeightResponse:
             return self.__repr__()
 
 
-class GeneralResponse:
+class PropertyResponse:
     """common class for StressResponse, StrainResponse, and ForceResponse"""
     def __init__(self):
         self.n = 1
@@ -198,11 +202,98 @@ class GeneralResponse:
         else:
             return self.__repr__()
 
-class ForceResponse(GeneralResponse):
+class FractionalMassResponse:
+    name = 'fractional_mass'
+    def __init__(self):
+        self.n = 1
+        self._n = 0
+        self._itable = 0
+        self.is_built = False
+        self.internal_id = []
+        self.dresp_id = []
+        self.region = []
+        self.response_label = []
+        self.subcase = []
+        self.type_flag = []
+        self.seid = []
+
+    def get_stats(self, short=False):
+        if short:
+            return 'responses.%s_response (%s)' % (self.name, self.n)
+        else:
+            return self.__repr__()
+
+    def append(self, internal_id, dresp_id, response_label, region,
+               subcase, type_flag, seid):
+        self.internal_id.append(internal_id)
+        self.dresp_id.append(dresp_id)
+        self.region.append(region)
+        self.response_label.append(response_label)
+        self.subcase.append(subcase)
+        self.type_flag.append(type_flag)
+        self.seid.append(seid)
+        self._n += 1
+
+    def __repr__(self):
+        msg = 'FractionalMassResponse()\n'
+        msg += '  response_label=%s\n' % np.array(self.response_label)
+        #msg += '  nid=%s\n' % np.array(self.nid)
+        msg += '  internal_id=%s\n' % np.array(self.internal_id)
+        msg += '  subcase=%s\n' % np.array(self.subcase)
+        msg += '  type_flag=%s\n' % np.array(self.type_flag)
+        msg += '  seid=%s\n' % np.array(self.seid)
+        return msg
+
+class DisplacementResponse:
+    name = 'displacement'
+    def __init__(self):
+        self.n = 1
+        self._n = 0
+        self._itable = 0
+        self.is_built = False
+        self.internal_id = []
+        self.dresp_id = []
+        self.region = []
+        self.response_label = []
+        self.subcase = []
+        self.nid = []
+        self.type_flag = []
+        self.seid = []
+
+    def append(self, internal_id, dresp_id, response_label, region,
+               subcase, type_flag, seid,
+               nid):
+        self.internal_id.append(internal_id)
+        self.dresp_id.append(dresp_id)
+        self.region.append(region)
+        self.response_label.append(response_label)
+        self.subcase.append(subcase)
+        self.nid.append(nid)
+        self.type_flag.append(type_flag)
+        self.seid.append(seid)
+        self._n += 1
+
+    def get_stats(self, short=False):
+        if short:
+            return 'responses.%s_response (%s)' % (self.name, self.n)
+        else:
+            return self.__repr__()
+
+    def __repr__(self):
+        msg = 'DisplacementResponse()\n'
+        msg += '  response_label=%s\n' % np.array(self.response_label)
+        msg += '  nid=%s\n' % np.array(self.nid)
+        msg += '  internal_id=%s\n' % np.array(self.internal_id)
+        msg += '  subcase=%s\n' % np.array(self.subcase)
+        msg += '  type_flag=%s\n' % np.array(self.type_flag)
+        msg += '  seid=%s\n' % np.array(self.seid)
+        return msg
+
+class ForceResponse(PropertyResponse):
     name = 'force'
-class StressResponse(GeneralResponse):
+class StressResponse(PropertyResponse):
     name = 'stress'
-class StrainResponse(GeneralResponse):
+class StrainResponse(PropertyResponse):
     name = 'strain'
 
 class FlutterResponse: