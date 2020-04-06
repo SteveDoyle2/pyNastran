f1ff2842360cc69d30029b6881c5e0fd7c1656e0
SteveDoyle2 <mesheb82@gmail.com>
2020-04-05 17:23:52 -0700

    fixing build again again

--- pyNastran/gui/test/all_tests_gui.py
+++ pyNastran/gui/test/all_tests_gui.py
@@ -1,8 +1,9 @@
 """with pyQt5/pySide2 and vtk"""
 import os
-import sys
+# import sys
 from pyNastran.gui.menus.test.test_menus import *
-if 'XVFB' in os.environ or sys.platform == 'win32':
+# if 'XVFB' in os.environ or sys.platform == 'win32':   # XVFB is for TravisCI and doesn't work
+if sys.platform == 'win32':
     from pyNastran.gui.menus.test.test_groups import *
 
 if __name__ == "__main__":  # pragma: no cover
