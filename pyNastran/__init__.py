3d4ab063990f344a9daccfb70a9887e8cdd9da54
SteveDoyle2 <mesheb82@gmail.com>
2020-04-07 23:04:16 -0700

    building gui

--- pyNastran/__init__.py
+++ pyNastran/__init__.py
@@ -1,9 +1,7 @@
-import os
-import sys
 
 
 # this variable is automatically set by the .spec file; should be False
-is_pynastrangui_exe = False
+is_pynastrangui_exe = True
 is_installed = False
 if is_pynastrangui_exe or is_installed:
     # pyInstaller
