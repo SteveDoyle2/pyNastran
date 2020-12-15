b778a0b760ea0de9ff171aadc148a68bff1b73b3
SteveDoyle2 <mesheb82@gmail.com>
2020-12-15 09:38:55 -0800

    these were in the folder but not committed...they were also labeled celas/spring

---
+++ pyNastran/dev/bdf_vectorized/cards/elements/damper/damp_element.py
@@ -0,0 +1,13 @@
+from pyNastran.dev.bdf_vectorized.cards.elements.element import Element
+
+class SpringElement(Element):
+    def __init__(self, model):
+        """
+        Defines the SpringElement object.
+
+        Parameters
+        ----------
+        model : BDF
+           the BDF object
+        """
+        Element.__init__(self, model)
