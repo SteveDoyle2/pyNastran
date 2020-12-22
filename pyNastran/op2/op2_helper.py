fixing underflow bug

pyNastran/op2/op2_helper.py
@@ -34,7 +34,7 @@ def polar_to_real_imag(mag, phase):
     except FloatingPointError:
         phase2 = np.asarray(phase, dtype='float64')
         rtheta = np.radians(phase2)
-        out = mag * (np.cos(rtheta) + 1.j * np.sin(rtheta))
+        out = mag * (np.cos(rtheta) + 1.j * np.sin(rtheta)).asdtype('complex64')
     return out
     #return rect(mag, radians(phase))
 

pyNastran/op2/op2_interface/op2_common.py
@@ -1334,8 +1334,7 @@ class OP2Common(Op2Codes, F06Writer):
             floats = np.frombuffer(data, dtype=self.fdtype8).reshape(nnodes, 14).copy()
             mag = floats[:, 2:8]
             phase = floats[:, 8:]
-            rtheta = np.radians(phase)
-            real_imag = mag * (np.cos(rtheta) + 1.j * np.sin(rtheta))
+            real_imag = polar_to_real_imag(mag, phase)
             #abs(real_imag), angle(real_imag, deg=True)
 
             obj._times[obj.itime] = dt
@@ -1480,8 +1479,7 @@ class OP2Common(Op2Codes, F06Writer):
 
             mag = floats[:, 2:8]
             phase = floats[:, 8:]
-            rtheta = np.radians(phase)
-            real_imag = mag * (np.cos(rtheta) + 1.j * np.sin(rtheta))
+            real_imag = polar_to_real_imag(mag, phase)
             obj.data[itotal:itotal2, obj.itime, :] = real_imag
             obj.itotal = itotal2
         else:

pyNastran/op2/op2_interface/utils.py
@@ -1,6 +1,7 @@
-from itertools import count
 from typing import List, Any
 import numpy as np
+from pyNastran.op2.op2_helper import polar_to_real_imag
+
 
 def reshape_bytes_block(block: bytes) -> bytes:
     nwords = len(block) // 2
@@ -46,8 +47,7 @@ def apply_mag_phase(floats: Any, is_magnitude_phase: bool,
     if is_magnitude_phase:
         mag = floats[:, isave1]
         phase = floats[:, isave2]
-        rtheta = np.radians(phase)
-        real_imag = mag * (np.cos(rtheta) + 1.j * np.sin(rtheta))
+        real_imag = polar_to_real_imag(mag, phase)
     else:
         real = floats[:, isave1]
         imag = floats[:, isave2]