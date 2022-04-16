c1a989f76e8e884e598a39e1f206de9c24fc07dd
SteveDoyle2 <mesheb82@gmail.com>
2022-04-16 00:10:13 -0700

    updating

--- pyNastran/nptyping.py
+++ pyNastran/nptyping.py
@@ -1,57 +1,113 @@
-from typing import Any, Optional
+from typing import Optional
 try:
-    from nptyping import NDArray
-    # vector:
-    # [0., 0., 0.]
-    NDArray3float = NDArray[(3, ), float]
-    NDArray33float = NDArray[(3, 3), float]
-    test = Optional[NDArray3float]
-    del test
+    import nptyping
+    if nptyping.__version__ > '2.0':
+        from nptyping import NDArray, Float, Int, Bool, Shape
+        # vector:
+        # [0., 0., 0.]
+        NDArray3float = NDArray[Shape['3'], Float]
+        NDArray33float = NDArray[Shape['3, 3'], Float]
+        test = Optional[NDArray3float]
+        del test
 
-    # vector:
-    # [0., 0., 0., 0., 0., 0.]
-    NDArray6float = NDArray[(6, ), float]
-    NDArray66float = NDArray[(6, 6), float]
+        # vector:
+        # [0., 0., 0., 0., 0., 0.]
+        NDArray6float = NDArray[Shape['6'], Float]
+        NDArray66float = NDArray[Shape['6, 6'], Float]
 
-    # vector:
-    # [0., 0., 0., ...]
-    NDArrayNbool = NDArray[(Any, ), bool]
-    NDArrayNfloat = NDArray[(Any, ), float]
-    NDArrayNint = NDArray[(Any, ), int]
+        # vector:
+        # [0., 0., 0., ...]
+        NDArrayNbool = NDArray[Shape['Size'], Bool]
+        NDArrayNfloat = NDArray[Shape['Size'], Float]
+        NDArrayNint = NDArray[Shape['Size'], Int]
 
-    # float matrix:
-    # [1., 0., 0.]
-    # [2., 0., 0.]
-    # [3., 0., 0.]
-    NDArrayN3float = NDArray[(Any, 3), float]
-    NDArrayN4float = NDArray[(Any, 4), float]
+        # float matrix:
+        # [1., 0., 0.]
+        # [2., 0., 0.]
+        # [3., 0., 0.]
+        NDArrayN3float = NDArray[Shape['Size, 3'], Float]
+        NDArrayN4float = NDArray[Shape['Size, 4'], Float]
 
-    NDArrayNNfloat = NDArray[(Any, Any), float]
+        NDArrayNNfloat = NDArray[Shape['Size, Size'], Float]
 
-    # int matrix:
-    # [1, 0]
-    # [2, 0]
-    # [3, 0]
-    NDArrayN2int = NDArray[(Any, 2), int]
+        # int matrix:
+        # [1, 0]
+        # [2, 0]
+        # [3, 0]
+        NDArrayN2int = NDArray[Shape['Size, 2'], Int]
 
-    # int matrix:
-    # [1, 0, 0]
-    # [2, 0, 0]
-    # [3, 0, 0]
-    NDArrayN3int = NDArray[(Any, 3), int]
+        # int matrix:
+        # [1, 0, 0]
+        # [2, 0, 0]
+        # [3, 0, 0]
+        NDArrayN3int = NDArray[Shape['Size, 3'], Int]
 
-    # int matrix:
-    # [1, 0, 0, 0]
-    # [2, 0, 0, 0]
-    # [3, 0, 0, 0]
-    NDArrayN4int = NDArray[(Any, 4), int]
-    NDArrayN6int = NDArray[(Any, 6), int]
-    NDArrayN8int = NDArray[(Any, 8), int]
-    NDArrayN9int = NDArray[(Any, 9), int]
-    NDArrayN10int = NDArray[(Any, 10), int]
-    NDArrayN13int = NDArray[(Any, 13), int]
-    NDArrayN15int = NDArray[(Any, 15), int]
-    NDArrayN20int = NDArray[(Any, 20), int]
+        # int matrix:
+        # [1, 0, 0, 0]
+        # [2, 0, 0, 0]
+        # [3, 0, 0, 0]
+        NDArrayN4int = NDArray[Shape['Size, 4'], Int]
+        NDArrayN6int = NDArray[Shape['Size, 6'], Int]
+        NDArrayN8int = NDArray[Shape['Size, 8'], Int]
+        NDArrayN9int = NDArray[Shape['Size, 9'], Int]
+        NDArrayN10int = NDArray[Shape['Size, 10'], Int]
+        NDArrayN13int = NDArray[Shape['Size, 13'], Int]
+        NDArrayN15int = NDArray[Shape['Size, 15'], Int]
+        NDArrayN20int = NDArray[Shape['Size, 20'], Int]
+    else:
+        from nptyping import NDArray
+        from typing import Any
+        # vector:
+        # [0., 0., 0.]
+        NDArray3float = NDArray[(3, ), float]
+        NDArray33float = NDArray[(3, 3), float]
+        test = Optional[NDArray3float]
+        del test
+
+        # vector:
+        # [0., 0., 0., 0., 0., 0.]
+        NDArray6float = NDArray[(6, ), float]
+        NDArray66float = NDArray[(6, 6), float]
+
+        # vector:
+        # [0., 0., 0., ...]
+        NDArrayNbool = NDArray[(Any, ), bool]
+        NDArrayNfloat = NDArray[(Any, ), float]
+        NDArrayNint = NDArray[(Any, ), int]
+
+        # float matrix:
+        # [1., 0., 0.]
+        # [2., 0., 0.]
+        # [3., 0., 0.]
+        NDArrayN3float = NDArray[(Any, 3), float]
+        NDArrayN4float = NDArray[(Any, 4), float]
+
+        NDArrayNNfloat = NDArray[(Any, Any), float]
+
+        # int matrix:
+        # [1, 0]
+        # [2, 0]
+        # [3, 0]
+        NDArrayN2int = NDArray[(Any, 2), int]
+
+        # int matrix:
+        # [1, 0, 0]
+        # [2, 0, 0]
+        # [3, 0, 0]
+        NDArrayN3int = NDArray[(Any, 3), int]
+
+        # int matrix:
+        # [1, 0, 0, 0]
+        # [2, 0, 0, 0]
+        # [3, 0, 0, 0]
+        NDArrayN4int = NDArray[(Any, 4), int]
+        NDArrayN6int = NDArray[(Any, 6), int]
+        NDArrayN8int = NDArray[(Any, 8), int]
+        NDArrayN9int = NDArray[(Any, 9), int]
+        NDArrayN10int = NDArray[(Any, 10), int]
+        NDArrayN13int = NDArray[(Any, 13), int]
+        NDArrayN15int = NDArray[(Any, 15), int]
+        NDArrayN20int = NDArray[(Any, 20), int]
 
 except ImportError:
     NDArrayNbool = \
