2c0544db95b3cc52b88c00cc07418aaee8c1e334
SteveDoyle2 <mesheb82@gmail.com>
2022-05-02 23:24:23 -0700

    adding zip_strict

--- pyNastran/utils/numpy_utils.py
+++ pyNastran/utils/numpy_utils.py
@@ -6,3 +6,9 @@ integer_string_types = (int, np.int32, np.int64, bytes, str)
 integer_float_types = (int, np.int32, np.int64, float, np.float32)
 float_types = (float, np.float32, np.float64)
 bytes_type = (bytes, np.bytes_)
+
+def zip_strict(*arrays):
+   lengths = [len(array) for array in arrays]
+   assert min(lengths) == max(lengths)
+   assert len(lengths) > 0, lengths
+   return zip(*arrays)
