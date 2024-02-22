d8415873d6e820e382817d705b685bc1bdc982e1
SteveDoyle2 <mesheb82@gmail.com>
2024-02-22 15:52:01 -0800

    deleting stuff that's not going in the release

--- pyNastran/converters/dev/lsdyna/test_lsdyna.py
+++
@@ -1,5 +0,0 @@
-from pyNastran.converters.dev.lsdyna.lsdyna import read_lsdyna
-
-if __name__ == '__main__':   # pragma: no cover
-    # https://www.nhtsa.gov/DOT/NHTSA/NVS/Biomechanics%20&%20Trauma/Thor-Lx%20Finite%20Element%20Model/thor-no-skin-right-non-sae.key
-    model = read_lsdyna('thor_no_skin_right_non_sae.key')
--- pyNastran/dev/bdf_vectorized3/types.py
+++
@@ -1,5 +0,0 @@
-from io import StringIO
-from typing import TextIO, Union
-
-__all__ = ['TextIOLike']
-TextIOLike = Union[TextIO, StringIO]
