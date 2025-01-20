644cd28a0fa5f3d1e1c5d652d1a7decb9924563d
SteveDoyle2 <mesheb82@gmail.com>
2025-01-20 14:36:37 -0800

    qtpy is now required to simplify gui setup and it's tiny

--- pyNastran/nptyping_interface.py
+++ pyNastran/nptyping_interface.py
@@ -81,75 +81,6 @@ def get_numpy_typing():
     )
     return out
 
-def get_nptyping_v2():
-    from typing import Optional
-    from nptyping import NDArray, Float, Int, Bool, Shape
-
-    # vector:
-    # [0., 0., 0.]
-    NDArray3float = NDArray[Shape['3'], Float]
-    NDArray33float = NDArray[Shape['3, 3'], Float]
-    test = Optional[NDArray3float]
-    del test
-
-    # vector:
-    # [0., 0., 0., 0., 0., 0.]
-    NDArray6float = NDArray[Shape['6'], Float]
-    NDArray66float = NDArray[Shape['6, 6'], Float]
-
-    # vector:
-    # [0., 0., 0., ...]
-    NDArrayNbool = NDArray[Shape['Size'], Bool]
-    NDArrayNfloat = NDArray[Shape['Size'], Float]
-    NDArrayNint = NDArray[Shape['Size'], Int]
-
-    # float matrix:
-    # [1., 0., 0.]
-    # [2., 0., 0.]
-    # [3., 0., 0.]
-    NDArrayN3float = NDArray[Shape['Size, 3'], Float]
-    NDArrayN4float = NDArray[Shape['Size, 4'], Float]
-
-    NDArrayNNfloat = NDArray[Shape['Size, Size'], Float]
-
-    # int matrix:
-    # [1, 0]
-    # [2, 0]
-    # [3, 0]
-    NDArrayN2int = NDArray[Shape['Size, 2'], Int]
-
-    # int matrix:
-    # [1, 0, 0]
-    # [2, 0, 0]
-    # [3, 0, 0]
-    NDArrayN3int = NDArray[Shape['Size, 3'], Int]
-
-    # int matrix:
-    # [1, 0, 0, 0]
-    # [2, 0, 0, 0]
-    # [3, 0, 0, 0]
-    NDArrayN4int = NDArray[Shape['Size, 4'], Int]
-    NDArrayN6int = NDArray[Shape['Size, 6'], Int]
-    NDArrayN8int = NDArray[Shape['Size, 8'], Int]
-    NDArrayN9int = NDArray[Shape['Size, 9'], Int]
-    NDArrayN10int = NDArray[Shape['Size, 10'], Int]
-    NDArrayN13int = NDArray[Shape['Size, 13'], Int]
-    NDArrayN15int = NDArray[Shape['Size, 15'], Int]
-    NDArrayN20int = NDArray[Shape['Size, 20'], Int]
-
-    out = (
-        NDArrayNbool,
-        NDArray3float, NDArray6float,
-        NDArrayNint, NDArrayNfloat,
-        NDArray66float,
-        NDArrayN3float, NDArrayN4float,
-        NDArray33float,
-        NDArrayN2int, NDArrayN3int, NDArrayN4int, NDArrayN6int, NDArrayN8int, NDArrayN9int,
-        NDArrayN10int, NDArrayN13int, NDArrayN15int, NDArrayN20int,
-        NDArrayNNfloat,
-    )
-    return out
-
 
 def get_nptyping_null():
     from typing import Any
@@ -182,9 +113,6 @@ def get_nptyping_null():
 import numpy as np
 if np.__version__ > '1.20' and np.__version__ < '2.0':
     out = get_numpy_typing()
-    #import nptyping
-    #assert nptyping.__version__ > '2.0'
-    #out = get_nptyping_v2()
 else:
     out = get_nptyping_null()
 
