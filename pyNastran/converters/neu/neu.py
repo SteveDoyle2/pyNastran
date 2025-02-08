be30e2b3199037273368d54abbd99996863de1be
Steve Doyle <mesheb82@gmail.com>
2025-02-07 22:43:09 -0800

    neu work

--- pyNastran/converters/neu/neu.py
+++ pyNastran/converters/neu/neu.py
@@ -95,12 +95,77 @@ def read_neu(neu_filename: str):
             pass
         elif key == '413':  # Layer Data
             pass
-        # """
-        # 450: Output Sets
-        # 451: Output Data Vectors (obsolete)
-        # """
+        elif key == '450':  # Output Sets
+            pass
+        elif key == '451':  # Output Data Vectors (obsolete)
+            read_results(block, version, debug=False)
         else:
             raise RuntimeError(key)
+
+def read_results(block, version, debug=True):
+    """
+       1,   1,   1,
+    CPRE:       2M= 0.9k= 0.1
+          0.,   0.,   0.,
+          0,  0,  1,  0,  0,  0,  0,  0,  0,  0,
+          0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+          0,  0,  3,  8,
+          0,  1,  1,
+          1001,   1.5713624074338103D-08,
+
+    """
+    a = block[1]
+    title = block[2]
+    nums = block[8:]
+    # print(a)
+    # print(title)
+    ids = []
+    values = []
+    results = {
+        title: (ids, values),
+    }
+    inum = 0
+    while inum < len(nums):
+        num = nums[inum]
+        out = num.strip(', \n').split(',')
+        nid_str, *value = [val.strip() for val in out]
+        nid = int(nid_str)
+        # if nid < 105 or nid > 8503:
+        #     print(f'nid = {nid}; values={value}')
+        if nid_str == '-1':
+            """
+                    -1,   0.0000000000000000D+00,
+                       1,   2,   1,
+                    CPIM:       2M= 0.9k= 0.1
+            """
+            inum += 2
+            if inum >= len(nums):
+                break
+            title = nums[inum]
+            ids = []
+            values = []
+            print(f'title = {title!r}')
+            results[title] = (ids, values)
+            # skip over the header
+            inum += 6
+            continue
+        inum += 1
+
+        ids.append(nid_str)
+        values.append(value[0].replace('D', 'E'))
+        assert len(value) == 1, value
+    print(len(ids))
+    print(nums[0])
+    print('done with results')
+    names = list(results.keys())
+    results2 = {}
+    for name, (ids, values) in results.items():
+        ids2 = np.array(ids, dtype='int32')
+        values2 = np.array(values, dtype='float64')
+        results2[name] = (ids2, values2)
+    print(names)
+    return results2
+
 
 def read_elements(block, version: str, debug: bool=False):
     if version == '7.':
@@ -120,6 +185,12 @@ def read_elements(block, version: str, debug: bool=False):
         offsetB: 0.0000000000000000D+00,   0.0000000000000000D+00,   0.0000000000000000D+00,
         """
         # for
+        elements = {}
+        cquad4 = []
+        ctria3 = []
+        ctetra = []
+        #cpenta = []
+        #chexa = []
         iblock_line = 1
         while iblock_line < len(block) - 2:
             sline1 = block[iblock_line].strip(', \n').split(',')
@@ -131,17 +202,28 @@ def read_elements(block, version: str, debug: bool=False):
             etype = int(etype)
             top = int(top)
             nids_temp = sline2 + sline3
+            nids = [int(nid.strip()) for nid in nids_temp[:4]]
+            if top == 3:
+                top = 'CTRIA3'
+                assert len(nids) == 3
+                ctria3.append([eid, pid, nids])
             if top == 4:
                 top = 'CQUAD4'
-                nids = [int(nid.strip()) for nid in nids_temp[:4]]
                 assert len(nids) == 4
+                cquad4.append([eid, pid, nids])
             else:
                 raise RuntimeError(top)
             print(f'{iblock_line}: eid={eid} pid={pid} etype={etype} top={top}; nids={nids}')
             iblock_line += 7
     else:
         raise RuntimeError(version)
-    return quads
+    if cquad4:
+        elements['CQUAD4'] = cquad4
+    if ctria3:
+        elements['CTRIA3'] = ctria3
+    if ctetra:
+        elements['CTETRA'] = ctetra
+    return elements
 
 def read_nodes(block, version: str, debug: bool=False) -> np.ndarray:
     xyzs = []
