28524222018beb304a265c799803c904e963c113
Steve Doyle <mesheb82@gmail.com>
2025-04-28 11:14:03 -0700

    adding

---
+++ pyNastran/dev/matnastran/field_writer_8.m
@@ -0,0 +1,166 @@
+
+function field = print_float_8(value)
+    % Prints a float in nastran 8-character width syntax using the
+    % highest precision possbile.
+
+    if isnan(value)
+        field = '        ';
+        return  % field
+    elseif value == 0.0
+        field = sprintf('%8s', '0.');
+        return  % field
+    elseif value > 0.  % positive, not perfect...
+        if value < 5e-8
+            field = print_scientific_8(value);
+            return  % field
+        elseif value < 0.001
+            field = print_scientific_8(value);
+            field2 = sprintf("%8.7f", value);  % small value
+            field2 = field2.strip('0 ');
+
+            field1 = field.replace('-', 'e-');
+
+            if field2 == '.'
+                field = print_scientific_8(value);
+                return  % field
+            end
+            if len(field2) <= 8 && float(field1) == float(field2)
+                field = field2;
+                field = field.strip(' 0');
+            end
+        %elif value < 0.1
+            %field = sprintf("%8.7f", value);
+        elseif value < 1.
+            field = sprintf("%8.7f", value);  % same as before...
+        elseif value < 10.
+            field = sprintf("%8.6f", value);
+        elseif value < 100.
+            field = sprintf("%8.5f", value);
+        elseif value < 1000.
+            field = sprintf("%8.4f", value);
+        elseif value < 10000.
+            field = sprintf("%8.3f", value);
+        elseif value < 100000.
+            field = sprintf("%8.2f", value);
+        elseif value < 1000000.
+            field = sprintf("%8.1f", value);
+        else  % big value
+            field = sprintf("%8.1f", value);
+            if field.index('.') < 8
+                field = sprintf('%8.1f', round(value));
+                field = field[0:8]
+                %assert '.' != field[0], field
+            else
+                field = print_scientific_8(value);
+            end
+            return  % field
+        end
+    else
+        if value > -5e-7
+            field = print_scientific_8(value);
+            return  % field
+        elseif value > -0.01  % -0.001
+            field = print_scientific_8(value);
+            field2 = sprintf("%8.6f", value);  % small value
+            field2 = field2.strip('0 ');
+
+            % get rid of the first minus sign, add it on afterwards
+            field1 = '-' + field.strip(' 0-').replace('-', 'e-');
+
+            if len(field2) <= 8 and float(field1) == float(field2)
+                field = field2.rstrip(' 0');
+                field = field.replace('-0.', '-.');
+            end
+
+        %elseif value > -0.1
+            % -0.01 >x>-0.1...should be 5 (maybe scientific...)
+            %field = "%8.6f" % value
+            %field = field.replace('-0.', '-.')
+        elseif value > -1.
+            % -0.1  >x>-1.....should be 6, but the baseline 0 is kept...
+            field = sprintf("%8.6f", value);
+            field = field.replace('-0.', '-.');
+        elseif value > -10.
+            field = sprintf("%8.5f", value);   % -1    >x>-10
+        elseif value > -100.
+            field = sprintf("%8.4f", value);   % -10   >x>-100
+        elseif value > -1000.
+            field = sprintf("%8.3f", value);   % -100  >x>-1000
+        elseif value > -10000.
+            field = sprintf("%8.2f", value);   % -1000 >x>-10000
+        elseif value > -100000.
+            field = sprintf("%8.1f", value);   % -10000>x>-100000
+        elseif value <= -999999.5
+            field = print_scientific_8(value);
+            return % field
+        else
+            field = sprintf("%8.1f", value);
+            %try:
+            ifield = field.index('.');
+            %except ValueError:
+            %    raise ValueError('error printing float; cant find decimal; field=%r value=%s' % (
+            %        field, value))
+            if ifield < 8
+                field = sprintf('%7d.', int(round(value, 0));
+                %assert '.' != field[0], field
+            else
+                field = print_scientific_8(value);
+            end
+            return % field
+        end
+    field = field.strip(' 0');
+
+    if not isstring(field)
+        error('not a string')
+    end
+    field = sprintf('%8s', field);
+
+    %assert len(field) == 8, ('value=%r field=%r is not 8 characters '
+    %                         'long, its %s' % (value, field, len(field)))
+    return  % field
+end
+
+function field = print_scientific_8(value)
+    % Prints a value in 8-character scientific notation.
+    % This is a sub-method and shouldn't typically be called
+    %
+    % Notes
+    % -----
+    % print_float_8 : a better float printing method
+
+    if value == 0.0
+        field = sprintf('%8s', '0.');
+        return  % field
+    end
+
+    python_value = sprintf('%8.11e', value);
+    svalue, sexponent = python_value.strip().split('e');
+    exponent = int(sexponent);  % removes 0s
+
+    if abs(values) < 1.
+        sign = '-';
+    else
+        sign = '+';
+    end
+    %sign = '-' if abs(value) < 1. else '+'
+
+    % the exponent will be added later...
+    exp2 = str(exponent).strip('-+');
+    value2 = float(svalue);
+
+    leftover = 5 - len(exp2);
+
+    if value < 0
+        fmt = sprintf("%%1.%sf", leftover - 1);
+    else
+        fmt = sprintf("%%1.%sf", leftover);
+    end
+
+    svalue3 = sprintf(fmt, value2);
+    svalue4 = svalue3.strip('0');
+    field = sprintf("%8s", svalue4 + sign + exp2);
+    return  % field
+end
+
+
+print_float_8(1.0)
