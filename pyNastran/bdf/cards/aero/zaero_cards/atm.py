zaero:  - adding alt range check  - fixing alt units op2:  - geom2 write cleanup

pyNastran/bdf/cards/aero/zaero_cards/atm.py
@@ -14,6 +14,7 @@ from typing import TYPE_CHECKING
 import numpy as np
 from matplotlib import pyplot as plt
 
+from pyNastran.utils.convert import convert_length
 from pyNastran.utils.numpy_utils import integer_types
 from pyNastran.bdf.field_writer_8 import print_card_8
 from pyNastran.bdf.cards.base_card import BaseCard
@@ -69,6 +70,11 @@ class ATMOS(BaseCard):
         assert length_unit in ['IN', 'FT', 'M', 'MM', 'CM', 'KM', 'NONE'], f'length_unit={length_unit}'
         assert temperature_unit in ['R', 'K', 'C', 'F'], temperature_unit
         assert len(atmosphere_table) > 0, atmosphere_table
+        alt = self.alt
+        sos = self.sos
+        assert len(np.unique(alt)) == len(alt)
+        assert np.array_equal(np.unique(alt), alt)
+        # assert np.array_equal(np.unique(sos), sos)
 
     @property
     def alt(self) -> np.ndarray:
@@ -143,7 +149,6 @@ class ATMOS(BaseCard):
             axes = [axes]
         assert len(axes) == ntables, (axes, ntables)
 
-
     def raw_fields(self):
         """
         Gets the fields in their unmodified form
@@ -161,7 +166,7 @@ class ATMOS(BaseCard):
         ] + self.atmosphere_table.ravel().tolist()
         return list_fields
 
-    def repr_fields(self):
+    def repr_fields(self) -> list:
         list_fields = self.raw_fields()
         return list_fields
 
@@ -321,6 +326,12 @@ class FIXMATM(BaseCard):
         assert len(alts) >= 0, alts
         if len(alts) > 1:
             assert alts[0] < alts[1], alts
+        if atm_id == 0:
+            # -100 kft to 260 kft
+            alts2 = convert_length(self.alts, self.length_unit.lower(), 'ft')
+            assert alts2.min() >= -100_000., (alts2.min(), alts2.max())  # ft
+            assert alts2.max() <= 260_000., (alts2.min(), alts2.max())  # ft
+            assert alts[0]
 
     @classmethod
     def add_card(cls, card: BDFCard, comment: str=''):
@@ -367,6 +378,21 @@ class FIXMATM(BaseCard):
 
     def cross_reference(self, model: BDF) -> None:
         cross_reference_atmos_mkaeroz_fluttf(self, model)
+        # self.plot_atmosphere()
+
+    def plot_atmosphere(self) -> None:
+        atmos = self.atmos_ref
+        from matplotlib import pyplot as plt
+        fig = plt.figure(1)
+        ax = fig.gca()
+        ax.plot(atmos.alt, atmos.sos, 'k-')
+
+        sos = np.interp(self.alts, atmos.alt, atmos.sos)
+        ax.plot(self.alts, sos, 'o')
+        ax.grid(True)
+        ax.set_xlabel('Altitude (ft)')
+        ax.set_ylabel('SO (ft/s)')
+        plt.show()
 
     def safe_cross_reference(self, model: BDF, xref_errors):
         self.cross_reference(model)

pyNastran/bdf/mesh_utils/cmd_line/create_flutter.py
@@ -453,13 +453,17 @@ def create_flutter(
             atm_temperature(alt, alt_units=alt_units, temperature_units=temperature_units)
             for alt in alts])
 
+        assert length_unit in ['in', 'm'], length_unit
+        alt_scale = 1.
+        if length_unit == 'in':
+            alt_scale = 12.
         # density0 = atm_density(0.0, 1716.0, alt_units=alt_units, density_units=density_units)
         # round density to 4 sig figs
         density_magnitude = np.floor(np.log10(np.abs(density)))
         decimals = 3 - density_magnitude
         decimals = np.nan_to_num(decimals, nan=0.0, posinf=0.0, neginf=0.0).astype(int)
         density2 = np.array([np.round(x, d) for x, d in zip(density, decimals)])
-        alts2 = alts.round(0)
+        alts2 = alts.round(0) * alt_scale
         sos2 = sos.round(0)
         atmosphere_table = np.column_stack([
                 alts2, sos2, density2, temperature.round(2),])
@@ -468,7 +472,7 @@ def create_flutter(
 
         atmosphere_list = atmosphere_table_reversed.ravel().tolist()
         msg = (
-            f"\n alt: ({alts2.min()}, {alts2.max()}) ({alt_units})"
+            f"\n alt: ({alts2.min()}, {alts2.max()}) ({length_unit})"
             f"\n sos: ({sos2.min()}, {sos2.max()}) ({velocity_units})"
             f"\n rho: ({density2.min()}, {density2.max()}) ({density_units})"
         )
@@ -486,8 +490,9 @@ def create_flutter(
         print_flag = 0
         alts2.sort()
         model.zaero.add_fixmatm(
-            sweep_id, mkaeroz_id, atm_id, mass_unit, length_unit,
-            fluttf_id, print_flag, alts2, vref=1.0, comment="")
+            sweep_id, mkaeroz_id, alts2,
+            atm_id=atm_id, mass_unit=mass_unit, length_unit=length_unit,
+            fluttf_id=fluttf_id, print_flag=fluttf_id, vref=1.0, comment="")
 
         freqs = [0.1, 0.2, 0.5, 1.]
         model.zaero.add_mkaeroz(mkaeroz_id, mach, freqs, comment="")